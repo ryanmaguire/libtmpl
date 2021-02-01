10183fd0590286ca199b9b14f6b90d1de59820b5
Ryan Maguire <ryan_maguire@student.uml.edu>
2021-02-01 16:37:08 -0500

    Adding include files from rss_ringoccs.

---
+++ include/tmpl_bool.h
@@ -0,0 +1,46 @@
+/******************************************************************************
+ *                                 LICENSE                                    *
+ ******************************************************************************
+ *  This file is part of libtmpl.                                             *
+ *                                                                            *
+ *  libtmpl is free software: you can redistribute it and/or modify it        *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  libtmpl is distributed in the hope that it will be useful,                *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with libtmpl.  If not, see <https://www.gnu.org/licenses/>.         *
+ ******************************************************************************
+ *                               tmpl_bool                                    *
+ ******************************************************************************
+ *  Purpose:                                                                  *
+ *      Define bool, false, and true for C89/C90 compilers. The C89/C90 does  *
+ *      not define such a data type. Booleans were first introduced in C99.   *
+ *  NOTES:                                                                    *
+ *      This code is identical to the code I wrote for rss_ringoccs, only the *
+ *      rssringoccs titles have been replaced with tmpl. rss_ringoccs is also *
+ *      released under GPL3.                                                  *
+ ******************************************************************************
+ *  Author:     Ryan Maguire, Dartmouth College                               *
+ *  Date:       February 1, 2021                                              *
+ ******************************************************************************/
+
+/*  Include guard to prevent including this file twice.                       */
+#ifndef __TMPL_BOOL_H__
+#define __TMPL_BOOL_H__
+
+/*  The C99, C11, and C18 standards require support for booleans, but it is   *
+ *  not available in C89/C90. We've typedef'd Booleans as tmpl_Bool to avoid  *
+ *  naming conflicts with C99 and higher Booleans. If you compiler            *
+ *  supports Booleans, you should be able to use stdbools with libtmpl.       */
+
+/*  We prepend tmpl onto False, True, and Bool to avoid name conflicts.       */
+typedef enum {tmpl_False, tmpl_True} tmpl_Bool;
+
+#endif
+/*  End of include guard.                                                     */
---
+++ include/tmpl_bytes.h
@@ -0,0 +1,100 @@
+/******************************************************************************
+ *                                 LICENSE                                    *
+ ******************************************************************************
+ *  This file is part of libtmpl.                                             *
+ *                                                                            *
+ *  libtmpl is free software: you can redistribute it and/or modify it        *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  libtmpl is distributed in the hope that it will be useful,                *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with libtmpl.  If not, see <https://www.gnu.org/licenses/>.         *
+ ******************************************************************************
+ *                                tmpl_bytes                                  *
+ ******************************************************************************
+ *  Purpose:                                                                  *
+ *      Provide tools for swapping bytes of char pointers and for determining *
+ *      the endianness of your platform. This is useful for reading binary    *
+ *      files, but also learning about how computers work and store numbers.  *
+ *  NOTES:                                                                    *
+ *      It is assumed a char is either 8 bits or 16 bits. This is probably    *
+ *      the case for your computer. The only devices I know of where this may *
+ *      not be true are hand-held calculators. So don't try to use            *
+ *      libtmpl on those, I suppose. If CHAR_BITS (defined in limits.h)       *
+ *      is neither of these, libtmpl will fail to build.                      *
+ *                                                                            *
+ *      This file is a fork of the bytes library I wrote for rss_ringoccs.    *
+ *      rss_ringoccs is also released under the GPL 3.                        *
+ ******************************************************************************
+ *  Author:     Ryan Maguire, Dartmouth College                               *
+ *  Date:       February 1, 2021                                              *
+ ******************************************************************************/
+
+/*  Include guard to prevent including this file twice.                       */
+#ifndef __TMPL_BYTES_H__
+#define __TMPL_BYTES_H__
+
+/*  Data type for determining the endianness of your platform.                */
+typedef enum {
+    tmpl_UnknownEndian,
+    tmpl_LittleEndian,
+    tmpl_MixedEndian,
+    tmpl_BigEndian
+} tmpl_Endian;
+
+/******************************************************************************
+ *  Function:                                                                 *
+ *      tmpl_Determine_Endianness                                             *
+ *  Purpose:                                                                  *
+ *      Determines the endianness of your platform.                           *
+ *  Arguments:                                                                *
+ *      None (void).                                                          *
+ *  Output:                                                                   *
+ *      tmpl_Endian endianness:                                               *
+ *          A tmpl_Endian data type whose value corresponds to the            *
+ *          endianness of your system.                                        *
+ *  NOTES:                                                                    *
+ *      1.) This function assumes CHAR_BITS (defined in limits.h) is either   *
+ *          8 or 16. If this is not true, libtmpl will fail to build.         *
+ ******************************************************************************/
+extern tmpl_Endian tmpl_Determine_Endianness(void);
+
+/******************************************************************************
+ *  Function:                                                                 *
+ *      tmpl_Swap_Bytes                                                       *
+ *  Purpose:                                                                  *
+ *      Swaps the values of two char pointers.                                *
+ *  Arguments:                                                                *
+ *      ptr1 (char *):                                                        *
+ *          The first pointer to a char.                                      *
+ *      ptr2 (char *):                                                        *
+ *          The second pointer to a char.                                     *
+ *  Output:                                                                   *
+ *      None (void).                                                          *
+ ******************************************************************************/
+extern void tmpl_Swap_Bytes(char *ptr1, char *ptr2);
+
+/******************************************************************************
+ *  Function:                                                                 *
+ *      tmpl_Swap_Most_Significant_Bit_2                                      *
+ *  Purpose:                                                                  *
+ *      Changes the endianness of a data type that is two chars long. This    *
+ *      is usually a "short". Similarly functions are provided for 4 and 8.   *
+ *  Arguments:                                                                *
+ *      ptr (char *):                                                         *
+ *          A pointer to a char array.                                        *
+ *  Output:                                                                   *
+ *      None (void).                                                          *
+ ******************************************************************************/
+extern void tmpl_Swap_Most_Significant_Bit_2(char *ptr);
+extern void tmpl_Swap_Most_Significant_Bit_4(char *ptr);
+extern void tmpl_Swap_Most_Significant_Bit_8(char *ptr);
+
+#endif
+/*  End of include guard.                                                     */
