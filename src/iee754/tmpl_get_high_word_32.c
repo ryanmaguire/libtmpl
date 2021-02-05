e8d28352bb96fb8d6292308d37012d366ff471b1
Ryan Maguire <ryan_maguire@student.uml.edu>
2021-02-05 09:47:14 -0500

    Added IEEE754 functions.

---
+++ src/iee754/tmpl_get_high_word_32.c
@@ -0,0 +1,9 @@
+#include <libtmpl/include/tmpl_ieee754.h>
+
+unsigned int tmpl_Get_High_Word32(tmpl_IEE754_Word32 x)
+{
+	unsigned int out = x.integer;
+	out = out >> 23;
+
+	return out;
+}
---
+++ src/iee754/tmpl_get_low_word_32.c
@@ -0,0 +1,8 @@
+#include <libtmpl/include/tmpl_ieee754.h>
+
+unsigned int tmpl_Get_Low_Word32(tmpl_IEE754_Word32 x)
+{
+	unsigned int out = x.integer;
+	out = out & 8388607U;
+	return out;
+}
