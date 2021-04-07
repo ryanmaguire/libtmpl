88420e54b7e65524604dfb652ce274f814e69267
ryanmaguire <rmaguire@protonmail.com>
2021-04-07 19:13:34 -0400

    Fixing dependencies, finishing IEEE 754

--- src/ieee754/tmpl_get_mantissa64.c
+++ src/ieee754/tmpl_get_mantissa64.c
@@ -59,9 +59,6 @@
  *  Date:       April 7, 2021                                                 *
  ******************************************************************************/
 
-/*  Fixed-width integer data types contained here.                            */
-#include <libtmpl/include/tmpl_integer.h>
-
 /*  Definitions, typedefs, and prototypes found here.                         */
 #include <libtmpl/include/tmpl_ieee754.h>
 
