309818537a9c06df6ca84a8a1295ad7418b6934e
ryanmaguire <ryan_maguire@student.uml.edu>
2021-05-01 09:04:51 -0400

    Updating special functions.

--- include/tmpl_special_functions.h
+++ include/tmpl_special_functions.h
@@ -13,59 +13,59 @@ extern double tmpl_Double_Bessel_I0_Asym[7];
 extern long double tmpl_LDouble_Bessel_I0_Taylor[29];
 extern long double tmpl_LDouble_Bessel_I0_Asym[9];
 
+extern float
+tmpl_Float_Bessel_I0(float x);
+
+extern double
+tmpl_Double_Bessel_I0(double x);
+
+extern long double
+tmpl_LDouble_Bessel_I0(long double x);
+
 extern float tmpl_Float_Bessel_J0_Taylor[17];
-extern float tmpl_Float_Bessel_J0_Asym[5];
+extern float tmpl_Float_Bessel_J0_Asym[9];
 
-extern double tmpl_Double_Bessel_J0_Taylor[25];
-extern double tmpl_Double_Bessel_J0_Asym[7];
+extern double tmpl_Double_Bessel_J0_Taylor[31];
+extern double tmpl_Double_Bessel_J0_Asym[9];
 
-extern long double tmpl_LDouble_Bessel_J0_Taylor[29];
+extern long double tmpl_LDouble_Bessel_J0_Taylor[31];
 extern long double tmpl_LDouble_Bessel_J0_Asym[9];
 
 extern float
 tmpl_Float_Bessel_J0(float x);
-                            
+
 extern double
 tmpl_Double_Bessel_J0(double x);
-                       
-extern long double
-tmpl_LDouble_Bessel_J0(long double x);            
 
-extern float
-tmpl_Float_Bessel_I0(float x);
-                            
-extern double
-tmpl_Double_Bessel_I0(double x);
-                       
 extern long double
-tmpl_LDouble_Bessel_I0(long double x);            
+tmpl_LDouble_Bessel_J0(long double x);
 
 extern float
 tmpl_Float_LambertW(float x);
-                            
+
 extern double
 tmpl_Double_LambertW(double x);
-                       
+
 extern long double
-tmpl_LDouble_LambertW(long double x);  
+tmpl_LDouble_LambertW(long double x);
 
 extern float
 tmpl_Float_Fresnel_Cos(float x);
-                            
+
 extern double
 tmpl_Double_Fresnel_Cos(double x);
-                       
+
 extern long double
-tmpl_LDouble_Fresnel_Cos(long double x);  
-    
+tmpl_LDouble_Fresnel_Cos(long double x);
+
 extern float
 tmpl_Float_Fresnel_Sin(float x);
-                            
+
 extern double
 tmpl_Double_Fresnel_Sin(double x);
-                       
+
 extern long double
-tmpl_LDouble_Fresnel_Sin(long double x);      
+tmpl_LDouble_Fresnel_Sin(long double x);
 
 
 extern void
@@ -145,85 +145,85 @@ tmpl_Min_Long(long *arr, long n_elements);
 extern unsigned long
 tmpl_Min_ULong(unsigned long *arr, long n_elements);
 
-                                         
-extern unsigned long **                                                                 
-tmpl_Where_Lesser_Char(char *data, unsigned long dim, double threshold);   
-                                                                               
-extern unsigned long **                                                                 
+
+extern unsigned long **
+tmpl_Where_Lesser_Char(char *data, unsigned long dim, double threshold);
+
+extern unsigned long **
 tmpl_Where_Lesser_UChar(unsigned char *data, unsigned long dim,
-                        double threshold);                        
-                                                                               
-extern unsigned long **                                                                 
-tmpl_Where_Lesser_Short(short *data, unsigned long dim, double threshold); 
-                                                                               
-extern unsigned long **                                                                 
-tmpl_Where_Lesser_UShort(unsigned short *data, unsigned long dim,          
-                         double threshold);                       
-                                                                               
-extern unsigned long **                                                                 
-tmpl_Where_Lesser_Int(int *data, unsigned long dim, double threshold);     
-                                                                               
-extern unsigned long **                                                                 
-tmpl_Where_Lesser_UInt(unsigned int *data, unsigned long dim,              
-                       double threshold);                         
-                                                                               
-extern unsigned long **                                                                 
-tmpl_Where_Lesser_Long(long *data, unsigned long dim, double threshold);   
-                                                                               
-extern unsigned long **                                                                 
-tmpl_Where_Lesser_ULong(unsigned long *data, unsigned long dim,            
-                        double threshold);                        
-                                                                               
-                                                                               
-extern unsigned long **                                                                 
-tmpl_Where_Lesser_Float(float *data, unsigned long dim, float threshold);  
-                                                                               
-extern unsigned long **                                                                 
-tmpl_Where_Lesser_Double(double *data, unsigned long dim,                  
-                                      double threshold);                       
-                                                                               
-extern unsigned long **                                                                 
-tmpl_Where_Lesser_LDouble(long double *data, unsigned long dim,         
+                        double threshold);
+
+extern unsigned long **
+tmpl_Where_Lesser_Short(short *data, unsigned long dim, double threshold);
+
+extern unsigned long **
+tmpl_Where_Lesser_UShort(unsigned short *data, unsigned long dim,
+                         double threshold);
+
+extern unsigned long **
+tmpl_Where_Lesser_Int(int *data, unsigned long dim, double threshold);
+
+extern unsigned long **
+tmpl_Where_Lesser_UInt(unsigned int *data, unsigned long dim,
+                       double threshold);
+
+extern unsigned long **
+tmpl_Where_Lesser_Long(long *data, unsigned long dim, double threshold);
+
+extern unsigned long **
+tmpl_Where_Lesser_ULong(unsigned long *data, unsigned long dim,
+                        double threshold);
+
+
+extern unsigned long **
+tmpl_Where_Lesser_Float(float *data, unsigned long dim, float threshold);
+
+extern unsigned long **
+tmpl_Where_Lesser_Double(double *data, unsigned long dim,
+                                      double threshold);
+
+extern unsigned long **
+tmpl_Where_Lesser_LDouble(long double *data, unsigned long dim,
                           long double threshold);
 
 
-extern unsigned long **                                                                 
-tmpl_Where_Greater_Char(char *data, unsigned long dim, double threshold);   
-                                                                               
-extern unsigned long **                                                                 
+extern unsigned long **
+tmpl_Where_Greater_Char(char *data, unsigned long dim, double threshold);
+
+extern unsigned long **
 tmpl_Where_Greater_UChar(unsigned char *data, unsigned long dim,
-                        double threshold);                        
-                                                                               
-extern unsigned long **                                                                 
-tmpl_Where_Greater_Short(short *data, unsigned long dim, double threshold); 
-                                                                               
-extern unsigned long **                                                                 
-tmpl_Where_Greater_UShort(unsigned short *data, unsigned long dim,          
-                         double threshold);                       
-                                                                               
-extern unsigned long **                                                                 
-tmpl_Where_Greater_Int(int *data, unsigned long dim, double threshold);     
-                                                                               
-extern unsigned long **                                                                 
-tmpl_Where_Greater_UInt(unsigned int *data, unsigned long dim,              
-                       double threshold);                         
-                                                                               
-extern unsigned long **                                                                 
-tmpl_Where_Greater_Long(long *data, unsigned long dim, double threshold);   
-                                                                               
-extern unsigned long **                                                                 
-tmpl_Where_Greater_ULong(unsigned long *data, unsigned long dim,            
-                        double threshold);                        
-                                                                
-extern unsigned long **                                                                 
-tmpl_Where_Greater_Float(float *data, unsigned long dim, float threshold);  
-                                                                               
-extern unsigned long **                                                                 
-tmpl_Where_Greater_Double(double *data, unsigned long dim,                  
-                                      double threshold);                       
-                                                                               
-extern unsigned long **                                                                 
-tmpl_Where_Greater_LDouble(long double *data, unsigned long dim,         
+                        double threshold);
+
+extern unsigned long **
+tmpl_Where_Greater_Short(short *data, unsigned long dim, double threshold);
+
+extern unsigned long **
+tmpl_Where_Greater_UShort(unsigned short *data, unsigned long dim,
+                         double threshold);
+
+extern unsigned long **
+tmpl_Where_Greater_Int(int *data, unsigned long dim, double threshold);
+
+extern unsigned long **
+tmpl_Where_Greater_UInt(unsigned int *data, unsigned long dim,
+                       double threshold);
+
+extern unsigned long **
+tmpl_Where_Greater_Long(long *data, unsigned long dim, double threshold);
+
+extern unsigned long **
+tmpl_Where_Greater_ULong(unsigned long *data, unsigned long dim,
+                        double threshold);
+
+extern unsigned long **
+tmpl_Where_Greater_Float(float *data, unsigned long dim, float threshold);
+
+extern unsigned long **
+tmpl_Where_Greater_Double(double *data, unsigned long dim,
+                                      double threshold);
+
+extern unsigned long **
+tmpl_Where_Greater_LDouble(long double *data, unsigned long dim,
                           long double threshold);
 
 
