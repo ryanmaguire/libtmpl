0258bc0dc78ac4e1901350d2caf73542813fb800
Ryan <ryan_maguire@student.uml.edu>
2021-05-07 09:09:39 -0400

    Updating optics code.

--- include/tmpl_optics.h
+++ include/tmpl_optics.h
@@ -6,6 +6,8 @@
 #define tmpl_Speed_Of_Light_KMS    299792.4580
 #define tmpl_Speed_Of_Light_KMS_L  299792.4580L
 
+#include <libtmpl/include/tmpl_complex.h>
+
 extern float
 Fresnel_Scale_Float(float lambda, float d, float phi, float b);
 
@@ -42,6 +44,68 @@ tmpl_Double_Frequency_To_Wavelength(double x);
 
 extern long double
 tmpl_LDouble_Frequency_To_Wavelength(long double x);
+
+/*  The ringlet and gap modeling functions.                                   */
+extern tmpl_ComplexDouble
+tmpl_CDouble_Ringlet_Diffraction(double x, double a, double b, double F);
+
+extern tmpl_ComplexDouble
+tmpl_CDouble_Gap_Diffraction(double x, double a, double b, double F);
+
+/*  Functions for computing the phase of a ringlet.                           */
+
+extern float
+tmpl_Float_Ringlet_Diffraction_Phase(float x, float a, float b, float F);
+
+extern double
+tmpl_Double_Ringlet_Diffraction_Phase(double x, double a,
+                                             double b, double F);
+
+extern tmpl_ComplexDouble
+tmpl_CDouble_Square_Wave_Diffraction(double x, double W, double F,
+                                     unsigned int N);
+
+extern long double
+tmpl_LDouble_Ringlet_Diffraction_Phase(long double x, long double a,
+                                       long double b, long double F);
+
+/*  Left and right straightedge modeling tools.                               */
+extern tmpl_ComplexDouble
+tmpl_CDouble_Right_Straightedge_Diffraction(double x, double edge, double F);
+
+extern tmpl_ComplexDouble
+tmpl_CDouble_Left_Straightedge_Diffraction(double x, double edge, double F);
+
+/******************************************************************************
+ *--------------------Single Slit Fraunhofer Diffraction----------------------*
+ ******************************************************************************/
+
+extern float
+tmpl_Float_One_Slit_Fraunhofer_Diffraction(float x, float z, float a);
+
+extern double
+tmpl_Double_One_Slit_Fraunhofer_Diffraction(double x, double z, double a);
+
+extern long double
+tmpl_LDouble_One_Slit_Fraunhofer_Diffraction(long double x, long double z,
+                                             long double a);
+
+/******************************************************************************
+ *--------------------Double Slit Fraunhofer Diffraction----------------------*
+ ******************************************************************************/
+
+extern float
+tmpl_Float_Two_Slit_Fraunhofer_Diffraction(float x, float z, float a,
+                                           float d, float lambda);
+
+extern double
+tmpl_Double_Two_Slit_Fraunhofer_Diffraction(double x, double z, double a,
+                                            double d, double lambda);
+
+extern long double
+tmpl_LDouble_Two_Slit_Fraunhofer_Diffraction(long double x, long double z,
+                                             long double a, long double d,
+                                             long double lambda);
 
 #endif
 
