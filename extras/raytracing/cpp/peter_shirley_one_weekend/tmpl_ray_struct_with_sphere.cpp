cfdb2bca0ff2fe5e348dd9f4385106d0c920211c
Ryan Maguire <ryan_maguire@student.uml.edu>
2021-05-20 08:48:27 -0400

    Updating raytracing code.

--- extras/raytracing/cpp/peter_shirley_one_weekend/tmpl_ray_struct_with_sphere.cpp
+++ extras/raytracing/cpp/peter_shirley_one_weekend/tmpl_ray_struct_with_sphere.cpp
@@ -54,18 +54,18 @@ struct tmpl_simple_color {
         green = g;
         blue  = b;
     }
-    
+
     /*  Operator for adding colors.                                           */
     tmpl_simple_color operator + (tmpl_simple_color r)
     {
         /*  Two colors are added by averaging their components.               */
         unsigned char out_red, out_green, out_blue;
-        
+
         /*  Cast to unsigned long's and compute the sum.                      */
         out_red   = (unsigned char)(0.5*((double)r.red   + (double)red));
         out_green = (unsigned char)(0.5*((double)r.green + (double)green));
         out_blue  = (unsigned char)(0.5*((double)r.blue  + (double)blue));
-        
+
         /*  Use the constructor to return the sum from the RGB values.        */
         return tmpl_simple_color(out_red, out_green, out_blue);
     }
@@ -77,7 +77,7 @@ struct tmpl_simple_color {
         r = (unsigned char)(a * (double)red);
         g = (unsigned char)(a * (double)green);
         b = (unsigned char)(a * (double)blue);
-        
+
         return tmpl_simple_color(r, g, b);
     }
 
@@ -102,27 +102,27 @@ struct tmpl_simple_vector {
     {
         return;
     }
- 
-    /*  Constructor from three doubles. Set the components to the inputs.     */ 
+
+    /*  Constructor from three doubles. Set the components to the inputs.     */
     tmpl_simple_vector(double a, double b, double c)
     {
         x = a;
         y = b;
         z = c;
     }
-    
+
     /*  Define the basic operators for the vector. First is vector addition.  */
     tmpl_simple_vector operator + (tmpl_simple_vector r)
     {
         return tmpl_simple_vector(x + r.x, y + r.y, z + r.z);
     }
-    
+
     /*  Vector subtraction.                                                   */
     tmpl_simple_vector operator - (tmpl_simple_vector r)
     {
         return tmpl_simple_vector(x - r.x, y - r.y, z - r.z);
     }
-    
+
     /*  Vector negation.                                                      */
     tmpl_simple_vector operator - (void)
     {
@@ -134,19 +134,19 @@ struct tmpl_simple_vector {
     {
         return tmpl_simple_vector(a*x, a*y, a*z);
     }
-    
+
     /*  Scalar division.                                                      */
     tmpl_simple_vector operator / (double a)
     {
         return tmpl_simple_vector(x/a, y/a, z/a);
     }
 
-    /*  Euclidean norm (the length of the vector).                            */    
+    /*  Euclidean norm (the length of the vector).                            */
     double norm(void)
     {
         return std::sqrt(x*x + y*y + z*z);
     }
-    
+
     /*  Square of the Euclidean norm.                                         */
     double normsq(void)
     {
@@ -158,13 +158,13 @@ struct tmpl_simple_vector {
     {
         return r.x*x + r.y*y + r.z*z;
     }
-    
+
     /*  Euclidean cross product in three dimensions.                          */
     tmpl_simple_vector cross(tmpl_simple_vector r)
     {
         return tmpl_simple_vector(y*r.z - z*r.y, z*r.x - x*r.z, x*r.y - y*r.x);
     }
-    
+
     /*  Unit vector for the given vector.                                     */
     tmpl_simple_vector unit(void)
     {
@@ -190,7 +190,7 @@ struct tmpl_simple_ray {
     {
         return;
     }
-    
+
     /*  Constructor from a starting point and a direction.                    */
     tmpl_simple_ray(tmpl_simple_vector A, tmpl_simple_vector B)
     {
@@ -214,6 +214,45 @@ struct tmpl_simple_ray {
     }
 };
 /*  End of tmpl_simple_ray definition.                                        */
+
+/*  Struct for dealing with sphere.                                           */
+struct tmpl_simple_sphere {
+    double radius;
+    tmpl_simple_vector center;
+
+    /*  Empty constructor. Return NULL.                                       */
+    tmpl_simple_sphere(void)
+    {
+        return;
+    }
+
+    /*  Main constructor.                                                     */
+    tmpl_simple_sphere(double r, tmpl_simple_vector c)
+    {
+        radius = r;
+        center = c;
+    }
+};
+/*  End of definition of tmpl_simple_sphere.                                  */
+
+bool sphere_is_hit(tmpl_simple_ray r, tmpl_simple_sphere s)
+{
+    tmpl_simple_vector oc = r.p - s.center;
+    double a = (r.v).normsq();
+    double b = 2.0 * (r.v).dot(oc);
+    double c = oc.normsq() - s.radius*s.radius;
+    double D = b*b - 4.0*a*c;
+
+    if (D > 0.0)
+    {
+        if (a*(-b + sqrt(D)) > 0.0)
+            return true;
+        else
+            return false;
+    }
+    else
+        return false;
+}
 
 /*  Function for coloring the background with a gradient.                     */
 static tmpl_simple_color sky_gradient(tmpl_simple_ray r)
@@ -242,17 +281,20 @@ int main(void)
     double viewport_height = 2.0;
     double viewport_width  = viewport_height * aspect_ratio;
     double focal_length = 1.0;
+    tmpl_simple_color red = tmpl_simple_color(255U, 0U, 0U);
+
+    tmpl_simple_sphere s = tmpl_simple_sphere(0.5, tmpl_simple_vector(0, 0, -1));
 
     tmpl_simple_vector origin = tmpl_simple_vector(0.0, 0.0, 0.0);
     tmpl_simple_vector horizontal, vertical, lower_left_corner;
     horizontal = tmpl_simple_vector(viewport_width, 0.0, 0.0);
     vertical   = tmpl_simple_vector(0.0, viewport_height, 0.0);
-    
+
     lower_left_corner = origin - (horizontal*0.5) - (vertical*0.5) -
                         tmpl_simple_vector(0.0, 0.0, focal_length);
 
     fp = std::fopen("tmpl_test_ray.ppm", "w");
-    
+
     if (!fp)
     {
         std::puts("fopen failed and returned NULL. Aborting.");
@@ -271,12 +313,16 @@ int main(void)
             v = (double)m * height_factor;
             direction = lower_left_corner + horizontal*u + vertical*v - origin;
             r = tmpl_simple_ray(origin, direction);
-            color = sky_gradient(r);
+
+            if (sphere_is_hit(r, s))
+                color = red;
+            else
+                color = sky_gradient(r);
+
             color.write(fp);
         }
     }
 
     return 0;
 }
-
 
