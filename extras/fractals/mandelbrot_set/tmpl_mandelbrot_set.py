502744fdb0c8b8899b97b015e424ce75c5c07770
Ryan Maguire <ryan_maguire@student.uml.edu>
2021-06-02 22:01:03 -0400

    Updates.

--- extras/fractals/mandelbrot_set/tmpl_mandelbrot_set.py
+++ extras/fractals/mandelbrot_set/tmpl_mandelbrot_set.py
@@ -19,7 +19,7 @@
 #   Purpose:                                                                   #
 #       Draw the mandelbrot set.                                               #
 #   Notes:                                                                     #
-#       This file is an extra and is not compiled as part of libtmpl.          #
+#       This file is an extra and is not part of the libtmpl library.          #
 ################################################################################
 #   Author: Ryan Maguire                                                       #
 #   Date:   June 1, 2021.                                                      #
@@ -30,10 +30,10 @@ width = 1024;
 height = 1024;
 
 # The maximum number of iterations allowed while computing the fractal.
-imax = 256;
+max_iters = 256;
 
-# The maximum radius such that points will automatically be consider divergent.
-zmax = 4.0;
+# The maximum radius that will automatically be considered divergent.
+radius = 4.0;
 
 # The values of the rectangle that will be drawn.
 x_min = -3.0;
@@ -46,37 +46,62 @@ x_factor = (x_max - x_min)/(width - 1.0);
 y_factor = (y_max - y_min)/(height - 1.0);
 
 # Open the PPM file and give it write permissions.
-fp = open("swipecat_fractal.ppm", "w");
+fp = open("tmpl_mandelbrot_set.ppm", "w");
 
 # Write the preamble to the PPM file. We'll use P3, which is text based, to
 # ensure that this file renders properly on Windows.
 fp.write("P3\n%d %d\n255\n" % (width, height));
 
+# Loop over the y axis
 for y in range(height):
-    z_y = y_max - y_factor*y;
+
+    # Compute the corresponding y coordinate in the plane.
+    c_y = y_max - y_factor*y;
+
+    # Loop over the x pixels.
     for x in range(width):
-        z_x = x_min + x_factor*x;
-        c = complex(z_x, z_y);
+
+        # Compute the corresponding x coordinate in the plane.
+        c_x = x_min + x_factor*x;
+
+        # Compute the complex number c_x + c_y i.
+        c = complex(c_x, c_y);
+
+        # Reset z back to zero and start the iteration.
         z = 0.0;
-        backgnd = 0.0;
-        for its in range(imax):
+
+        # Perform the Mandelbrot iteration and see if the point diverges.
+        for its in range(max_iters):
             z = z*z + c;
-            if abs(z.real) >= zmax:
+
+            if abs(z) >= radius:
                 break;
 
-        if (its == imax - 1):
+        # If we never diverged, this point is part of the Mandelbrot set.
+        if (its == max_iters - 1):
             red = 0;
             green = 0;
             blue = 0;
+
+        # Otherwise, give a gradient to represent how many iterations it took
+        # for the iteration to diverge.
         elif (its < 64):
             red = 4*its;
             green = 4*its;
             blue = 255 - 4*its;
+
+        # Color points that take a very long time to diverge yellow.
         else:
             red = 255;
             green = 255;
             blue = 0;
 
+        # Write this color to the PPM file
         fp.write("%u %u %u\n" % (red, green, blue));
+
+    # End of x for-loop.
+# End of y for-loop.
+
+# Close the file and end the routine.
 fp.close();
 
