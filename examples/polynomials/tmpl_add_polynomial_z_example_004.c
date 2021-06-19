86e3b75ec02486e2d27b1f0bdbc7cf152a6afa3b
ryanmaguire <rmaguire@protonmail.com>
2021-06-19 15:07:23 -0400

    Updating polynomial tools.

---
+++ examples/polynomials/tmpl_add_polynomial_z_example_004.c
@@ -0,0 +1,72 @@
+/******************************************************************************
+ *                                 LICENSE                                    *
+ ******************************************************************************
+ *  This file is part of libtmpl.                                             *
+ *                                                                            *
+ *  libtmpl is free software: you can redistribute it and/or modify it        *
+ *  under the terms of the GNU General Public License as published by         *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  libtmpl is distributed in the hope that it will be useful,                *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with libtmpl.  If not, see <https://www.gnu.org/licenses/>.         *
+ ******************************************************************************
+ *  Purpose:                                                                  *
+ *      Provides an example of using polynomial addition in Z[x] without any  *
+ *      error checks. This is the bare minimum needed for a computation. It   *
+ *      is best to have error checks, since malloc can fail. The result is:   *
+ *          (1 + x) + (1 + x) = 2 + 2x                                        *
+ ******************************************************************************
+ *  Author:     Ryan Maguire, Dartmouth College                               *
+ *  Date:       June 19, 2021                                                 *
+ ******************************************************************************/
+
+/*  Polynomials are found here, as well as all polynomial functions.          */
+#include <libtmpl/include/tmpl_polynomial.h>
+
+/*  The stderr FILE pointer is found here.                                    */
+#include <stdio.h>
+
+/*  And free is found here.                                                   */
+#include <stdlib.h>
+
+/*  Function for testing the use of polynomial addition in Z[x].              */
+int main(void)
+{
+    /*  Declare pointers to polynomials, and char pointers for strings.       */
+    tmpl_PolynomialZ *P, *sum;
+    char *Pstr, *sumstr;
+
+    /*  Create polynomials for P and Q with all coefficients set to zero.     */
+    P = tmpl_Create_Zero_PolynomialZ(1);
+
+    /*  We'll set P to the polynomial 1 + x.                                  */
+    P->coeffs[0] = 1L;
+    P->coeffs[1] = 1L;
+
+    /*  Create an empty polynomial to store the sum in.                       */
+    sum = tmpl_Create_Empty_PolynomialZ();
+    
+    /*  Compute the sum of P with itself, and store it in sum.                */
+    tmpl_PolynomialZ_Add(P, P, sum);
+
+    /*  Get the string form of the polynomials.                               */
+    Pstr = tmpl_PolynomialZ_Get_String(P);
+    sumstr = tmpl_PolynomialZ_Get_String(sum);
+
+    /*  Print the result.                                                     */
+    fprintf(stderr, "(%s) + (%s) = %s\n", Pstr, Pstr, sumstr);
+
+    /*  Free everything.                                                      */
+    free(sumstr);
+    free(Pstr);
+    tmpl_Destroy_PolynomialZ(&P);
+    tmpl_Destroy_PolynomialZ(&sum);
+    return 0;
+}
+/*  End of main.                                                              */
--- src/polynomial/tmpl_add_polynomial_z.c
+++ src/polynomial/tmpl_add_polynomial_z.c
@@ -1,4 +1,85 @@
-
+/******************************************************************************
+ *                                 LICENSE                                    *
+ ******************************************************************************
+ *  This file is part of libtmpl.                                             *
+ *                                                                            *
+ *  libtmpl is free software: you can redistribute it and/or modify it        *
+ *  under the terms of the GNU General Public License as published by         *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  libtmpl is distributed in the hope that it will be useful,                *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with libtmpl.  If not, see <https://www.gnu.org/licenses/>.         *
+ ******************************************************************************
+ *                           tmpl_add_polynomial_z                            *
+ ******************************************************************************
+ *  Purpose:                                                                  *
+ *      Code for adding two elements of Z[x].                                 *
+ ******************************************************************************
+ *                             DEFINED FUNCTIONS                              *
+ ******************************************************************************
+ *  Function Name:                                                            *
+ *      tmpl_PolynomialZ_Add                                                  *
+ *  Purpose:                                                                  *
+ *      Add two polynomials in Z[x].                                          *
+ *  Arguments:                                                                *
+ *      P (tmpl_PolynomialZ *):                                               *
+ *          A pointer to the first polynomial.                                *
+ *      Q (tmpl_PolynomialZ *):                                               *
+ *          A pointer to the second polynomial.                               *
+ *      sum (tmpl_PolynomialZ *):                                             *
+ *          A pointer to the resulting sum of P and Q.                        *
+ *  Output:                                                                   *
+ *      None (void).                                                          *
+ *  Called Functions:                                                         *
+ *      malloc (stdlib.h):                                                    *
+ *          Standard library function for allocating memory.                  *
+ *      realloc (stdlib.h):                                                   *
+ *          Standard library function for re-allocating memory.               *
+ *  Method:                                                                   *
+ *      Add the coefficients term by term.                                    *
+ *  Notes:                                                                    *
+ *      It is assumed none of the three polynomial pointers are NULL. If they *
+ *      are, nothing is done. If malloc fails, the error_occurred Boolean is  *
+ *      set to true. Similarly with realloc.                                  *
+ ******************************************************************************
+ *                               DEPENDENCIES                                 *
+ ******************************************************************************
+ *  1.) tmpl_bool.h:                                                          *
+ *          Header file containing Booleans.                                  *
+ *  2.) tmpl_string.h:                                                        *
+ *          Header file where tmpl_strdup is declared. This is a clone of     *
+ *          strdup function, which is a part of the POSIX C standard, but not *
+ *          part of the C standard library.                                   *
+ *  2.) tmpl_polynomial.h:                                                    *
+ *          Header file containing the definition of polynomials and the      *
+ *          functions prototype.                                              *
+ *  3.) stdlib.h:                                                             *
+ *          C Standard library header file containing malloc and realloc.     *
+ ******************************************************************************
+ *                            A NOTE ON COMMENTS                              *
+ ******************************************************************************
+ *  It is anticipated that many users of this code will have experience in    *
+ *  either Python or IDL, but not C. Many comments are left to explain as     *
+ *  much as possible. Vagueness or unclear code should be reported to:        *
+ *  https://github.com/ryanmaguire/libtmpl/issues                             *
+ ******************************************************************************
+ *                            A FRIENDLY WARNING                              *
+ ******************************************************************************
+ *  This code is compatible with the C89/C90 standard. The setup script that  *
+ *  is used to compile this in make.sh uses gcc and has the                   *
+ *  -pedantic and -std=c89 flags to check for compliance. If you edit this to *
+ *  use C99 features (built-in complex, built-in booleans, C++ style comments *
+ *  and etc.), or GCC extensions, you will need to edit the config script.    *
+ ******************************************************************************
+ *  Author:     Ryan Maguire, Dartmouth College                               *
+ *  Date:       June 19, 2021                                                 *
+ ******************************************************************************/
 
 /*  Booleans found here.                                                      */
 #include <libtmpl/include/tmpl_bool.h>
@@ -9,9 +90,10 @@
 /*  Function prototype is declared here.                                      */
 #include <libtmpl/include/tmpl_polynomial.h>
 
-/*  malloc and calloc are found here.                                         */
+/*  malloc and realloc are found here.                                        */
 #include <stdlib.h>
 
+/*  Function for adding two polynomials in Z[x].                              */
 void
 tmpl_PolynomialZ_Add(tmpl_PolynomialZ *P,
                      tmpl_PolynomialZ *Q,
@@ -68,6 +150,11 @@ tmpl_PolynomialZ_Add(tmpl_PolynomialZ *P,
         return;   
     }
 
+    /*  Similarly if the sum pointer has error_occurred set to true, abort    */
+    if (sum->error_occurred)
+        return;
+
+    /*  Order the polynomials in terms of their degree.                       */
     if (P->degree <= Q->degree)
     {
         first = P;
@@ -82,8 +169,10 @@ tmpl_PolynomialZ_Add(tmpl_PolynomialZ *P,
     /*  If the sum coeffs pointer is NULL, allocate memory for it.            */
     if (sum->coeffs == NULL)
     {
+        /*  The degree of the sum is the degree of the larger of the two      *
+         *  polynomials. That is, deg(P+Q) = max(deg(P), deg(Q)).             */
         sum->degree = second->degree;
-        sum->coeffs = malloc(sizeof(*sum->coeffs) * sum->degree + 1UL);
+        sum->coeffs = malloc(sizeof(*sum->coeffs) * (sum->degree + 1UL));
 
         /*  Check if malloc failed.                                           */
         if (sum->coeffs == NULL)
@@ -103,7 +192,7 @@ tmpl_PolynomialZ_Add(tmpl_PolynomialZ *P,
     else if (sum->degree != second->degree)
     {
         sum->degree = second->degree;
-        tmp = realloc(sum->coeffs, sizeof(*sum->coeffs) * sum->degree + 1UL);
+        tmp = realloc(sum->coeffs, sizeof(*sum->coeffs) * (sum->degree + 1UL));
 
         /*  Check if realloc failed.                                          */
         if (tmp == NULL)
@@ -116,14 +205,22 @@ tmpl_PolynomialZ_Add(tmpl_PolynomialZ *P,
             );
             return;
         }
-        sum->coeffs = tmp;
+
+        /*  If realloc did not fail, have the coeffs pointer set to tmp.      */
+        else
+            sum->coeffs = tmp;
     }
 
+    /*  For zero up to the degree of the smaller polynomial, polynomial       *
+     *  addition is done component-wise.                                      */
     for (n = 0UL; n <= first->degree; ++n)
         sum->coeffs[n] = first->coeffs[n] + second->coeffs[n];
 
+    /*  For the rest of the coefficients, the sum is simply the coefficient   *
+     *  of the larger degree polynomial.                                      */
     for (n = first->degree + 1UL; n <= second->degree; ++n)
         sum->coeffs[n] = second->coeffs[n];
 
     return;
 }
+/*  End of tmpl_PolynomialZ_Add.                                              */
