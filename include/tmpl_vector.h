0d198b83bcfe843ab5f9c3eb329c11581592a7c6
Ryan <ryan_maguire@student.uml.edu>
2021-06-23 17:38:13 -0400

    Reorganizing stuff.

--- include/tmpl_vector.h
+++ include/tmpl_vector.h
@@ -49,6 +49,14 @@
 #define TMPL_VECTOR_H
 
 #include <libtmpl/include/tmpl_bool.h>
+#include <libtmpl/include/tmpl_complex.h>
+
+typedef struct tmpl_ComplexDoubleVector {
+    tmpl_ComplexDouble *data;
+    unsigned long length;
+    tmpl_Bool error_occurred;
+    char *error_message;
+} tmpl_ComplexDoubleVector;
 
 /******************************************************************************
  *                          Real Valued Vectors                               *
