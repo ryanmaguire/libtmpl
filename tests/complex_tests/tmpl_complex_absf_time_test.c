11cea58ae6fc021cc6710832a0238d3f85f772f7
Ryan Maguire <Ryan.J.Maguire.GR@dartmouth.edu>
2021-08-25 12:08:40 -0400

    Fixing complex tests.

--- tests/complex_tests/tmpl_complex_absf_time_test.c
+++ tests/complex_tests/tmpl_complex_absf_time_test.c
@@ -17,18 +17,27 @@
  *  along with libtmpl.  If not, see <https://www.gnu.org/licenses/>.         *
  ******************************************************************************/
 
+/*  tmpl complex functions found here.                                        */
 #include <libtmpl/include/tmpl_complex.h>
+
+/*  C99 complex functions here.                                               */
 #include <complex.h>
+
+/*  malloc and free are here.                                                 */
 #include <stdlib.h>
+
+/*  printf here.                                                              */
 #include <stdio.h>
+
+/*  Time data types and functions here.                                       */
 #include <time.h>
 
 /*  Routine for testing tmpl_CFloat_Abs.                                      */
 int main(void)
 {
-    float **y0, **y1;
-    tmpl_ComplexFloat **z0;
-    complex float **z1;
+    float *y0, *y1;
+    tmpl_ComplexFloat *z0;
+    complex float *z1;
 
     const unsigned int N = 10000U;
     const float start = -100.0F;
@@ -39,17 +48,43 @@ int main(void)
     clock_t t1, t2;
     float z_x, z_y, max_rel, max_abs, temp;
 
-    y0 = malloc(sizeof(*y0) * N);
-    y1 = malloc(sizeof(*y1) * N);
-    z0 = malloc(sizeof(*z0) * N);
-    z1 = malloc(sizeof(*z1) * N);
+    /*  Allocate memory and check that malloc did not fail.                   */
+    y0 = malloc(sizeof(*y0) * N * N);
 
-    for (x = 0U; x < N; ++x)
+    if (y0 == NULL)
     {
-        y0[x] = malloc(sizeof(*y0[x]) * N);
-        y1[x] = malloc(sizeof(*y1[x]) * N);
-        z0[x] = malloc(sizeof(*z0[x]) * N);
-        z1[x] = malloc(sizeof(*z1[x]) * N);
+        puts("malloc failed and returned NULL. Aborting.");
+        return -1;
+    }
+
+    y1 = malloc(sizeof(*y1) * N * N);
+
+    if (y1 == NULL)
+    {
+        puts("malloc failed and returned NULL. Aborting.");
+        free(y0);
+        return -1;
+    }
+
+    z0 = malloc(sizeof(*z0) * N * N);
+
+    if (z0 == NULL)
+    {
+        puts("malloc failed and returned NULL. Aborting.");
+        free(y0);
+        free(y1);
+        return -1;
+    }
+
+    z1 = malloc(sizeof(*z1) * N * N);
+
+    if (z1 == NULL)
+    {
+        puts("malloc failed and returned NULL. Aborting.");
+        free(y0);
+        free(y1);
+        free(z0);
+        return -1;
     }
 
     for (x = 0U; x < N; ++x)
@@ -58,8 +93,8 @@ int main(void)
         {
             z_x = (float)x*ds + start;
             z_y = (float)y*ds + start;
-            z0[x][y] = tmpl_CFloat_Rect(z_x, z_y);
-            z1[x][y] = z_x + _Complex_I*z_y;
+            z0[x + y*N] = tmpl_CFloat_Rect(z_x, z_y);
+            z1[x + y*N] = z_x + _Complex_I*z_y;
         }
     }
 
@@ -67,14 +102,14 @@ int main(void)
     t1 = clock();
     for (x = 0U; x < N; ++x)
         for (y = 0U; y < N; ++y)
-            y0[x][y] = tmpl_CFloat_Abs(z0[x][y]);
+            y0[x + y*N] = tmpl_CFloat_Abs(z0[x + y*N]);
     t2 = clock();
     printf("libtmpl: %f\n", (double)(t2-t1)/CLOCKS_PER_SEC);
 
     t1 = clock();
     for (x = 0U; x < N; ++x)
         for (y = 0U; y < N; ++y)
-            y1[x][y] = cabsf(z1[x][y]);
+            y1[x + y*N] = cabsf(z1[x + y*N]);
     t2 = clock();
     printf("c99:     %f\n", (double)(t2-t1)/CLOCKS_PER_SEC);
 
@@ -84,14 +119,14 @@ int main(void)
     {
         for (y = 0U; y < N; ++y)
         {
-            temp = y0[x][y] - y1[x][y];
+            temp = y0[x + y*N] - y1[x + y*N];
             if (temp < 0.0F)
                 temp = -temp;
 
             if (max_abs < temp)
                 max_abs = temp;
 
-            temp = (y0[x][y] - y1[x][y])/y1[x][y];
+            temp = (y0[x + y*N] - y1[x + y*N])/y1[x + y*N];
             if (temp < 0.0F)
                 temp = -temp;
 
@@ -100,17 +135,10 @@ int main(void)
         }
     }
 
-    printf("Max Abs Error: %.8f\n", (double)max_abs);
-    printf("Max Rel Error: %.8f\n", (double)max_rel);
+    printf("Max Abs Error: %.8e\n", (double)max_abs);
+    printf("Max Rel Error: %.8e\n", (double)max_rel);
 
-    for (x = 0U; x < N; ++x)
-    {
-        free(y0[x]);
-        free(y1[x]);
-        free(z0[x]);
-        free(z1[x]);
-    }
-
+    /*  Free all of the memory allocated.                                     */
     free(y0);
     free(y1);
     free(z0);
