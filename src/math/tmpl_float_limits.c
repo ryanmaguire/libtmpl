066bab6bacfa33ec8227555ae91ddc9d8cb4830f
Ryan Maguire <Ryan.J.Maguire.GR@dartmouth.edu>
2021-09-08 22:17:19 -0400

    Found typo.

--- src/math/tmpl_float_limits.c
+++ src/math/tmpl_float_limits.c
@@ -13,4 +13,4 @@ tmpl_Max_LDouble_Base_E = LDBL_MAX_10_EXP * 2.3025850929940456840179910L;
 const float tmpl_Min_Float_Base_E = FLT_MIN_10_EXP * 2.302585093F;
 const double tmpl_Min_Double_Base_E = DBL_MIN_10_EXP  * 2.30258509299404568;
 const long double
-tmpl_Min_LDouble_Base_E = LDBL_MIN_10_EXP * .3025850929940456840179910L;
+tmpl_Min_LDouble_Base_E = LDBL_MIN_10_EXP * 2.3025850929940456840179910L;
