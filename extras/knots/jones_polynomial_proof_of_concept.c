c7d1c853aa82690a7fda7a063052e7f78844e860
Ryan Maguire <Ryan.J.Maguire.GR@dartmouth.edu>
2021-10-01 08:26:28 -0400

    Jones' Polynomial stuff.'

---
+++ extras/knots/jones_polynomial_proof_of_concept.c
@@ -0,0 +1,89 @@
+/******************************************************************************
+ *                                 LICENSE                                    *
+ ******************************************************************************
+ *  This file is part of libtmpl.                                             *
+ *                                                                            *
+ *  libtmpl is free software: you can redistribute it and/or modify           *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  libtmpl is distributed in the hope that it will be useful,                *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with libtmpl.  If not, see <https://www.gnu.org/licenses/>.         *
+ ******************************************************************************
+ *  Attempt of a proof of concept of computing the Jones' polynomial from     *
+ *  the Gauss code of a knot.                                                 *
+ ******************************************************************************
+ *  Author: Ryan Maguire                                                      *
+ *  Date:   2021/09/30                                                        *
+ ******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+enum crossing_sign {negative_crossing, positive_crossing};
+enum crossing_type {under_crossing, over_crossing};
+
+struct knot {
+    unsigned int number_of_crossings;
+    enum crossing_sign *sign;
+    enum crossing_type *type;
+    unsigned int *crossing_number;
+};
+
+struct laurent_polynomial {
+    signed int lowest_degree;
+    signed int highest_degree;
+    signed int *coeffs;
+};
+
+static void
+print_poly(struct laurent_polynomial P)
+{
+    signed int n, N;
+
+    if (!P.coeffs)
+    {
+        puts("0");
+        return;
+    }
+
+    N = P.highest_degree - P.lowest_degree + 1;
+
+    printf("%dq^%d", P.coeffs[0], P.lowest_degree);
+
+    for (n = 1; n < N; ++n)
+    {
+        if (P.coeffs[n] == 0)
+            continue;
+        else if (P.coeffs[n] == 1)
+            printf(" + q^%d", P.lowest_degree + n);
+        else if (P.coeffs[n] == -1)
+            printf(" - q^%d", P.lowest_degree + n);
+        else if (P.coeffs[n] > 1)
+            printf(" + %dq^%d", P.coeffs[n], P.lowest_degree + n);
+        else 
+            printf(" - %dq^%d", -P.coeffs[n], P.lowest_degree + n);
+    }
+
+    printf("\n");
+}
+
+int main(void)
+{
+    struct laurent_polynomial P;
+    signed int coeffs[7] = {3, 2, 1, 0, -1, -2, -3};
+    P.lowest_degree = -3;
+    P.highest_degree = 3;
+    P.coeffs = coeffs;
+
+    print_poly(P);
+    print_poly(poly_add(P, P));
+
+    return 0;
+}
