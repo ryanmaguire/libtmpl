7c1586f22294b62246ba91ee9eb9ad9b33b43b56
ryanmaguire <ryan_maguire@student.uml.edu>
2021-10-27 11:41:28 -0400

    Getting rid of Pylint complaints.

--- extras/fractals/mandelbrot_set/tmpl_mandelbrot_set_fancy_coloring.py
+++ extras/fractals/mandelbrot_set/tmpl_mandelbrot_set_fancy_coloring.py
@@ -1,3 +1,4 @@
+"""
 ################################################################################
 #                                  LICENSE                                     #
 ################################################################################
@@ -24,61 +25,70 @@
 #   Author: Ryan Maguire                                                       #
 #   Date:   June 1, 2021.                                                      #
 ################################################################################
+"""
 
 # The natural logarithm function is found here.
-import math;
+import math
 
 # The width and height of the PPM file.
-width = 1200;
-height = 960;
+WIDTH = 1024
+HEIGHT = 1024
 
 # The maximum number of iterations allowed while computing the fractal.
-imax = 100;
+MAX_ITER = 100
 
 # The maximum radius such that points will automatically be consider divergent.
-zmax = 150;
+MAX_RADIUS = 5.0
 
 # The values of the rectangle that will be drawn.
-x_min = -3.0;
-x_max = 1.0;
-y_min = -2.0;
-y_max = 2.0;
+XMIN = -3.0
+XMAX = 1.0
+YMIN = -2.0
+YMAX = 2.0
 
 # Factors for transforming a pixel to the corresponding point in the plane.
-x_factor = (x_max - x_min)/(width - 1.0);
-y_factor = (y_max - y_min)/(height - 1.0);
+XFACTOR = (XMAX - XMIN)/(WIDTH - 1.0)
+YFACTOR = (YMAX - YMIN)/(HEIGHT - 1.0)
 
 # Open the PPM file and give it write permissions.
-fp = open("swipecat_fractal.ppm", "w");
+FP = open("swipecat_fractal.ppm", "w")
 
 # Write the preamble to the PPM file. We'll use P3, which is text based, to
 # ensure that this file renders properly on Windows.
-fp.write("P3\n%d %d\n255\n" % (width, height));
+FP.write("P3\n%d %d\n255\n" % (WIDTH, HEIGHT))
 
-for y in range(height):
-    z_y = y_max - y_factor*y;
-    for x in range(width):
-        z_x = x_min + x_factor*x;
-        c = complex(z_x, z_y);
-        z = 0.0;
-        backgnd = 0.0;
-        for its in range(imax):
-            z = z*z + c;
-            if abs(z.real) >= zmax:
-                backgnd = math.log(math.log(abs(z.real) + 1.0) * 0.33333333333);
-                backgnd = math.log(abs(its - backgnd)) * 0.3076923076923077;
-                break;
+# Loop over the y-axis.
+for y in range(HEIGHT):
+    z_y = YMAX - YFACTOR*y
 
-        val = max(0.0, 1.0 - abs(1.0 - backgnd));
-        if (backgnd <= 1.0):
-            r = int(255.0 * val**4);
-            g = int(255.0 * val**2.5);
-            b = int(255.0 * val);
+    # Loop over the x-axis.
+    for x in range(WIDTH):
+        z_x = XMIN + XFACTOR*x
+        c = complex(z_x, z_y)
+        z = 0.0
+        backgnd = 0.0
+
+        # Iterate over f(z) = z^2 + c and check for divergence.
+        for its in range(MAX_ITER):
+            z = z*z + c
+
+            # If we've diverged, break and set the background color.
+            if abs(z.real) >= MAX_RADIUS:
+                backgnd = math.log(math.log(abs(z.real) + 1.0) * 0.33333333333)
+                backgnd = math.log(abs(its - backgnd)) * 0.3076923076923077
+                break
+
+        # Value used for coloring the set.
+        val = max(0.0, 1.0 - abs(1.0 - backgnd))
+        if backgnd <= 1.0:
+            r = int(255.0 * val**4)
+            g = int(255.0 * val**2.5)
+            b = int(255.0 * val)
         else:
-            r = int(255.0 * val);
-            g = int(255.0 * val**1.5);
-            b = int(255.0 * val**3);
+            r = int(255.0 * val)
+            g = int(255.0 * val**1.5)
+            b = int(255.0 * val**3)
 
-        fp.write("%u %u %u\n" % (r, g, b));
-fp.close();
-
+        # Color the current pixel of the PPM file.
+        FP.write("%u %u %u\n" % (r, g, b))
+FP.close()
