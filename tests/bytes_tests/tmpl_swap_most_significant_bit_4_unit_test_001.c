668149f442824f27d82a29c601481b4972b8d7d3
ryanmaguire <rmaguire@protonmail.com>
2022-01-25 13:03:09 -0500

    More bytes tests.

---
+++ tests/bytes_tests/tmpl_swap_most_significant_bit_4_unit_test_001.c
@@ -0,0 +1,54 @@
+/******************************************************************************
+ *                                 LICENSE                                    *
+ ******************************************************************************
+ *  This file is part of libtmpl.                                             *
+ *                                                                            *
+ *  libtmpl is free software: you can redistribute it and/or modify it        *
+ *  under the terms of the GNU General Public License as published by         *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  libtmpl is distributed in the hope that it will be useful,                *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with libtmpl.  If not, see <https://www.gnu.org/licenses/>.         *
+ ******************************************************************************
+ *  Author:     Ryan Maguire                                                  *
+ *  Date:       January 25, 2022                                              *
+ ******************************************************************************/
+
+/*  Needed for the puts function.                                             */
+#include <stdio.h>
+
+/*  tmpl_Swap_Most_Significant_Bit_4 found here.                              */
+#include <libtmpl/include/tmpl_bytes.h>
+
+/*  Function for testing tmpl_Swap_Most_Significant_Bit_4.                    */
+int main(void)
+{
+    /*  Create random char array that will be swapped later.                  */
+    const char c[4] = {0x1A, 0x68, 0x33, 0x3E};
+
+    /*  char array that will be swapped.                                      */
+    char x[4];
+    x[0] = c[0];
+    x[1] = c[1];
+    x[2] = c[2];
+    x[3] = c[3];
+
+    /*  Swap the variables.                                                   */
+    tmpl_Swap_Most_Significant_Bit_4(x);
+
+    /*  Check if it worked.                                                   */
+    if ((x[0] != c[3]) || (x[1] != c[2]) || (x[2] != c[1]) || (x[3] != c[0]))
+        puts("FAIL");
+    else
+        puts("PASS");
+
+    return 0;
+}
+/*  End of main.                                                              */
+
