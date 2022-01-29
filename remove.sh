a4a2e44c8d10d361b7e3b656f35d3add4b6be0e6
ryanmaguire <rmaguire@protonmail.com>
2022-01-29 09:56:55 -0500

    Adding license.

--- remove.sh
+++ remove.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+################################################################################
+#                                  LICENSE                                     #
+################################################################################
+#   This file is part of libtmpl.                                              #
+#                                                                              #
+#   libtmpl is free software: you can redistribute it and/or modify            #
+#   it under the terms of the GNU General Public License as published by       #
+#   the Free Software Foundation, either version 3 of the License, or          #
+#   (at your option) any later version.                                        #
+#                                                                              #
+#   libtmpl is distributed in the hope that it will be useful,                 #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of             #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              #
+#   GNU General Public License for more details.                               #
+#                                                                              #
+#   You should have received a copy of the GNU General Public License          #
+#   along with libtmpl.  If not, see <https://www.gnu.org/licenses/>.          #
+################################################################################
+#   Author:     Ryan Maguire                                                   #
+#   Date:       March 10, 2021                                                 #
+################################################################################
+
 # This script is for removing libtmpl from your system.
 SONAME="libtmpl.so"
 SODIR="/usr/local/lib"
@@ -8,7 +30,7 @@ END_HEADER=include/tmpl_endianness.h
 
 echo "Removing libtmpl:"
 echo "    Clearing older files..."
-rm -f *.so *.o
+rm -f *.so *.o *.obj *.lib
 
 echo "    Removing endianness header file if it exists..."
 if [ -e "$END_HEADER" ]; then
