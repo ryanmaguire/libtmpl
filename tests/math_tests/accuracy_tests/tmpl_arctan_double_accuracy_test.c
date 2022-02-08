53a051ac22e17623b4c76ad1f8ca827a32529e4b
ryanmaguire <rmaguire@protonmail.com>
2022-02-08 16:15:50 -0500

    Updating tests and Windows script.

--- tests/math_tests/accuracy_tests/tmpl_arctan_double_accuracy_test.c
+++ tests/math_tests/accuracy_tests/tmpl_arctan_double_accuracy_test.c
@@ -16,58 +16,10 @@
  *  You should have received a copy of the GNU General Public License         *
  *  along with libtmpl.  If not, see <https://www.gnu.org/licenses/>.         *
  ******************************************************************************/
+#include "tmpl_math_accuracy_tests.h"
 
-/*  tmpl_Double_Arctan function is here.                                      */
-#include <libtmpl/include/tmpl_math.h>
-
-/*  printf here.                                                              */
-#include <stdio.h>
-
-/*  atan found here.                                                          */
-#include <math.h>
-
-/*  DBL_MAX found here.                                                       */
-#include <float.h>
-
-/*  Function for testing the speed and accuracy of tmpl_Double_Arctan.        */
 int main(void)
 {
-    double max_abs, max_rel, temp, x, y0, y1;
-    unsigned long int n;
-
-    const double start = -0.1*DBL_MAX;
-    const double end = 0.1*DBL_MAX;
-    const unsigned long int N = 1E8;
-    const double dx = (end - start) / (double)N;
-
-    printf("Functions: tmpl_Double_Arctan vs atan\n\n");
-    printf("Start:     %.16e\n", start);
-    printf("End:       %.16e\n", end);
-    printf("Number:    %lu\n", N);
-    printf("Increment: %.16e\n\n", dx);
-
-    max_abs = 0.0;
-    max_rel = 0.0;
-    x = start;
-    for (n = 0UL; n < N; ++n)
-    {
-        y0 = tmpl_Double_Arctan(x);
-        y1 = atan(x);
-
-        temp = fabs(y0 - y1);
-        if (max_abs < temp)
-            max_abs = temp;
-
-        temp = fabs((y0 - y1) / y1);
-        if (max_rel < temp)
-            max_rel = temp;
-
-        x += dx;
-    }
-
-    printf("Max Abs Error: %.24e\n", max_abs);
-    printf("Max Rel Error: %.24e\n", max_rel);
-    return 0;
+    RUN_TEST(double, -1.0E7, 1.0E7, 10000000000UL, tmpl_Double_Arctan, atan);
 }
-/*  End of main.                                                              */
 
--- tests/math_tests/accuracy_tests/tmpl_math_accuracy_tests.h
+++ tests/math_tests/accuracy_tests/tmpl_math_accuracy_tests.h
@@ -18,7 +18,6 @@
  ******************************************************************************/
 
 #include <libtmpl/include/tmpl_math.h>
-#include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
 #include <time.h>
@@ -30,7 +29,7 @@ do {                                                                           \
     long double rms_rel = 0.0L;                                                \
     long double rms_abs = 0.0L;                                                \
     long double temp;                                                          \
-    type *x, *y0, *y1;                                                         \
+    type x, y0, y1;                                                            \
     unsigned long int n;                                                       \
     clock_t t1, t2;                                                            \
                                                                                \
@@ -39,65 +38,25 @@ do {                                                                           \
     const unsigned long int N = (unsigned long int)samples;                    \
     const type dx = (end - start) / (type)N;                                   \
                                                                                \
-    x = malloc(sizeof(*x) * N);                                                \
-                                                                               \
-    if (!x)                                                                    \
-    {                                                                          \
-        puts("malloc failed and returned NULL for x. Aborting.");              \
-        return -1;                                                             \
-    }                                                                          \
-                                                                               \
-    y0 = malloc(sizeof(*y0) * N);                                              \
-                                                                               \
-    if (!y0)                                                                   \
-    {                                                                          \
-        puts("malloc failed and returned NULL for y0. Aborting.");             \
-        free(x);                                                               \
-        return -1;                                                             \
-    }                                                                          \
-                                                                               \
-    y1 = malloc(sizeof(*y1) * N);                                              \
-                                                                               \
-    if (!y1)                                                                   \
-    {                                                                          \
-        puts("malloc failed and returned NULL for y1. Aborting.");             \
-        free(x);                                                               \
-        free(y0);                                                              \
-        return -1;                                                             \
-    }                                                                          \
-                                                                               \
     printf(#f0 " vs. " #f1 "\n");                                              \
     printf("start:   %.16Le\n", (long double)start);                           \
     printf("end:     %.16Le\n", (long double)end);                             \
     printf("samples: %lu\n", N);                                               \
     printf("dx:      %.16Le\n", (long double)dx);                              \
                                                                                \
-    x[0] = start;                                                              \
-    for (n = 1UL; n < N; ++n)                                                  \
-        x[n] = x[n-1] + dx;                                                    \
-                                                                               \
-    t1 = clock();                                                              \
-    for (n = 0UL; n < N; ++n)                                                  \
-        y0[n] = f0(x[n]);                                                      \
-    t2 = clock();                                                              \
-    printf("libtmpl: %f seconds\n", (double)(t2-t1)/CLOCKS_PER_SEC);           \
-                                                                               \
-    t1 = clock();                                                              \
-    for (n = 0UL; n < N; ++n)                                                  \
-        y1[n] = f1(x[n]);                                                      \
-    t2 = clock();                                                              \
-    printf("C:       %f seconds\n", (double)(t2-t1)/CLOCKS_PER_SEC);           \
-                                                                               \
+    x = start;                                                                 \
     for (n = 0UL; n < N; ++n)                                                  \
     {                                                                          \
-        temp = fabsl((long double)(y0[n] - y1[n]));                            \
+        y0 = f0(x);                                                            \
+        y1 = f1(x);                                                            \
+        temp = fabsl((long double)(y0 - y1));                                  \
         rms_abs += temp*temp;                                                  \
         if (max_abs < temp)                                                    \
             max_abs = temp;                                                    \
                                                                                \
-        temp = fabsl((long double)((y0[n] - y1[n]) / y1[n]));                  \
+        temp = fabsl((long double)((y0 - y1) / y1));                           \
                                                                                \
-        if (y1[n] != 0)                                                        \
+        if (y1 != 0)                                                           \
         {                                                                      \
             rms_rel += temp*temp;                                              \
                                                                                \
@@ -112,8 +71,5 @@ do {                                                                           \
     printf("max rel error: %.16Le\n", max_rel);                                \
     printf("rms abs error: %.16Le\n", rms_abs);                                \
     printf("rms rel error: %.16Le\n", rms_rel);                                \
-    free(x);                                                                   \
-    free(y0);                                                                  \
-    free(y1);                                                                  \
 } while (0)
 
