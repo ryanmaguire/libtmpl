ec36be73f0b3ad1faf36690327f94a7e02b7f3d5
ryanmaguire <rmaguire@protonmail.com>
2022-02-12 17:01:58 -0500

    Working on long double log.

--- src/math/tmpl_log_ldouble.c
+++ src/math/tmpl_log_ldouble.c
@@ -1,5 +1,148 @@
+/******************************************************************************
+ *                                 LICENSE                                    *
+ ******************************************************************************
+ *  This file is part of libtmpl.                                             *
+ *                                                                            *
+ *  libtmpl is free software: you can redistribute it and/or modify it        *
+ *  under the terms of the GNU General Public License as published by         *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  libtmpl is distributed in the hope that it will be useful,                *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with libtmpl.  If not, see <https://www.gnu.org/licenses/>.         *
+ ******************************************************************************
+ *                            tmpl_log_ldouble                                *
+ ******************************************************************************
+ *  Purpose:                                                                  *
+ *      Contains code for computing the natural log at long double precision. *
+ ******************************************************************************
+ *                             DEFINED FUNCTIONS                              *
+ ******************************************************************************
+ *  Function Name:                                                            *
+ *      tmpl_LDouble_Log                                                      *
+ *  Purpose:                                                                  *
+ *      Computes log(x) = ln(x) = log_e(x), with e = 2.71828...               *
+ *  Arguments:                                                                *
+ *      x (long double):                                                      *
+ *          A real number.                                                    *
+ *  Output:                                                                   *
+ *      log_x (double):                                                       *
+ *          The natural log of x at long double precision.                    *
+ *  Called Functions:                                                         *
+ *      None if IEEE-754 support is available and libtmpl algorithms have     *
+ *      been requested. log from math.h otherwise.                            *
+ *  Method:                                                                   *
+ *      If IEEE-754 support is available, and if libtmpl algorithms have      *
+ *      been requested, log(x) is computed as follows:                        *
+ *                                                                            *
+ *          Check if x < 0, return Not-A-Number if so.                        *
+ *                                                                            *
+ *          Check if x = 0.0, return -infinity if so.                         *
+ *                                                                            *
+ *          Check if x is a subnormal (all exponent bits set to zero) number, *
+ *          normalize by 2^52 if so, and then pass to the main part of the    *
+ *          algorithm.                                                        *
+ *                                                                            *
+ *          For values not in the range 0.99 < x < 1.0, compute log(x) as     *
+ *          follows:                                                          *
+ *                                                                            *
+ *              log(x) = log(1.m * 2^b)                                       *
+ *                     = log(1.m) + log(2^b)                                  *
+ *                     = log(1.m) + b*log(2)                                  *
+ *                     = log(u) + b*log(2)      with u = 1.m                  *
+ *                     = log(ut/t) + b*log(2)   with t = 1 + k/128 for some k.*
+ *                     = log(u/t) + log(t) + b*log(2)                         *
+ *                                                                            *
+ *          Precompute log(t) in a table. Precompute 1/t in a table so that   *
+ *          u/t can be computed as u * (1/t). Also precompute log(2). The     *
+ *          value k is chosen to be the largest value such that               *
+ *          t = 1 + k/128 <= u. This value k can be obtained directly from the*
+ *          mantissa. By looking at the most significant 7 bits of the        *
+ *          mantissa, the value k is simply these 7 bits read in binary.      *
+ *                                                                            *
+ *          The value s = u/t is such that 1 <= s < 1 + 1/128. Compute log(s) *
+ *          via the following sum:                                            *
+ *                                                                            *
+ *                        inf                                                 *
+ *                        ----                                                *
+ *              log(s) =  \      2     2n+1                                   *
+ *                        /    ------ A             A = (s - 1) / (s + 1)     *
+ *                        ---- 2n + 1                                         *
+ *                        n = 0                                               *
+ *                                                                            *
+ *          A polynomial with the first few terms is then used. The           *
+ *          standard Taylor series polynomial for ln(1 + x) with x small has  *
+ *          poor convergence, roughly on the order of 1/N where N is the      *
+ *          number of terms. This alternative sum in is terms of the square   *
+ *          of a small value, and has much better convergence.                *
+ *                                                                            *
+ *          For values slightly less than 1, the computation of (s-1) / (s+1) *
+ *          leads to large relative error since log(1) = 1. We can achieve    *
+ *          better relative error using the standard Taylor series to several *
+ *          terms. This is slower than the series above, but more accurate in *
+ *          this range. That is, for 0.99 < x < 1.0 we use:                   *
+ *                                                                            *
+ *                        inf                                                 *
+ *                        ----                                                *
+ *              log(x) =  \    -1   n                                         *
+ *                        /    --- s             s = 1 - x                    *
+ *                        ----  n                                             *
+ *                        n = 1                                               *
+ *                                                                            *
+ *                                                                            *
+ *          For double-double implementations, we compute log(x) as follows.  *
+ *                                                                            *
+ *              log(x) = log(x_hi + x_low)                                    *
+ *                     = log(x * x_hi / x_hi)                                 *
+ *                     = log(x / x_hi) + log(x_hi)                            *
+ *                     = log(1 - 1 + x/x_hi) + log(x_hi)                      *
+ *                     = log(1 + (x - x_hi)/x_hi) + log(x_hi)                 *
+ *                     = log(1 + x_low / x_hi) + log(x_hi)                    *
+ *                                                                            *
+ *          x_low / x_hi is small, so log (1 + x_low / x_hi) can be well      *
+ *          approximated by the first few terms of the Taylor series for      *
+ *          log(1 + t). log(x_hi) is computed in the same way as a normal     *
+ *          long double (see above).                                          *
+ *                                                                            *
+ *      If the user has not requested libtmpl algorithms, or if IEEE-754      *
+ *      support is not available (highly unlikely), then #include <math.h>    *
+ *      is called and tmpl_Double_Log returns logl(x) from math.h             *
+ ******************************************************************************
+ *                               DEPENDENCIES                                 *
+ ******************************************************************************
+ *  1.) tmpl_math.h:                                                          *
+ *          Header file containing tmpl_IEEE754_Double typedef and the        *
+ *          function prototype.                                               *
+ *  2.) math.h:                                                               *
+ *          Only included if TMPL_HAS_IEEE754_LDOUBLE == 0 or if              *
+ *          TMPL_USE_MATH_ALGORITHMS == 0. If either of these is true, then   *
+ *          tmpl_LDouble_Log is identical to the logl function.               *
+ ******************************************************************************
+ *                            A NOTE ON COMMENTS                              *
+ ******************************************************************************
+ *  It is anticipated that many users of this code will have experience in    *
+ *  either Python or IDL, but not C. Many comments are left to explain as     *
+ *  much as possible. Vagueness or unclear code should be reported to:        *
+ *  https://github.com/ryanmaguire/libtmpl/issues                             *
+ ******************************************************************************
+ *                            A FRIENDLY WARNING                              *
+ ******************************************************************************
+ *  This code is compatible with the C89/C90 standard. The setup script that  *
+ *  is used to compile this in make.sh uses gcc and has the                   *
+ *  -pedantic and -std=c89 flags to check for compliance. If you edit this to *
+ *  use C99 features (built-in complex, built-in booleans, C++ style comments *
+ *  and etc.), or GCC extensions, you will need to edit the config script.    *
+ ******************************************************************************
+ *  Author:     Ryan Maguire                                                  *
+ *  Date:       February 12, 2022                                             *
+ ******************************************************************************/
 
-
+/*  Function prototype found here.                                            */
 #include <libtmpl/include/tmpl_math.h>
 
 /*  We can only implement this function if IEEE754 support is available. Also *
@@ -418,7 +561,7 @@ long double tmpl_LDouble_Log(long double x)
 
 #if TMPL_LDOUBLE_ENDIANNESS != TMPL_LDOUBLE_64_BIT_BIG_ENDIAN && \
     TMPL_LDOUBLE_ENDIANNESS != TMPL_LDOUBLE_64_BIT_LITTLE_ENDIAN
-    poly = A*(
+    poly = A * (
         2.0L + A_sq * (
             0.666666666666666666666666666666666666667L + A_sq * (
                 0.4L + A_sq * 0.285714285714285714285714285714285714L
