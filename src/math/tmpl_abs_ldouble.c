5e7f86587a1c227e3e624f18a7ab0450c45b9877
ryanmaguire <rmaguire@protonmail.com>
2022-03-01 10:57:02 -0500

    Updating fabsl.

--- src/math/tmpl_abs_ldouble.c
+++ src/math/tmpl_abs_ldouble.c
@@ -24,7 +24,7 @@
  *                             DEFINED FUNCTIONS                              *
  ******************************************************************************
  *  Function Name:                                                            *
- *      tmpl_LDouble_Abs:                                                     *
+ *      tmpl_LDouble_Abs                                                      *
  *  Purpose:                                                                  *
  *      Computes the absolute value of a real number.                         *
  *                   --                                                       *
@@ -37,6 +37,9 @@
  *  Output:                                                                   *
  *      abs_x (long double):                                                  *
  *          The absolute value of x.                                          *
+ *  Called Functions:                                                         *
+ *      None if IEEE-754 support is available and/or libtmpl algorithms have  *
+ *      been requested. fabsl from math.h if available. fabs otherwise.       *
  *  Method:                                                                   *
  *      If IEEE-754 support is available, simply set the sign bit of the      *
  *      input to 0.                                                           *
@@ -45,10 +48,55 @@
  *      the input is positive or not, returning x for non-negative and -x     *
  *      otherwise.                                                            *
  *                                                                            *
- *      NOTE:                                                                 *
- *          If IEEE-754 is not supported, if the input is NaN one may get     *
- *          +/- NaN (which is still NaN). This is because NaN always          *
- *          evaluates to false when a comparison is made (==, <, >, etc.).    *
+ *  Notes:                                                                    *
+ *      If IEEE-754 is not supported and if the input is NaN one may get      *
+ *      +/- NaN (which is still NaN). This is because NaN always              *
+ *      evaluates to false when a comparison is made (==, <, >, etc.).        *
+ *                                                                            *
+ *      A time and accuracy test against glibc yields the following:          *
+ *                                                                            *
+ *          Using IEEE-754 method:                                            *
+ *                                                                            *
+ *          tmpl_LDouble_Abs vs. fabsl                                        *
+ *          start:   -1.0000000000000000e+06                                  *
+ *          end:     1.0000000000000000e+06                                   *
+ *          samples: 1305052789                                               *
+ *          dx:      1.5325050579237527e-03                                   *
+ *          libtmpl: 10.063154 seconds                                        *
+ *          C:       11.074599 seconds                                        *
+ *          max abs error: 0.0000000000000000e+00                             *
+ *          max rel error: 0.0000000000000000e+00                             *
+ *          rms abs error: 0.0000000000000000e+00                             *
+ *          rms rel error: 0.0000000000000000e+00                             *
+ *                                                                            *
+ *          Using if-then method:                                             *
+ *                                                                            *
+ *          tmpl_LDouble_Abs vs. fabsl                                        *
+ *          start:   -1.0000000000000000e+06                                  *
+ *          end:     1.0000000000000000e+06                                   *
+ *          samples: 1305052789                                               *
+ *          dx:      1.5325050579237527e-03                                   *
+ *          libtmpl: 11.209910 seconds                                        *
+ *          C:       11.037735 seconds                                        *
+ *          max abs error: 0.0000000000000000e+00                             *
+ *          max rel error: 0.0000000000000000e+00                             *
+ *          rms abs error: 0.0000000000000000e+00                             *
+ *          rms rel error: 0.0000000000000000e+00                             *
+ *                                                                            *
+ *      fabs is a built-in function, so I doubt software will be able to get  *
+ *      better performance.                                                   *
+ *                                                                            *
+ *      These tests were performed with the following specs:                  *
+ *                                                                            *
+ *          CPU:  AMD Ryzen 3900 12-core                                      *
+ *          MAX:  4672.0698 MHz                                               *
+ *          MIN:  2200.0000 MHz                                               *
+ *          ARCH: x86_64                                                      *
+ *          RAM:  Ripjaw DDR4-3600 16GBx4                                     *
+ *          MB:   Gigabyte Aorus x570 Elite WiFi                              *
+ *          OS:   Debian 11 (Bullseye) GNU/LINUX                              *
+ *                                                                            *
+ *      Performance will of course vary on different systems.                 *
  ******************************************************************************
  *                               DEPENDENCIES                                 *
  ******************************************************************************
@@ -90,8 +138,11 @@
 /*  Header file where the prototype for the function is defined.              */
 #include <libtmpl/include/tmpl_math.h>
 
+/*  Only implement this if the user requested libtmpl algorithms.             */
+#if defined(TMPL_USE_MATH_ALGORITHMS) && TMPL_USE_MATH_ALGORITHMS == 1
+
 /*  Check for IEEE-754 long double support.                                   */
-#if defined(TMPL_HAS_IEEE754_LDOUBLE) && TMPL_HAS_IEEE754_LDOUBLE == 1
+#if defined(TMPL_HAS_IEEE754_LDOUBLE) && TMPL_HAS_IEEE754_LDOUBLE == 0
 
 /*  Long double precision absolute value function (fabsl equivalent).         */
 long double tmpl_LDouble_Abs(long double x)
@@ -107,6 +158,14 @@ long double tmpl_LDouble_Abs(long double x)
     && TMPL_LDOUBLE_ENDIANNESS != TMPL_LDOUBLE_128_BIT_DOUBLEDOUBLE_BIG_ENDIAN
     w.bits.sign = 0x0U;
 #else
+    /*  For double-double we have x = xhi + xlo. Define                       *
+     *  abs_x = |x| = abs_xhi + abs_xlo. If xhi and xlo have the same sign,   *
+     *  |x| = |xhi| + |xlo| and so abs_xhi = |xhi| and abs_xlo = |xlo|. If    *
+     *  xhi and xlo have different signs, |x| = |xhi| - |xlo| so              *
+     *  abs_xhi = |xhi| and abs_xlo = -|xlo|. In both cases abs_xhi = |xhi|.  *
+     *  The sign of abs_xlo depends on the signs of xhi and xlo. That is,     *
+     *  whether or not they are the same. Indeed, the sign of abs_xlo is the  *
+     *  exlusive or, also called XOR, of the signs of xhi and xlo. Use this.  */
     w.bits.signb = w.bits.signa ^ w.bits.signb;
     w.bits.signa = 0x0U;
 #endif
@@ -114,6 +173,7 @@ long double tmpl_LDouble_Abs(long double x)
     /*  Return the long double part of the union.                             */
     return w.r;
 }
+/*  End of tmpl_LDouble_Abs.                                                  */
 
 #else
 /*  For #if defined(TMPL_HAS_IEEE754_LDOUBLE) && TMPL_HAS_IEEE754_LDOUBLE == 1*/
@@ -124,18 +184,48 @@ long double tmpl_LDouble_Abs(long double x)
 /*  Long double precision absolute value function (fabsl equivalent).         */
 long double tmpl_LDouble_Abs(long double x)
 {
-    /*  Declare necessary variables. C89 requires declarations at the top.    */
-    long double abs_x;
-
     /*  If x is positive return it, otherwise return its negative.            */
     if (x >= 0.0L)
-        abs_x = x;
+        return x;
     else
-        abs_x = -x;
-
-    return abs_x;
+        return -x;
 }
 /*  End of tmpl_LDouble_Abs.                                                  */
 
 #endif
 /*  End #if defined(TMPL_HAS_IEEE754_LDOUBLE) && TMPL_HAS_IEEE754_LDOUBLE == 1*/
+
+#else
+/*  #if defined(TMPL_USE_MATH_ALGORITHMS) && TMPL_USE_MATH_ALGORITHMS == 1    */
+
+/*  math.h provides the fabsl function in C99 and higher.                     */
+#include <math.h>
+
+/*  C99 and higher have fabsl defined. C89 compilers may not. Microsoft has   *
+ *  fabsl but does not define the __STDC_VERSION__ macro by default.          */
+#if (defined(__STDC_VERSION__) && __STDC_VERSION__ >= 199901L) || \
+    (defined(_WIN32) || defined(_WIN64) || defined(_MSC_VER))
+
+/*  Single precision absolute value function (fabsl equivalent).              */
+long double tmpl_Float_Abs(long double x)
+{
+    return fabsl(x);
+}
+/*  End of tmpl_LDouble_Abs.                                                  */
+
+#else
+/*  C89 implementations are not required to provide fabsl.                    */
+
+/*  Single precision absolute value function (fabsl equivalent).              */
+long double tmpl_LDouble_Abs(long double x)
+{
+    double abs_x = fabs((double)x);
+    return (long double)abs_x;
+}
+/*  End of tmpl_LDouble_Abs.                                                  */
+
+#endif
+/*  #if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 199901L              */
+
+#endif
+/*  #if defined(TMPL_USE_MATH_ALGORITHMS) && TMPL_USE_MATH_ALGORITHMS == 1    */
