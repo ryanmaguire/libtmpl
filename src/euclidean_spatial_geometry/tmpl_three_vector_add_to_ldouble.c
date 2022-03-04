042071c2bb8886c24f3e586cdd71d03dc90cf099
ryanmaguire <rmaguire@protonmail.com>
2022-03-04 16:05:16 -0500

    Fixing typo.

--- src/euclidean_spatial_geometry/tmpl_three_vector_add_to_ldouble.c
+++ src/euclidean_spatial_geometry/tmpl_three_vector_add_to_ldouble.c
@@ -1,8 +1,8 @@
 #include <libtmpl/include/tmpl_euclidean_spatial_geometry.h>
 
 void
-tmpl_3DLDouble_Ptr_AddTo(tmpl_ThreeVectorLongDouble *target,
-                         tmpl_ThreeVectorLongDouble *source)
+tmpl_3DLDouble_AddTo(tmpl_ThreeVectorLongDouble *target,
+                     tmpl_ThreeVectorLongDouble *source)
 {
     target->dat[0] += source->dat[0];
     target->dat[1] += source->dat[1];
