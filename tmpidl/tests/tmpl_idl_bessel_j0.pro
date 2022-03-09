4bafbb69b58f6c7eae354a47783f6db36a248536
ryanmaguire <rmaguire@protonmail.com>
2022-03-09 12:15:23 -0500

    Updating IDL stuff.

--- tmpidl/tests/tmpl_idl_bessel_j0.pro
+++ tmpidl/tests/tmpl_idl_bessel_j0.pro
@@ -19,37 +19,18 @@
 ;                             tmpl_idl_bessel_j0                               ;
 ;******************************************************************************;
 ;  Purpose:                                                                    ;
-;      Provide an example of using the C-IDL API to call libtmpl               ;
-;      functions in IDL.                                                       ;
+;      Time and accuracy test for the IDL wrapper of Bessel J0.                ;
 ;******************************************************************************;
-;  Author:     Ryan Maguire, Wellesley College                                 ;
-;  Date:       December 3, 2020                                                ;
+;  Author:     Ryan Maguire                                                    ;
+;  Date:       March 9, 2022                                                   ;
 ;******************************************************************************;
 
-; Function for computing the Bessel J0 function via the library libtmpl.
-FUNCTION BESSEL_J0, x
+; IDL wrappers are located here. @ is IDL's equivalent of #include.
+@../tmpidl_special_functions.pro
 
-    ; libtmpl wants a double, so convert if necessary.
-    x = DOUBLE(x)
-
-    ; Get the number of elements in the input array.
-    n = LONG(N_ELEMENTS(x))
-
-    ; Create an empty array to store the values.
-    result = DBLARR(n)
-
-    ; Use 'call_external' to pass the IDL parameters to the C code.
-    s = CALL_EXTERNAL('/usr/local/lib/libtmpidl.so', $
-    		      'tmpidl_Bessel_J0', x, n, result)
-
-    ; The output has been stored in result, so return this.
-    RETURN, result
-END
-
-; A little test for comparing the libtmpl Bessel J0 with
-; IDL's built-in version.
+; A test for comparing the tmpidl Bessel J0 with IDL's built-in version.
 PRO TEST
-    x = DINDGEN(1000000)*0.001
+    x = DINDGEN(10000001) * 0.0001
 
     t1  = SYSTIME(/SECONDS)
     y_c = BESSEL_J0(x)
@@ -59,61 +40,35 @@ PRO TEST
     y_idl = BESELJ(x, 0, /DOUBLE)
     t4    = SYSTIME(/SECONDS)
 
-    PRINT, "C Time:   ", t2-t1
-    PRINT, "IDL Time: ", t4-t3
+    PRINT, "tmpidl Time: ", t2-t1
+    PRINT, "IDL Time:    ", t4-t3
     PRINT, "Ratio:    ", (t4-t3)/(t2-t1)
     PRINT, ""
     PRINT, "Max Errors: ", MAX(ABS(y_c-y_idl))
     PRINT, "RMS Error:  ", SQRT(MEAN((y_c-y_idl)^2))
 END
 
-; Running this on a 2017 iMac 3.4 GHz quad-core intel i5 with Catalini 10.15.17
-; gave the following:
+; Running this on a 2017 iMac 3.4 GHz quad-core intel i5 with Ubuntu-Budgie
+; 20.04 gave the following:
 ;
-;   C Time:      0.0029559135
-;   IDL Time:    0.0062208176
+;   tmpidl Time:    0.42873502
+;   IDL Time:        1.8394210
+;   Ratio:           4.2903448
 ;
-;   Max Errors:   2.2359086e-09
-;   RMS Error:    1.6999667e-10
+;   Max Errors:    1.3093551e-10
+;   RMS Error:     4.8522063e-12
 ;
-; Not bad! More then twice as fast and within double precision.
-; Note, if we run this in a basic time test using C:
+; Running this when libtmpidl was built with OpenMP support gave:
 ;
-;   #include <stdio.h>
-;   #include <stdlib.h>
-;   #include <time.h>
-;   #include <libtmpl/include/tmpl_special_functions.h>
+;   tmpidl Time:    0.19176412
+;   IDL Time:        1.8159111
+;   Ratio:           9.4695039
 ;
-;   int main(void)
-;   {
-;       double *x, *y, dx;
-;       clock_t t1, t2;
-;       int n, N;
+;   Max Errors:    1.3093551e-10
+;   RMS Error:     4.8522063e-12
 ;
-;       N  = 100000;
-;       dx = 0.001;
+; This was performed on a computer with 4 cores.
 ;
-;       x = malloc(sizeof(*x)*N);
-;       y = malloc(sizeof(*y)*N);
-;
-;       for (n=0; n<N; ++n)
-;           x[n] = dx*n;
-;
-;       t1 = clock();
-;       for (n=0; n<N; ++n)
-;           y[n] = tmpl_Double_Bessel_J0(x[n]);
-;       t2 = clock();
-;
-;       printf("Time: %f\n", (double)(t2 - t1)/CLOCKS_PER_SEC);
-;       free(x);
-;       free(y);
-;       return 0;
-;   }
-;
-; Compiling with:
-;   gcc test.c -O3 -o test -ltmpl
-;
-; The output is:
-;   Time: 0.002859
-;
-; Which is barely different from IDL making the call to libtmpl.
+; These tests were run using GDL - GNU Data Language, Version 0.9.9, a Free and
+; Open Source implementation of IDL.
+
---
+++ tmpidl/tmpidl_special_functions.c
@@ -0,0 +1,142 @@
+/******************************************************************************
+ *                                 LICENSE                                    *
+ ******************************************************************************
+ *  This file is part of libtmpl.                                             *
+ *                                                                            *
+ *  libtmpl is free software: you can redistribute it and/or modify it        *
+ *  under the terms of the GNU General Public License as published by         *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  libtmpl is distributed in the hope that it will be useful,                *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with libtmpl.  If not, see <https://www.gnu.org/licenses/>.         *
+ ******************************************************************************
+ *                          tmpidl_special_functions                          *
+ ******************************************************************************
+ *  Purpose:                                                                  *
+ *      Provide the special functions sublibrary of libtmpl for IDL users.    *
+ ******************************************************************************
+ *  Author:     Ryan Maguire                                                  *
+ *  Date:       March 9, 2022                                                 *
+ ******************************************************************************/
+
+/*  All special functions functions are found here.                           */
+#include <libtmpl/include/tmpl_special_functions.h>
+
+extern void tmpidl_Bessel_J0(int argc, void *argv[]);
+extern void tmpidl_Bessel_I0(int argc, void *argv[]);
+extern void tmpidl_Fresnel_Cos(int argc, void *argv[]);
+extern void tmpidl_Fresnel_Sin(int argc, void *argv[]);
+
+/*  IDL wrapper for the Bessel I0 function.                                   */
+void tmpidl_Bessel_I0(int argc, void *argv[])
+{
+    /*  Declare two double pointers, the input and output for IDL.            */
+    double *x, *y;
+
+    /*  Size is the size of the input IDL array.                              */
+    unsigned long n, size;
+
+    /*  We're expecting three inputs, so check this.                          */
+    if (argc != 3)
+        return;
+
+    /*  Get the parameters passed from IDL.                                   */
+    x = (double *)argv[0];
+    y = (double *)argv[2];
+    size = *(unsigned long *)argv[1];
+
+    /*  Loop through each point and compute the function for every value.     */
+#ifdef _OPENMP
+#pragma omp parallel for
+#endif
+    for (n = 0U; n < size; ++n)
+        y[n] = tmpl_Double_Bessel_I0(x[n]);
+}
+/*  End of tmpidl_Bessel_I0.                                                  */
+
+/*  IDL wrapper for the Bessel J0 function.                                   */
+void tmpidl_Bessel_J0(int argc, void *argv[])
+{
+    /*  Declare two double pointers, the input and output for IDL.            */
+    double *x, *y;
+
+    /*  Size is the size of the input IDL array.                              */
+    unsigned long n, size;
+
+    /*  We're expecting three inputs, so check this.                          */
+    if (argc != 3)
+        return;
+
+    /*  Get the parameters passed from IDL.                                   */
+    x = (double *)argv[0];
+    y = (double *)argv[2];
+    size = *(unsigned long *)argv[1];
+
+    /*  Loop through each point and compute the function for every value.     */
+#ifdef _OPENMP
+#pragma omp parallel for
+#endif
+    for (n = 0U; n < size; ++n)
+        y[n] = tmpl_Double_Bessel_J0(x[n]);
+}
+/*  End of tmpidl_Bessel_J0.                                                  */
+
+/*  IDL wrapper for the Fresnel Cosine function.                              */
+void tmpidl_Fresnel_Cos(int argc, void *argv[])
+{
+    /*  Declare two double pointers, the input and output for IDL.            */
+    double *x, *y;
+
+    /*  Size is the size of the input IDL array.                              */
+    unsigned long n, size;
+
+    /*  We're expecting three inputs, so check this.                          */
+    if (argc != 3)
+        return;
+
+    /*  Get the parameters passed from IDL.                                   */
+    x = (double *)argv[0];
+    y = (double *)argv[2];
+    size = *(unsigned long *)argv[1];
+
+    /*  Loop through each point and compute the function for every value.     */
+#ifdef _OPENMP
+#pragma omp parallel for
+#endif
+    for (n = 0U; n < size; ++n)
+        y[n] = tmpl_Double_Fresnel_Cos(x[n]);
+}
+/*  End of tmpidl_Fresnel_Cos.                                                */
+
+/*  IDL wrapper for the Fresnel Sine function.                                */
+void tmpidl_Fresnel_Sin(int argc, void *argv[])
+{
+    /*  Declare two double pointers, the input and output for IDL.            */
+    double *x, *y;
+
+    /*  Size is the size of the input IDL array.                              */
+    unsigned long n, size;
+
+    /*  We're expecting three inputs, so check this.                          */
+    if (argc != 3)
+        return;
+
+    /*  Get the parameters passed from IDL.                                   */
+    x = (double *)argv[0];
+    y = (double *)argv[2];
+    size = *(unsigned long *)argv[1];
+
+    /*  Loop through each point and compute the function for every value.     */
+#ifdef _OPENMP
+#pragma omp parallel for
+#endif
+    for (n = 0U; n < size; ++n)
+        y[n] = tmpl_Double_Fresnel_Sin(x[n]);
+}
+/*  End of tmpidl_Fresnel_Sin.                                                */
