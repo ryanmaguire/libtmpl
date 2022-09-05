cbcc7e1f73cfb3af8b9b0a2b86978f68eb48b3a4
ryanmaguire <rmaguire@protonmail.com>
2022-09-05 13:44:12 -0400

    Updating math header.

--- include/tmpl_math.h
+++ include/tmpl_math.h
@@ -785,6 +785,28 @@ extern const long double tmpl_Min_LDouble_Base_E;
  *  Output:                                                                   *
  *      double abs_x:                                                         *
  *          The absolute value of x, |x|.                                     *
+ *  Source Code:                                                              *
+ *      libtmpl/src/math/tmpl_abs_float.c                                     *
+ *      libtmpl/src/math/tmpl_abs_double.c                                    *
+ *      libtmpl/src/math/tmpl_abs_ldouble.c                                   *
+ *      libtmpl/include/tmpl_math_abs_inline.h (inline version)               *
+ *  Examples:                                                                 *
+ *      libtmpl/examples/math_examples/tmpl_abs_float_example.c               *
+ *      libtmpl/examples/math_examples/tmpl_abs_double_example.c              *
+ *      libtmpl/examples/math_examples/tmpl_abs_ldouble_example.c             *
+ *  Tests:                                                                    *
+ *      libtmpl/tests/math_tests/unit_tests/                                  *
+ *          tmpl_abs_float_unit_test_001.c                                    *
+ *          tmpl_abs_double_unit_test_001.c                                   *
+ *          tmpl_abs_ldouble_unit_test_001.c                                  *
+ *      libtmpl/tests/math_tests/time_tests/                                  *
+ *          tmpl_abs_float_time_test.c                                        *
+ *          tmpl_abs_double_time_test.c                                       *
+ *          tmpl_abs_ldouble_time_test.c                                      *
+ *      libtmpl/tests/math_tests/accuracy_tests/                              *
+ *          tmpl_abs_float_accuracy_test.c                                    *
+ *          tmpl_abs_double_accuracy_test.c                                   *
+ *          tmpl_abs_ldouble_accuracy_test.c                                  *
  ******************************************************************************/
 
 /*  The absolute value function is small enough that a user may want to       *
