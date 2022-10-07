c45358c12e8a8847543927f76936be772c759208
ryanmaguire <rmaguire@protonmail.com>
2022-10-07 13:49:34 -0400

    Added license.

--- src/math/tmpl_arctan_maclaurin_ldouble.c
+++ src/math/tmpl_arctan_maclaurin_ldouble.c
@@ -1,3 +1,73 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is part of libtmpl.                                             *
+ *                                                                            *
+ *  libtmpl is free software: you can redistribute it and/or modify           *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  libtmpl is distributed in the hope that it will be useful,                *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with libtmpl.  If not, see <https://www.gnu.org/licenses/>.         *
+ ******************************************************************************
+ *                        tmpl_arctan_maclaurin_ldouble                       *
+ ******************************************************************************
+ *  Purpose:                                                                  *
+ *      Computes the Maclaurin series of atan(x) at long double precision.    *
+ ******************************************************************************
+ *                             DEFINED FUNCTIONS                              *
+ ******************************************************************************
+ *  Function Name:                                                            *
+ *      tmpl_LDouble_Arctan_Maclaurin                                         *
+ *  Purpose:                                                                  *
+ *      Computes the Maclaurin series of atan(x) for small values x.          *
+ *  Arguments:                                                                *
+ *      x (long double):                                                      *
+ *          A real number.                                                    *
+ *  Output:                                                                   *
+ *      atan_x (long double):                                                 *
+ *          The Maclaurin series of atan(x).                                  *
+ *  Called Functions:                                                         *
+ *      None.                                                                 *
+ *  Method:                                                                   *
+ *      Use Horner's method to evaluate the polynomial.                       *
+ *                                                                            *
+ *                      infty                                                 *
+ *                      -----                                                 *
+ *                      \         (-1)^n                                      *
+ *          atan(x) =   /        -------- * x^{2n+1}                          *
+ *                      -----    (2n + 1)                                     *
+ *                      n = 0                                                 *
+ *                                                                            *
+ *  64-Bit Double Version:                                                    *
+ *      Use the first 9 terms (0 <= n <= 8) of this expansion to compute.     *
+ *  80-Bit Extended / 128-Bit Quadruple / 128-Bit Double-Double / Portable:   *
+ *      Use the first 12 terms (0 <= n <= 11) of this expansion to compute.   *
+ *  Notes:                                                                    *
+ *      Only accurate for small values. For |x| < 1/8 this function is        *
+ *      accurate to quadruple precision (10^-34 relative error) (for the      *
+ *      64-bit version it is accurate to double precision, 10^-16, for        *
+ *      |x| < 0.15). The larger the input is, the worse the error. By the     *
+ *      alternating series theorem, the absolute error is bounded by          *
+ *      (1/25)*|x|^25, or (1/19)*|x|^19 for the 64-bit version.               *
+ ******************************************************************************
+ *                                DEPENDENCIES                                *
+ ******************************************************************************
+ *  1.) tmpl_config.h:                                                        *
+ *          Header file containing TMPL_USE_INLINE macro.                     *
+ *  2.) tmpl_math.h:                                                          *
+ *          Header file with the functions prototype.                         *
+ ******************************************************************************
+ *  Author:     Ryan Maguire                                                  *
+ *  Date:       September 22, 2022                                            *
+ ******************************************************************************/
+
 /*  TMPL_USE_INLINE macro found here.                                         */
 #include <libtmpl/include/tmpl_config.h>
 
@@ -7,18 +77,24 @@
 /*  IEEE-754 data types and macros found here.                                */
 #include <libtmpl/include/tmpl_math.h>
 
+/*  64-bit long double does not need any more precision than 64-bit double.   */
 #if TMPL_LDOUBLE_ENDIANNESS == TMPL_LDOUBLE_64_BIT_LITTLE_ENDIAN || \
     TMPL_LDOUBLE_ENDIANNESS == TMPL_LDOUBLE_64_BIT_BIG_ENDIAN
 
-#define A0 (1.00000000000000000000)
-#define A1 (-3.33333333333329318027E-01)
-#define A2 (1.99999999998764832476E-01)
-#define A3 (-1.42857142725034663711E-01)
-#define A4 (1.11111104054623557880E-01)
-#define A5 (-9.09088713343650656196E-02)
-#define A6 (7.69187620504482999495E-02)
-#define A7 (-6.66107313738753120669E-02)
-#define A8 (5.83357013379057348645E-02)
+/******************************************************************************
+ *                           64-Bit Double Version                            *
+ ******************************************************************************/
+
+/*  Coefficients for the Maclaurin series at double precision.                */
+#define A0 (1.00000000000000000000000000000E+00L)
+#define A1 (-3.33333333333333333333333333333E-01L)
+#define A2 (2.00000000000000000000000000000E-01L)
+#define A3 (-1.42857142857142857142857142857E-01L)
+#define A4 (1.11111111111111111111111111111E-01L)
+#define A5 (-9.09090909090909090909090909090E-02L)
+#define A6 (7.69230769230769230769230769231E-02L)
+#define A7 (-6.66666666666666666666666666667E-02L)
+#define A8 (5.88235294117647058823529411765E-02L)
 
 /*  Maclaurin series for arctan, double precision, to 9 terms.                */
 long double tmpl_LDouble_Arctan_Maclaurin(long double x)
@@ -29,8 +105,9 @@ long double tmpl_LDouble_Arctan_Maclaurin(long double x)
     /*  Use Horner's method to compute the polynomial.                        */
     return x*(A0+x2*(A1+x2*(A2+x2*(A3+x2*(A4+x2*(A5+x2*(A6+x2*(A7+x2*A8))))))));
 }
-/*  End of tmpl_Double_Arctan_Maclaurin.                                      */
+/*  End of tmpl_LDouble_Arctan_Maclaurin.                                     */
 
+/*  Undefine the coefficients in case someone wants to #include this file.    */
 #undef A0
 #undef A1
 #undef A2
@@ -42,8 +119,14 @@ long double tmpl_LDouble_Arctan_Maclaurin(long double x)
 #undef A8
 
 #else
+/*  Else for 64-bit long double version.                                      */
 
-#define A00 (1.00000000000000000000000000000000000E00L)
+/******************************************************************************
+ *   80-Bit Extended / 128-bit Quadruple / 128-bit Double-Double / Portable   *
+ ******************************************************************************/
+
+/*  Coefficients for the Maclaurin series at long double precision.           */
+#define A00 (1.00000000000000000000000000000000000E+00L)
 #define A01 (-3.33333333333333333333333333333333333E-01L)
 #define A02 (2.00000000000000000000000000000000000E-01L)
 #define A03 (-1.42857142857142857142857142857142857E-01L)
@@ -90,6 +173,7 @@ long double tmpl_LDouble_Arctan_Maclaurin(long double x)
 }
 /*  End of tmpl_LDouble_Arctan_Maclaurin.                                     */
 
+/*  Undefine the coefficients in case someone wants to #include this file.    */
 #undef A00
 #undef A01
 #undef A02
@@ -107,4 +191,4 @@ long double tmpl_LDouble_Arctan_Maclaurin(long double x)
 /*  End difference between 64-bit long double and higher precisions.          */
 
 #endif
-/*  End of #if TMPL_USE_INLINE == 1.                                          */
+/*  End of #if TMPL_USE_INLINE != 1.                                          */
