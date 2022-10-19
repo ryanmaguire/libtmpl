9e763211e7eb4d05e778717081819bf382f35458
ryanmaguire <rmaguire@protonmail.com>
2022-10-19 14:07:44 -0400

    Finalizing more inline code.

--- include/math/tmpl_math_cbrt_data_float.h
+++ include/math/tmpl_math_cbrt_data_float.h
@@ -1,3 +1,23 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is part of libtmpl.                                             *
+ *                                                                            *
+ *  libtmpl is free software: you can redistribute it and/or modify           *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  libtmpl is distributed in the hope that it will be useful,                *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with libtmpl.  If not, see <https://www.gnu.org/licenses/>.         *
+ ******************************************************************************/
+
+/*  Include guard to prevent including this file twice.                       */
 #ifndef TMPL_MATH_CBRT_DATA_FLOAT_H
 #define TMPL_MATH_CBRT_DATA_FLOAT_H
 
@@ -69,3 +89,4 @@ static float tmpl_float_cbrt_lookup_table[128] = {
 };
 
 #endif
+/*  End of include guard.                                                     */
