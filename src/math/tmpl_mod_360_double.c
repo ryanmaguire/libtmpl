9d48c46a0958040cf832b693d01bce6ad618d694
ryanmaguire <rmaguire@protonmail.com>
2022-10-28 18:55:03 -0400

    Removed need for table, replaced with expo trick.

--- src/math/tmpl_mod_360_double.c
+++ src/math/tmpl_mod_360_double.c
@@ -4,44 +4,9 @@
 /*  Check for IEEE-754 double.                                                */
 #if TMPL_HAS_IEEE754_DOUBLE == 1
 
-static const double my_360_arr[32] = {
-    360.0 * 0x1.0p0,
-    360.0 * 0x1.0p32,
-    360.0 * 0x1.0p64,
-    360.0 * 0x1.0p96,
-    360.0 * 0x1.0p128,
-    360.0 * 0x1.0p160,
-    360.0 * 0x1.0p192,
-    360.0 * 0x1.0p224,
-    360.0 * 0x1.0p256,
-    360.0 * 0x1.0p288,
-    360.0 * 0x1.0p320,
-    360.0 * 0x1.0p352,
-    360.0 * 0x1.0p384,
-    360.0 * 0x1.0p416,
-    360.0 * 0x1.0p448,
-    360.0 * 0x1.0p480,
-    360.0 * 0x1.0p512,
-    360.0 * 0x1.0p544,
-    360.0 * 0x1.0p576,
-    360.0 * 0x1.0p608,
-    360.0 * 0x1.0p640,
-    360.0 * 0x1.0p672,
-    360.0 * 0x1.0p704,
-    360.0 * 0x1.0p736,
-    360.0 * 0x1.0p768,
-    360.0 * 0x1.0p800,
-    360.0 * 0x1.0p832,
-    360.0 * 0x1.0p864,
-    360.0 * 0x1.0p896,
-    360.0 * 0x1.0p928,
-    360.0 * 0x1.0p960,
-    360.0 * 0x1.0p992,
-};
-
 double tmpl_Double_Mod_360(double x)
 {
-    tmpl_IEEE754_Double w, tmp;
+    tmpl_IEEE754_Double w, tmp, pow2_360, invpow2_360;
     unsigned int n;
 
     /*  Set the double part of the union to the input.                        */
@@ -61,10 +26,19 @@ double tmpl_Double_Mod_360(double x)
     if (w.r < 360.0)
         return x;
 
-    n = (w.bits.expo - TMPL_DOUBLE_UBIAS - 8U)/32U + 1U;
+    pow2_360.r = 3.60E2;
+    invpow2_360.r = 2.777777777777777777777777777777777777778E-03;
+    n = (w.bits.expo - TMPL_DOUBLE_UBIAS - 8U)/32U;
 
-    for (; n != 0U; --n)
-        w.r -= my_360_arr[n-1U]*tmpl_Double_Floor(w.r / my_360_arr[n-1U]);
+    pow2_360.bits.expo += (32*n & 0x7FF);
+    invpow2_360.bits.expo -= (32*n & 0x7FF);
+
+    for (; (n+1U) != 0U; n--)
+    {
+        w.r -= pow2_360.r*tmpl_Double_Floor(w.r * invpow2_360.r);
+        pow2_360.bits.expo -= 32U;
+        invpow2_360.bits.expo += 0x20U;
+    }
 
     if (tmp.bits.sign)
         return -w.r;
