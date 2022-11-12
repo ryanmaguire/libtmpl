ecaadae61494e0fcbbf54587cb243a8ca6cf6b81
ryanmaguire <rmaguire@protonmail.com>
2022-11-12 13:40:33 -0500

    Updating nan routines.

--- src/math/tmpl_nan_float.c
+++ src/math/tmpl_nan_float.c
@@ -33,12 +33,12 @@
 /*  Function prototype and IEEE-754 data types here.                          */
 #include <libtmpl/include/tmpl_math.h>
 
+/*  Check for IEEE-754. This is the easiest way to define NaN.                */
+#if TMPL_HAS_IEEE754_FLOAT == 1
+
 /*  Single-precision Not-A-Number.                                            */
 float tmpl_Float_NaN(void)
 {
-    /*  Check for IEEE-754. This is the easiest way to define NaN.            */
-#if defined(TMPL_HAS_IEEE754_FLOAT) && TMPL_HAS_IEEE754_FLOAT == 1
-
     /*  IEEE-754 declares single precision nan to have 1 for first and last   *
      *  bits of the mantissa, 1 for the all exponents bits, and 0 for the     *
      *  sign. Set the bits to this and then return the resulting float.       */
@@ -52,57 +52,22 @@ float tmpl_Float_NaN(void)
     /*  man1 has 16 bits. 0000000000000001 is 0x1 in hex.                     */
     x.bits.man1 = 0x1U;
     return x.r;
-#else
-/*  For #if defined(TMPL_HAS_IEEE754_FLOAT) && TMPL_HAS_IEEE754_FLOAT == 1    */
+}
+/*  End of tmpl_Float_NaN.                                                    */
 
+#else
+/*  For #if TMPL_HAS_IEEE754_FLOAT == 1.                                      */
+
+/*  Single-precision Not-A-Number.                                            */
+float tmpl_Float_NaN(void)
+{
     /*  glibc sets the following for compilers lacking IEEE-754 support. This *
      *  may result in compiler warnings, and may also result in undefined     *
      *  behavior, but works often in practice.                                */
     float x = 0.0F;
     return x / x;
-#endif
-/*  End #if defined(TMPL_HAS_IEEE754_FLOAT) && TMPL_HAS_IEEE754_FLOAT == 1    */
-
 }
 /*  End of tmpl_Float_NaN.                                                    */
 
-/*  Double-precision Not-A-Number.                                            */
-double tmpl_Double_NaN(void)
-{
-    /*  Check for IEEE-754. This is the easiest way to define NaN.            */
-#if defined(TMPL_HAS_IEEE754_DOUBLE) && TMPL_HAS_IEEE754_DOUBLE == 1
-
-    /*  IEEE-754 declares double precision nan to have 1 for first and last   *
-     *  bits of the mantissa, 1 for the all exponents bits, and 0 for the     *
-     *  sign. Set the bits to this and then return the resulting float.       */
-    tmpl_IEEE754_Double x;
-    x.bits.sign = 0x0U;
-    x.bits.expo = 0x7FFU;
-    x.bits.man0 = 0x8U;
-    x.bits.man1 = 0x0U;
-    x.bits.man2 = 0x0U;
-    x.bits.man3 = 0x1U;
-    return x.r;
-#else
-/*  For #if defined(TMPL_HAS_IEEE754_DOUBLE) && TMPL_HAS_IEEE754_DOUBLE == 1  */
-
-    /*  glibc sets the following for compilers lacking IEEE-754 support. This *
-     *  may result in compiler warnings, and may also result in undefined     *
-     *  behavior, but works often in practice.                                */
-    double x = 0.0;
-    return x / x;
 #endif
-/*  End #if defined(TMPL_HAS_IEEE754_DOUBLE) && TMPL_HAS_IEEE754_DOUBLE == 1  */
-
-}
-/*  End of tmpl_Double_Infinity.                                              */
-
-/*  Long double precision Not-A-Number.                                       */
-long double tmpl_LDouble_NaN(void)
-{
-    /*  Simply cast the double version and return.                            */
-    double x = tmpl_Double_NaN();
-    long double lx = (long double)x;
-    return lx;
-}
-/*  End of tmpl_LDouble_NaN.                                                  */
+/*  End of #if TMPL_HAS_IEEE754_FLOAT == 1.                                   */
