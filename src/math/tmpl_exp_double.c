1014d69960887cb09f4a765b62ae40ecc5ebecd0
ryanmaguire <rmaguire@protonmail.com>
2023-01-30 18:46:09 -0500

    Adding license and algorithm description.

--- src/math/tmpl_exp_double.c
+++ src/math/tmpl_exp_double.c
@@ -1,23 +1,148 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is part of libtmpl.                                             *
+ *                                                                            *
+ *  libtmpl is free software: you can redistribute it and/or modify           *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  libtmpl is distributed in the hope that it will be useful,                *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with libtmpl.  If not, see <https://www.gnu.org/licenses/>.         *
+ ******************************************************************************
+ *                              tmpl_exp_double                               *
+ ******************************************************************************
+ *  Purpose:                                                                  *
+ *      Computes f(x) = e^x at double precision.                              *
+ ******************************************************************************
+ *                             DEFINED FUNCTIONS                              *
+ ******************************************************************************
+ *  Function Name:                                                            *
+ *      tmpl_Double_Exp                                                       *
+ *  Purpose:                                                                  *
+ *      Computes the base e exponential of a real number.                     *
+ *  Arguments:                                                                *
+ *      x (double):                                                           *
+ *          A real number.                                                    *
+ *  Output:                                                                   *
+ *      exp_x (double):                                                       *
+ *          The exponential of x.                                             *
+ *  IEEE-754 Version:                                                         *
+ *      Called Functions:                                                     *
+ *          tmpl_Double_Exp_Maclaurin (tmpl_math.h):                          *
+ *              Computes exp(x) via a Maclaurin series for |x| < 1/16.        *
+ *          tmpl_Double_Exp_Remez (tmpl_math.h):                              *
+ *              Computes exp(x) via a Remez minimax polynomial for |x| < 1/4. *
+ *          tmpl_Double_Exp_Pade (tmpl_math.h):                               *
+ *              Computes exp(x) with a Pade approximant for |x| < 1.          *
+ *          tmpl_Double_Exp_Neg_Kernel (tmpl_math.h):                         *
+ *              Computes exp(x) for negative x with x <= -1.                  *
+ *          tmpl_Double_Exp_Pos_Kernel (tmpl_math.h):                         *
+ *              Computes exp(x) for positive x with x >= 1.                   *
+ *      Method:                                                               *
+ *          For very small x, |x| < 1/16, use a Maclaurin series.             *
+ *                                                                            *
+ *                        infty                                               *
+ *                        -----                                               *
+ *                        \      1   n                                        *
+ *              exp(x) =  /     --- x                                         *
+ *                        -----  n!                                           *
+ *                        n = 0                                               *
+ *                                                                            *
+ *          For slightly larger x, |x| < 1/4, use a Minimax polynomial on     *
+ *          the interval [-1/4, 1/4]. The coefficients were computed using    *
+ *          the Remez exchange algorithm.                                     *
+ *                                                                            *
+ *          For |x| < 1 use a Pade approximant.                               *
+ *                                                                            *
+ *                        a0 + a1 x + ... + an x^n                            *
+ *              exp(x) ~= ------------------------                            *
+ *                         1 + b1 x + ... + bm x^m                            *
+ *                                                                            *
+ *          For |x| > log(DBL_MAX) ~= 709, use the limit.                     *
+ *                                                                            *
+ *              exp(+inf) = +inf                                              *
+ *              exp(-inf) = 0                                                 *
+ *                                                                            *
+ *          For x = +/- NaN, return x. Lastly, for 1 <= |x| <= log(DBL_MAX)   *
+ *          perform the following:                                            *
+ *                                                                            *
+ *              exp(x) = exp(k*ln(2) + r)           k integer, |r| < ln(2)    *
+ *                     = exp(k*ln(2)) * exp(r)                                *
+ *                     = 2^k * exp(r)                                         *
+ *                     = 2^k * exp(n/128 + t)       n integer, |t| < 1/128    *
+ *                     = 2^k * exp(n/128) * exp(t)                            *
+ *                                                                            *
+ *          2^k can be computed by setting the exponent bit to k, exp(n/128)  *
+ *          is computed with a lookup table, and exp(t) is computed using a   *
+ *          minimax polynomial on the interval [-1/128, 1/128]. The           *
+ *          coefficients were again computed using the Remez exchange         *
+ *          algorithm. Note the coefficients for the minimax polynomial for   *
+ *          exp on [-1/128, 1/128] differ from the ones on [-1/4, 1/4].       *
+ *      Error:                                                                *
+ *          Based on 2,244,604,859 samples with -700 < x < 700.               *
+ *              max relative error: 6.0259536545328452e-16                    *
+ *              rms relative error: 9.7044236080734406e-17                    *
+ *              max absolute error: 2.4363285028499998e+288                   *
+ *              rms absolute error: 1.8029986755465275e+286                   *
+ *          Values assume 100% accuracy of glibc. Actual error in glibc is    *
+ *          around 1 ULP (~2 x 10^-16).                                       *
+ *  Portable Version:                                                         *
+ *      Called Functions:                                                     *
+ *          tmpl_Double_Is_NaN (tmpl_math.h):                                 *
+ *              Determines if a real number is NaN.                           *
+ *          tmpl_Double_Is_Inf (tmpl_math.h):                                 *
+ *              Determines if a real number is infinity.                      *
+ *          tmpl_Double_Abs (tmpl_math.h):                                    *
+ *              Computes the absolute value of a real number.                 *
+ *          tmpl_Double_Exp_Maclaurin (tmpl_math.h):                          *
+ *              Computes exp(x) via a Maclaurin series for |x| < 1/16.        *
+ *          tmpl_Double_Exp_Remez (tmpl_math.h):                              *
+ *              Computes exp(x) via a Remez minimax polynomial for |x| < 1/4. *
+ *          tmpl_Double_Exp_Pade (tmpl_math.h):                               *
+ *              Computes exp(x) with a Pade approximant for |x| < 1.          *
+ *          tmpl_Double_Exp_Neg_Kernel (tmpl_math.h):                         *
+ *              Computes exp(x) for negative x with x <= -1.                  *
+ *          tmpl_Double_Exp_Pos_Kernel (tmpl_math.h):                         *
+ *              Computes exp(x) for positive x with x >= 1.                   *
+ *      Method:                                                               *
+ *          Same as IEEE-754 method, but determine the range of x by          *
+ *          computing |x| and comparing this value with 1/16, 1/4, etc.       *
+ *      Error:                                                                *
+ *          Based on 2,244,604,859 samples with -700 < x < 700.               *
+ *              max relative error: 6.0259536545328452e-16                    *
+ *              rms relative error: 9.7044236080734406e-17                    *
+ *              max absolute error: 2.4363285028499998e+288                   *
+ *              rms absolute error: 1.8029986755465275e+286                   *
+ *          Values assume 100% accuracy of glibc. Actual error in glibc is    *
+ *          around 1 ULP (~2 x 10^-16).                                       *
+ ******************************************************************************
+ *                                DEPENDENCIES                                *
+ ******************************************************************************
+ *  1.) tmpl_math.h:                                                          *
+ *          Header file with the functions prototype.                         *
+ ******************************************************************************
+ *  Author:     Ryan Maguire                                                  *
+ *  Date:       January 30, 2023                                              *
+ ******************************************************************************/
+
 /*  Function prototype found here.                                            */
 #include <libtmpl/include/tmpl_math.h>
 
 /*  With IEEE-754 support we can get a bit of a speed boost.                  */
 #if TMPL_HAS_IEEE754_DOUBLE == 1
 
-/*  Coefficients for the numerator of the Remez polynomial.                   */
-#define A00 (+9.9999999999999999988430264938382617864875775206464E-01)
-#define A01 (+1.0000000000000002567155499064945764299110332942888E+00)
-#define A02 (+5.0000000000000011303128565037818364883412745825979E-01)
-#define A03 (+1.6666666666658450641538834111096804338714833792536E-01)
-#define A04 (+4.1666666666648208299845851261960139340509395965848E-02)
-#define A05 (+8.3333333406957231917106078391264123789847252849529E-03)
-#define A06 (+1.3888888900173203766146657738825963558824828302470E-03)
-#define A07 (+1.9841242914202268899999786488562084101590255844480E-04)
-#define A08 (+2.4801555544629532354737090416957032661760944182686E-05)
-#define A09 (+2.7600400233476040774827464572558357184830459675937E-06)
-#define A10 (+2.7599120100813904758992842016718714757809673113599E-07)
+/******************************************************************************
+ *                              IEEE-754 Version                              *
+ ******************************************************************************/
 
-/*  Function for computing e^x at double precision.                           */
+/*  Function for computing e^x at double precision (exp equivalent).          */
 double tmpl_Double_Exp(double x)
 {
     /*  Declare necessary variables. C89 requires this at the top.            */
@@ -42,12 +167,16 @@ double tmpl_Double_Exp(double x)
             return x;
     }
 
-    /*  For |x| < 1/16 the Maclaurin series is accurate to double precision.  */
+    /*  For |x| < 1/4 use one of two polynomial approximations.               */
     if (w.bits.expo < TMPL_DOUBLE_UBIAS - 2U)
     {
+        /*  For very small x, the Maclaurin series uses less terms.           */
         if (w.bits.expo < TMPL_DOUBLE_UBIAS - 4U)
             return tmpl_Double_Exp_Maclaurin(x);
-        return A00+x*(A01+x*(A02+x*(A03+x*(A04+x*(A05+x*(A06+x*(A07+x*(A08+x*(A09+x*A10)))))))));
+
+        /*  Otherwise, for |x| < 1/4 the minimax polynomial is accurate.      */
+        else
+            return tmpl_Double_Exp_Remez(x);
     }
 
     /*  For |x| < 1, the Pade approximant is sufficient and much faster than  *
@@ -74,7 +203,11 @@ double tmpl_Double_Exp(double x)
 #else
 /*  Else for #if TMPL_HAS_IEEE754_DOUBLE == 1.                                */
 
-/*  Function for computing e^x at double precision.                           */
+/******************************************************************************
+ *                              Portable Version                              *
+ ******************************************************************************/
+
+/*  Function for computing e^x at double precision (exp equivalent).          */
 double tmpl_Double_Exp(double x)
 {
     /*  Declare necessary variables. C89 requires this at the top.            */
@@ -93,7 +226,7 @@ double tmpl_Double_Exp(double x)
 
         /*  And the limit as x tends to +infinity is +infinity.               */
         else
-            return tmpl_Double_Abs(x);
+            return x;
     }
 
     /*  Compute |x| to reduce the number of comparisons made. We want to find *
@@ -103,6 +236,10 @@ double tmpl_Double_Exp(double x)
     /*  For |x| < 1/16 the Maclaurin series is accurate to double precision.  */
     if (abs_x < 0.0625)
         return tmpl_Double_Exp_Maclaurin(x);
+
+    /*  For |x| < 1/4 the Remez minimax polynomial is fast and accurate.      */
+    else if (abs_x < 0.25)
+        return tmpl_Double_Exp_Remez(x);
 
     /*  For |x| < 1, the Pade approximant is sufficient and much faster than  *
      *  the kernel functions. Use this.                                       */
