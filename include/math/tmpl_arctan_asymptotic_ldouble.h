7741d0d6cf748d447f41740e85a2bfea8fdaeef5
ryanmaguire <rmaguire@protonmail.com>
2023-01-30 09:42:21 -0500

    Changing 'inline' to 'TMPL_INLINE_DECL'

--- include/math/tmpl_arctan_asymptotic_ldouble.h
+++ include/math/tmpl_arctan_asymptotic_ldouble.h
@@ -106,7 +106,8 @@
 #define A5 (-9.09090909090909090909090909090E-02L)
 
 /*  Long double precision asymptotic expansion for the arctangent function.   */
-inline long double tmpl_LDouble_Arctan_Asymptotic(long double x)
+TMPL_INLINE_DECL
+long double tmpl_LDouble_Arctan_Asymptotic(long double x)
 {
     /*  Declare necessary variables.                                          */
     const long double z = 1.0L / x;
