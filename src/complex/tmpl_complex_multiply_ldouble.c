2e9c6a95062c875be58cbca1e741e93bc4b7ad2c
ryanmaguire <rmaguire@protonmail.com>
2023-02-07 17:46:49 -0500

    Updating complex multiplication. Adding inline option.

--- src/complex/tmpl_complex_multiply_ldouble.c
+++ src/complex/tmpl_complex_multiply_ldouble.c
@@ -1,9 +1,9 @@
 /******************************************************************************
- *                                 LICENSE                                    *
+ *                                  LICENSE                                   *
  ******************************************************************************
  *  This file is part of libtmpl.                                             *
  *                                                                            *
- *  libtmpl is free software: you can redistribute it and/or modify it        *
+ *  libtmpl is free software: you can redistribute it and/or modify           *
  *  it under the terms of the GNU General Public License as published by      *
  *  the Free Software Foundation, either version 3 of the License, or         *
  *  (at your option) any later version.                                       *
@@ -16,20 +16,22 @@
  *  You should have received a copy of the GNU General Public License         *
  *  along with libtmpl.  If not, see <https://www.gnu.org/licenses/>.         *
  ******************************************************************************
- *                          tmpl_complex_multiply                             *
+ *                       tmpl_complex_multiply_ldouble                        *
  ******************************************************************************
  *  Purpose:                                                                  *
  *      Contains the source code for complex multiplication.                  *
  ******************************************************************************
- *                               DEPENDENCIES                                 *
+ *                                DEPENDENCIES                                *
  ******************************************************************************
- *  1.) tmpl_complex.h:                                                       *
+ *  1.) tmpl_config.h:                                                        *
+ *          Header file containing the TMPL_USE_INLINE macro.                 *
+ *  2.) tmpl_complex.h:                                                       *
  *          Header where complex types and function prototypes are defined.   *
  ******************************************************************************
- *  Author:     Ryan Maguire, Dartmouth College                               *
+ *  Author:     Ryan Maguire                                                  *
  *  Date:       February 18, 2021                                             *
  ******************************************************************************
- *                             Revision History                               *
+ *                              Revision History                              *
  ******************************************************************************
  *  2020/11/30: Ryan Maguire                                                  *
  *      Created file (Wellesley College for librssringoccs).                  *
@@ -39,90 +41,67 @@
  *      Edited file for use in libtmpl.                                       *
  ******************************************************************************/
 
+/*  TMPL_USE_INLINE macro found here.                                         */
+#include <libtmpl/include/tmpl_config.h>
+
+/*  This file is only used if inline support is not requested.                */
+#if TMPL_USE_INLINE != 1
+
 /*  Where the prototypes are declared and where complex types are defined.    */
 #include <libtmpl/include/tmpl_complex.h>
+
+/*  Two algorithms are offered. The standard one, and the Gauss-Karatsuba     *
+ *  algorithm for complex multiplication.                                     */
+#ifndef TMPL_COMPLEX_LDOUBLE_MULTIPLY_ALGORITHM
+#define TMPL_COMPLEX_LDOUBLE_MULTIPLY_ALGORITHM 0
+#endif
+
+/*  Check the value of the macro. 0 corresponds to the classic method, and 1  *
+ *  represents the Gauss-Karatsuba algorithm.                                 */
+#if TMPL_COMPLEX_LDOUBLE_MULTIPLY_ALGORITHM == 0
 
 /*  In C99, since _Complex is a built-in data type, given double _Complex z1  *
  *  and double _Complex z2, you can just do z1 * z2. Structs cannot be        *
  *  multiplied so we need a function for computing this.                      */
 
 /*  Single precision complex multiplication.                                  */
-tmpl_ComplexFloat
-tmpl_CFloat_Multiply(tmpl_ComplexFloat z0, tmpl_ComplexFloat z1)
-{
-    /*  Declare necessary variables. C89 requires declarations at the top.    */
-    tmpl_ComplexFloat prod;
-    float real0, real1;
-    float imag0, imag1;
-    float prod_re, prod_im;
-
-    /*  Extract the real and imaginary parts from the inputs.                 */
-    real0 = tmpl_CFloat_Real_Part(z0);
-    real1 = tmpl_CFloat_Real_Part(z1);
-    imag0 = tmpl_CFloat_Imag_Part(z0);
-    imag1 = tmpl_CFloat_Imag_Part(z1);
-
-    /*  The product uses the distributive law in combination with the fact    *
-     *  that i^2 = -1. This gives us the following formulas:                  */
-    prod_re = real0*real1 - imag0*imag1;
-    prod_im = real0*imag1 + imag0*real1;
-
-    /*  Create the output from prod_re and prod_im and return.                */
-    prod = tmpl_CFloat_Rect(prod_re, prod_im);
-    return prod;
-}
-/*  End of tmpl_CFloat_Multiply.                                              */
-
-/*  Double precision complex multiplication.                                  */
-tmpl_ComplexDouble
-tmpl_CDouble_Multiply(tmpl_ComplexDouble z0, tmpl_ComplexDouble z1)
-{
-    /*  Declare necessary variables. C89 requires declarations at the top.    */
-    tmpl_ComplexDouble prod;
-    double real0, real1;
-    double imag0, imag1;
-    double prod_re, prod_im;
-
-    /*  Extract the real and imaginary parts from the inputs.                 */
-    real0 = tmpl_CDouble_Real_Part(z0);
-    real1 = tmpl_CDouble_Real_Part(z1);
-    imag0 = tmpl_CDouble_Imag_Part(z0);
-    imag1 = tmpl_CDouble_Imag_Part(z1);
-
-    /*  The product uses the distributive law in combination with the fact    *
-     *  that i^2 = -1. This gives us the following formulas:                  */
-    prod_re = real0*real1 - imag0*imag1;
-    prod_im = real0*imag1 + imag0*real1;
-
-    /*  Create the output from prod_re and prod_im and return.                */
-    prod = tmpl_CDouble_Rect(prod_re, prod_im);
-    return prod;
-}
-/*  End of tmpl_CDouble_Multiply.                                             */
-
-/*  Long double precision complex multiplication.                             */
 tmpl_ComplexLongDouble
 tmpl_CLDouble_Multiply(tmpl_ComplexLongDouble z0, tmpl_ComplexLongDouble z1)
 {
     /*  Declare necessary variables. C89 requires declarations at the top.    */
     tmpl_ComplexLongDouble prod;
-    long double real0, real1;
-    long double imag0, imag1;
-    long double prod_re, prod_im;
-
-    /*  Extract the real and imaginary parts from the inputs.                 */
-    real0 = tmpl_CLDouble_Real_Part(z0);
-    real1 = tmpl_CLDouble_Real_Part(z1);
-    imag0 = tmpl_CLDouble_Imag_Part(z0);
-    imag1 = tmpl_CLDouble_Imag_Part(z1);
 
     /*  The product uses the distributive law in combination with the fact    *
      *  that i^2 = -1. This gives us the following formulas:                  */
-    prod_re = real0*real1 - imag0*imag1;
-    prod_im = real0*imag1 + imag0*real1;
-
-    /*  Create the output from prod_re and prod_im and return.                */
-    prod = tmpl_CLDouble_Rect(prod_re, prod_im);
+    prod.dat[0] = z0.dat[0]*z1.dat[0] - z0.dat[1]*z1.dat[1];
+    prod.dat[1] = z0.dat[0]*z1.dat[1] + z0.dat[1]*z1.dat[0];
     return prod;
 }
 /*  End of tmpl_CLDouble_Multiply.                                            */
+
+#else
+/*  Else for #if TMPL_COMPLEX_LDOUBLE_MULTIPLY_ALGORITHM == 0.                */
+
+/*  Single precision complex multiplication.                                  */
+tmpl_ComplexLongDouble
+tmpl_CLDouble_Multiply(tmpl_ComplexLongDouble z0, tmpl_ComplexLongDouble z1)
+{
+    /*  Declare necessary variables. C89 requires declarations at the top.    */
+    tmpl_ComplexLongDouble prod;
+
+    /*  The Gauss-Karatsuba algorithm requires 3 multipications, instead of   *
+     *  4, but needs more additions and subtractions.                         */
+    const long double k1 = z1.dat[0] * (z0.dat[0] + z0.dat[1]);
+    const long double k2 = z0.dat[0] * (z1.dat[1] - z1.dat[0]);
+    const long double k3 = z0.dat[1] * (z1.dat[0] + z1.dat[1]);
+    prod.dat[0] = k1 - k3;
+    prod.dat[1] = k1 + k2;
+    return prod;
+}
+/*  End of tmpl_CLDouble_Multiply.                                            */
+
+#endif
+/*  End of #if TMPL_COMPLEX_LDOUBLE_MULTIPLY_ALGORITHM == 0.                  */
+
+#endif
+/*  End of #if TMPL_USE_INLINE != 1.                                          */
