df1b35f88fb43e7e776a38259c828a1cab0c9648
ryanmaguire <rmaguire@protonmail.com>
2023-02-07 10:56:54 -0500

    Adding ConjugateSelf function.

---
+++ src/complex/tmpl_complex_conjugateself_double.c
@@ -0,0 +1,70 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is part of libtmpl.                                             *
+ *                                                                            *
+ *  libtmpl is free software: you can redistribute it and/or modify           *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  libtmpl is distributed in the hope that it will be useful,                *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with libtmpl.  If not, see <https://www.gnu.org/licenses/>.         *
+ ******************************************************************************
+ *                     tmpl_complex_conjugateself_double                      *
+ ******************************************************************************
+ *  Purpose:                                                                  *
+ *      Contains the source code for the complex conjugate.                   *
+ ******************************************************************************
+ *                             DEFINED FUNCTIONS                              *
+ ******************************************************************************
+ *  Function Name:                                                            *
+ *      tmpl_CDouble_ConjugateSelf:                                           *
+ *  Purpose:                                                                  *
+ *      Computes the complex conjugate of a complex number.                   *
+ *                                                                            *
+ *          conj(z) = conj(x + iy) = x - iy                                   *
+ *                                                                            *
+ *  Arguments:                                                                *
+ *      z (tmpl_ComplexDouble *):                                             *
+ *          A pointer to a complex number.                                    *
+ *  Output:                                                                   *
+ *      None (void).                                                          *
+ *  Method:                                                                   *
+ *      Negate the imaginary part of z and return.                            *
+ ******************************************************************************
+ *                                DEPENDENCIES                                *
+ ******************************************************************************
+ *  1.) tmpl_config.h:                                                        *
+ *          Header file where TMPL_USE_INLINE is found.                       *
+ *  2.) tmpl_complex.h:                                                       *
+ *          Header where complex types and function prototypes are defined.   *
+ ******************************************************************************
+ *  Author:     Ryan Maguire                                                  *
+ *  Date:       February 07, 2023                                             *
+ ******************************************************************************/
+
+/*  TMPL_USE_INLINE macro found here.                                         */
+#include <libtmpl/include/tmpl_config.h>
+
+/*  This file is only used if inline support is not requested.                */
+#if TMPL_USE_INLINE != 1
+
+/*  Where the prototypes are declared and where complex types are defined.    */
+#include <libtmpl/include/tmpl_complex.h>
+
+/*  Double precision complex conjugate function.                              */
+void tmpl_CDouble_ConjugateSelf(tmpl_ComplexDouble *z)
+{
+    /*  The complex conjugate of x + iy is x - iy. Negate the imagary part.   */
+    z->dat[1] = -z->dat[1];
+}
+/*  End of tmpl_CDouble_Conjugate.                                            */
+
+#endif
+/*  End of #if TMPL_USE_INLINE != 1.                                          */
