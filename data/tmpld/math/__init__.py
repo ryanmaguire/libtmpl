c94e9ba112cac62ecf02a1d666efdfc4664571f6
ryanmaguire <rmaguire@protonmail.com>
2023-03-14 10:11:53 -0400

    Adding a double factorial function.

---
+++ data/tmpld/math/__init__.py
@@ -0,0 +1,28 @@
+"""
+################################################################################
+#                                   LICENSE                                    #
+################################################################################
+#   This file is part of libtmpl.                                              #
+#                                                                              #
+#   libtmpl is free software: you can redistribute it and/or modify it         #
+#   under the terms of the GNU General Public License as published by          #
+#   the Free Software Foundation, either version 3 of the License, or          #
+#   (at your option) any later version.                                        #
+#                                                                              #
+#   libtmpl is distributed in the hope that it will be useful,                 #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of             #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              #
+#   GNU General Public License for more details.                               #
+#                                                                              #
+#   You should have received a copy of the GNU General Public License          #
+#   along with libtmpl.  If not, see <https://www.gnu.org/licenses/>.          #
+################################################################################
+#   Purpose:                                                                   #
+#       Provide various functions that appear in Taylor series, etc.           #
+################################################################################
+#   Author: Ryan Maguire                                                       #
+#   Date:   March 13, 2023.                                                    #
+################################################################################
+"""
+
+from tmpld.math.double_factorial import double_factorial
