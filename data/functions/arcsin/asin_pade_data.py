7fc6802507f2771c20cf2502661c8966eeaf8c44
ryanmaguire <rmaguire@protonmail.com>
2023-04-17 10:30:34 -0400

    Adding data for arcsin.

---
+++ data/functions/arcsin/asin_pade_data.py
@@ -0,0 +1,37 @@
+"""
+################################################################################
+#                                   LICENSE                                    #
+################################################################################
+#   This file is part of libtmpl.                                              #
+#                                                                              #
+#   libtmpl is free software: you can redistribute it and/or modify it         #
+#   under the terms of the GNU General Public License as published by          #
+#   the Free Software Foundation, either version 3 of the License, or          #
+#   (at your option) any later version.                                        #
+#                                                                              #
+#   libtmpl is distributed in the hope that it will be useful,                 #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of             #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              #
+#   GNU General Public License for more details.                               #
+#                                                                              #
+#   You should have received a copy of the GNU General Public License          #
+#   along with libtmpl.  If not, see <https://www.gnu.org/licenses/>.          #
+################################################################################
+#   Purpose:                                                                   #
+#       Computes the Pade coefficients for the inverse sine function, asin.    #
+################################################################################
+#   Author: Ryan Maguire                                                       #
+#   Date:   January 8, 2023.                                                   #
+################################################################################
+"""
+
+# Algorithm for computing Pade coefficients given here.
+import tmpld.pade
+
+# Arcsin Taylor coefficients given here.
+import arcsin
+
+# Compute and print the coefficients for the Pade approximant of asin(x).
+A = [arcsin.taylor(n) for n in range(11)]
+(P, Q) = tmpld.pade.pade(A, 4, 5)
+tmpld.pade.print_coeffs(P, Q)
