a41894dca5a800340743e77a06d91b2acec22f1b
ryanmaguire <rmaguire@protonmail.com>
2023-04-18 11:12:26 -0400

    Moving implementation to include/math/.

--- src/math/tmpl_abs_float.c
+++ src/math/tmpl_abs_float.c
@@ -19,152 +19,20 @@
  *                               tmpl_abs_float                               *
  ******************************************************************************
  *  Purpose:                                                                  *
- *      Computes f(x) = |x| at single precision.                              *
- ******************************************************************************
- *                             DEFINED FUNCTIONS                              *
- ******************************************************************************
- *  Function Name:                                                            *
- *      tmpl_Float_Abs                                                        *
- *  Purpose:                                                                  *
- *      Computes the absolute value of a real number.                         *
- *                   --                                                       *
- *                  |   x,  x >= 0                                            *
- *          |x|  =  |  -x,  else                                              *
- *                   --                                                       *
- *  Arguments:                                                                *
- *      x (float):                                                            *
- *          A real number, the argument for |x|.                              *
- *  Output:                                                                   *
- *      abs_x (float):                                                        *
- *          The absolute value of x.                                          *
- *  IEEE-754 Version:                                                         *
- *      Called Functions:                                                     *
- *          None.                                                             *
- *      Method:                                                               *
- *          Set the sign bit to 0. A 32-bit float is represented by:          *
- *                                                                            *
- *              s eeeeeeee xxxxxxxxxxxxxxxxxxxxxxx                            *
- *              - -------- -----------------------                            *
- *           sign exponent        mantissa                                    *
- *                                                                            *
- *          The absolute value can be computed by setting s to 0. Note, this  *
- *          method will work for NaN and inf, and the output will be          *
- *          "positive" NaN and positive infinity, respectively.               *
- *      Error:                                                                *
- *          Based on 2,103,916,953 samples with -10^6 < x < 10^6.             *
- *              max relative error: 0.0                                       *
- *              rms relative error: 0.0                                       *
- *              max absolute error: 0.0                                       *
- *              rms absolute error: 0.0                                       *
- *  Portable Version:                                                         *
- *      Called Functions:                                                     *
- *          None.                                                             *
- *      Method:                                                               *
- *          Use an if-then statement to check if the input is positive,       *
- *          returning x for non-negative and -x otherwise.                    *
- *      Error:                                                                *
- *          Based on 2,103,916,953 samples with -10^6 < x < 10^6.             *
- *              max relative error: 0.0                                       *
- *              rms relative error: 0.0                                       *
- *              max absolute error: 0.0                                       *
- *              rms absolute error: 0.0                                       *
- *  Notes:                                                                    *
- *      If IEEE-754 is not supported and if the input is NaN one may get      *
- *      +/- NaN (which is still NaN). This is because NaN always              *
- *      evaluates to false when a comparison is made (==, <, >, etc.).        *
- ******************************************************************************
- *                                DEPENDENCIES                                *
- ******************************************************************************
- *  1.) tmpl_config.h:                                                        *
- *          Header file containing TMPL_USE_INLINE macro.                     *
- *  2.) tmpl_math.h:                                                          *
- *          Header file with the functions prototype.                         *
+ *      Computes f(x) = |x|. See include/math/tmpl_abs_float.h for details.   *
  ******************************************************************************
  *  Author:     Ryan Maguire                                                  *
  *  Date:       February 16, 2021                                             *
- ******************************************************************************
- *                              Revision History                              *
- ******************************************************************************
- *  2020/11/01: Ryan Maguire                                                  *
- *      Created file (Wellesley College for librssringoccs).                  *
- *  2020/12/08: Ryan Maguire                                                  *
- *      Frozen for v1.3 of librssringoccs.                                    *
- *  2021/02/16: Ryan Maguire                                                  *
- *      Copied file from rss_ringoccs.                                        *
- *  2021/02/24: Ryan Maguire                                                  *
- *      Added IEEE 754 code for computing the absolute value function.        *
- *  2022/03/01: Ryan Maguire                                                  *
- *      Added check for TMPL_USE_MATH_ALGORITHMS macro. This function will    *
- *      simply use fabs from math.h if TMPL_USE_MATH_ALGORITHMS is not 1.     *
- *  2022/09/05: Ryan Maguire                                                  *
- *      Added inline version.                                                 *
- *  2022/09/13: Ryan Maguire                                                  *
- *      Removed math.h version (fabsf alias). This version is now a macro for *
- *      the fabsf function in tmpl_math.h (only if TMPL_USE_MATH_ALGORITHMS   *
- *      is set to zero).                                                      *
  ******************************************************************************/
 
-/*  Location of the TMPL_USE_INLINE macro.                                    */
+/*  Location of the TMPL_USE_INLINE and TMPL_USE_MATH_ALGORITHMS macros.      */
 #include <libtmpl/include/tmpl_config.h>
 
-/*  This file is only compiled if inline support is not requested.            */
-#if TMPL_USE_INLINE != 1
+/*  Only used if libtmpl algorithms are requested and inline support is not.  */
+#if TMPL_USE_INLINE != 1 && TMPL_USE_MATH_ALGORITHMS == 1
 
-/*  Only implement this if the user requested libtmpl algorithms.             */
-#if TMPL_USE_MATH_ALGORITHMS == 1
-
-/*  Header file where the prototype for the function is defined.              */
-#include <libtmpl/include/tmpl_math.h>
-
-/*  Check for IEEE-754 support.                                               */
-#if TMPL_HAS_IEEE754_FLOAT == 1
-
-/******************************************************************************
- *                              IEEE-754 Version                              *
- ******************************************************************************/
-
-/*  Single precision absolute value function (fabsf equivalent).              */
-float tmpl_Float_Abs(float x)
-{
-    /*  Declare necessary variables. C89 requires declarations at the top.    */
-    tmpl_IEEE754_Float w;
-
-    /*  Set the float part of the word to the input x.                        */
-    w.r = x;
-
-    /*  Set the sign bit to zero, indicating positive.                        */
-    w.bits.sign = 0x0U;
-
-    /*  Return the float part of the union.                                   */
-    return w.r;
-}
-/*  End of tmpl_Float_Abs.                                                    */
-
-#else
-/*  Else for #if TMPL_HAS_IEEE754_FLOAT == 1.                                 */
-
-/******************************************************************************
- *                              Portable Version                              *
- ******************************************************************************/
-
-/*  Lacking IEEE-754 support, an if-then statement works and is portable.     */
-
-/*  Single precision absolute value function (fabsf equivalent).              */
-float tmpl_Float_Abs(float x)
-{
-    /*  If x is positive return it, otherwise return its negative.            */
-    if (x >= 0.0F)
-        return x;
-    else
-        return -x;
-}
-/*  End of tmpl_Float_Abs.                                                    */
+/*  Implemented in include/math/tmpl_abs_float.h.                             */
+#include "../../include/math/tmpl_abs_float.h"
 
 #endif
-/*  End of #if TMPL_HAS_IEEE754_FLOAT == 1.                                   */
-
-#endif
-/*  End of #if TMPL_USE_MATH_ALGORITHMS == 1.                                 */
-
-#endif
-/*  End of #if TMPL_USE_INLINE != 1.                                          */
+/*  End of #if TMPL_USE_INLINE != 1 && TMPL_USE_MATH_ALGORITHMS == 1.         */
