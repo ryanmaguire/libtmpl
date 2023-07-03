0fbb9fbfa6c9c64de7e02afb5107e42352c1b3db
ryanmaguire <rmaguire@protonmail.com>
2023-07-03 18:01:38 -0400

    Updating interpolation routines.

--- include/tmpl_interpolate.h
+++ include/tmpl_interpolate.h
@@ -1,9 +1,9 @@
 /******************************************************************************
- *                                 LICENSE                                    *
+ *                                  LICENSE                                   *
  ******************************************************************************
  *  This file is part of libtmpl.                                             *
  *                                                                            *
- *  libtmpl is free software: you can redistribute it and/or modify it        *
+ *  libtmpl is free software: you can redistribute it and/or modify           *
  *  it under the terms of the GNU General Public License as published by      *
  *  the Free Software Foundation, either version 3 of the License, or         *
  *  (at your option) any later version.                                       *
@@ -16,27 +16,12 @@
  *  You should have received a copy of the GNU General Public License         *
  *  along with libtmpl.  If not, see <https://www.gnu.org/licenses/>.         *
  ******************************************************************************
- *                             tmpl_interpolate                               *
+ *                              tmpl_interpolate                              *
  ******************************************************************************
  *  Purpose:                                                                  *
  *      Provide routines for interpolating data.                              *
  ******************************************************************************
- *                            A NOTE ON COMMENTS                              *
- ******************************************************************************
- *  It is anticipated that many users of this code will have experience in    *
- *  either Python or IDL, but not C. Many comments are left to explain as     *
- *  much as possible. Vagueness or unclear code should be reported to:        *
- *  https://github.com/ryanmaguire/libtmpl/issues                             *
- ******************************************************************************
- *                            A FRIENDLY WARNING                              *
- ******************************************************************************
- *  This code is compatible with the C89/C90 standard. The setup script that  *
- *  is used to compile this in make.sh uses gcc and has the                   *
- *  -pedantic and -std=c89 flags to check for compliance. If you edit this to *
- *  use C99 features (built-in complex, built-in booleans, C++ style comments *
- *  and etc.), or GCC extensions, you will need to edit the config script.    *
- ******************************************************************************
- *  Author:     Ryan Maguire, Wellesley College                               *
+ *  Author:     Ryan Maguire                                                  *
  *  Date:       December 30, 2020                                             *
  ******************************************************************************/
 
@@ -44,43 +29,59 @@
 #ifndef TMPL_INTERPOLATE_H
 #define TMPL_INTERPOLATE_H
 
+/*  size_t typedef provided here.                                             */
+#include <stddef.h>
+
 /******************************************************************************
  *  Function:                                                                 *
  *      tmpl_Double_Sorted_Interp1d                                           *
  *  Purpose:                                                                  *
  *      Interpolate values of (x, y) to (x_new, y_new).                       *
  *  Arguments:                                                                *
- *      x (double *):                                                         *
- *          A sorted array of real numbers that are strictly                  *
- *          monotonically increasing.                                         *
- *      y (double *):                                                         *
+ *      x (const double *):                                                   *
+ *          A sorted array of real numbers that are monotonically increasing. *
+ *      y (const double *):                                                   *
  *          The data points corresponding to x.                               *
- *      N (unsigned long):                                                    *
+ *      len (size_t):                                                         *
  *          The number of elements of x and y.                                *
- *      x_new (double *):                                                     *
- *          The new data points. Must lie between min(x) and max(x).          *
+ *      x_new (const double *):                                               *
+ *          The new data points, sorted in increasing order.                  *
  *      y_new (double *):                                                     *
  *          The interpolated data corresponding to x_new.                     *
- *      N_new (unsigned long):                                                *
+ *      len_new (size_t):                                                     *
  *          The number of elements of x_new and y_new.                        *
  *  Output:                                                                   *
  *      None (void).                                                          *
- *  NOTES:                                                                    *
- *      It is assumed the pointer x has it's data sorted and strictly         *
- *      monotonically increasing. That is, x[n] < x[n+1] for all valid n.     *
+ *  Notes:                                                                    *
+ *      1.) Both x and x_new are assumed to be sorted in increasing order.    *
+ *                                                                            *
+ *      2.) For values x_new[m] < x[0], this function sets y_new[m] = y[0].   *
+ *          Similarly for values x_new[m] > x[len-1] the set value is         *
+ *          y_new[m] = y[len-1]. No extrapolating is done.                    *
  ******************************************************************************/
 extern void
-tmpl_Float_Sorted_Interp1d(float *x, float *y, unsigned long N,
-                           float *x_new, float *y_new, unsigned long N_new);
+tmpl_Float_Sorted_Interp1d(const float *x,
+                           const float *y,
+                           size_t len,
+                           const float *x_new,
+                           float *y_new,
+                           size_t len_new);
 
 extern void
-tmpl_Double_Sorted_Interp1d(double *x, double *y, unsigned long N,
-                            double *x_new, double *y_new, unsigned long N_new);
+tmpl_Double_Sorted_Interp1d(const double *x,
+                            const double *y,
+                            size_t len,
+                            const double *x_new,
+                            double *y_new,
+                            size_t len_new);
 
 extern void
-tmpl_LDouble_Sorted_Interp1d(long double *x, long double *y, unsigned long N,
-                             long double *x_new, long double *y_new,
-                             unsigned long N_new);
+tmpl_LDouble_Sorted_Interp1d(const long double *x,
+                             const long double *y,
+                             size_t len,
+                             const long double *x_new,
+                             long double *y_new,
+                             size_t len_new);
 
 #endif
 /*  End of include guard.                                                     */
