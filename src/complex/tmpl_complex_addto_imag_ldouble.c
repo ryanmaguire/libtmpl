1d171ceb648cb69081c72474aa9b71b6af9993e5
ryanmaguire <rmaguire@protonmail.com>
2023-07-10 09:47:46 -0400

    Reducing duplicate code for inline functions.

--- src/complex/tmpl_complex_addto_imag_ldouble.c
+++ src/complex/tmpl_complex_addto_imag_ldouble.c
@@ -19,61 +19,21 @@
  *                      tmpl_complex_addto_imag_ldouble                       *
  ******************************************************************************
  *  Purpose:                                                                  *
- *      Contains the source code for complex addition.                        *
- ******************************************************************************
- *                             DEFINED FUNCTIONS                              *
- ******************************************************************************
- *  Function Name:                                                            *
- *      tmpl_CLDouble_AddTo_Imag                                              *
- *  Purpose:                                                                  *
- *      Adds a complex number with an imaginary one:                          *
- *                                                                            *
- *          z + iy = (a + ib) + iy = a + i(b + y)                             *
- *                                                                            *
- *  Arguments:                                                                *
- *      z (tmpl_ComplexLongDouble *):                                         *
- *          A pointer to a complex number. The sum is stored here.            *
- *      y (long double):                                                      *
- *          An imaginary number.                                              *
- *  Output:                                                                   *
- *      None (void).                                                          *
- *  Called Functions:                                                         *
- *      None.                                                                 *
- *  Method:                                                                   *
- *      Compute the component-wise sum and store it in the first pointer.     *
- ******************************************************************************
- *                                DEPENDENCIES                                *
- ******************************************************************************
- *  1.) tmpl_config.h:                                                        *
- *          Contains the TMPL_USE_INLINE macro.                               *
- *  2.) tmpl_complex.h:                                                       *
- *          Header where complex types and function prototypes are defined.   *
+ *      Complex addition. Implemented in                                      *
+ *      include/complex/tmpl_complex_addto_imag_ldouble.h. See for details.   *
  ******************************************************************************
  *  Author:     Ryan Maguire                                                  *
  *  Date:       February 6, 2023                                              *
  ******************************************************************************/
 
-/*  TMPL_USE_INLINE found here.                                               */
+/*  Location of the TMPL_USE_INLINE macro.                                    */
 #include <libtmpl/include/tmpl_config.h>
 
-/*  This file is only used if inline support is not requested.                */
+/*  Only used if inline support is not requested.                             */
 #if TMPL_USE_INLINE != 1
 
-/*  Where the prototypes are declared and where complex types are defined.    */
-#include <libtmpl/include/tmpl_complex.h>
-
-/*  In C99, since _Complex is a built-in data type, given double _Complex z   *
- *  and double y, you can just do z += _Complex_I*y. With C89 we use structs  *
- *  to define complex numbers. Structs cannot be added, so we need a function *
- *  for computing the sum.                                                    */
-
-/*  Double precision complex addition. Equivalent of += operation.            */
-void tmpl_CLDouble_AddTo_Imag(tmpl_ComplexLongDouble *z, long double y)
-{
-    /*  Add the value to the imaginary part of the complex number.            */
-    z->dat[1] += y;
-}
-/*  End of tmpl_CLDouble_AddTo_Imag.                                          */
+/*  Implemented in include/complex/tmpl_complex_addto_imag_ldouble.h.         */
+#include "../../include/complex/tmpl_complex_addto_imag_ldouble.h"
 
 #endif
 /*  End of #if TMPL_USE_INLINE != 1.                                          */
--- src/complex/tmpl_complex_argument_float.c
+++ src/complex/tmpl_complex_argument_float.c
@@ -19,85 +19,20 @@
  *                        tmpl_complex_argument_float                         *
  ******************************************************************************
  *  Purpose:                                                                  *
- *      Contains the source code for the complex argument.                    *
- ******************************************************************************
- *                             DEFINED FUNCTIONS                              *
- ******************************************************************************
- *  Function Name:                                                            *
- *      tmpl_CFloat_Argument                                                  *
- *  Purpose:                                                                  *
- *      Computes the argument of a complex number:                            *
- *                                                                            *
- *          arg(z) =  arg(r * exp(i theta)) = theta                           *
- *                                                                            *
- *      Equivalently:                                                         *
- *                                                                            *
- *          arg(z) = arg(x + iy) = arctan(y, x)                               *
- *                                                                            *
- *  Arguments:                                                                *
- *      z (tmpl_ComplexFloat):                                                *
- *          A complex number.                                                 *
- *  Output:                                                                   *
- *      arg (float):                                                          *
- *          The argument of z.                                                *
- *  Method:                                                                   *
- *      Extract the real and imaginary parts and return atan2(y, x).          *
- *  Notes:                                                                    *
- *      Because the atan2 function is used, there is a discontinuity along    *
- *      the negative real axis. That is, the argument returns a real value    *
- *      in the interval (-pi, pi] (+pi is inclusive).                         *
- *                                                                            *
- *      This file is a fork of the code I wrote for rss_rinoccs.              *
- *      librssringoccs is also released under GPL3.                           *
- ******************************************************************************
- *                                DEPENDENCIES                                *
- ******************************************************************************
- *  1.) tmpl_config.h:                                                        *
- *          Header file containing the TMPL_USE_INLINE macro.                 *
- *  2.) tmpl_math.h:                                                          *
- *          Header file containing 2D arctan.                                 *
- *  3.) tmpl_complex.h:                                                       *
- *          Header where complex types and function prototypes are defined.   *
+ *      Complex argument. See include/complex/tmpl_complex_argument_float.h.  *
  ******************************************************************************
  *  Author:     Ryan Maguire                                                  *
  *  Date:       February 16, 2021                                             *
- ******************************************************************************
- *                              Revision History                              *
- ******************************************************************************
- *  2020/11/30: Ryan Maguire                                                  *
- *      Created file (Wellesley College for librssringoccs).                  *
- *  2020/12/02 (Ryan Maguire):                                                *
- *      Frozen for v1.3.                                                      *
- *  2021/02/16: Ryan Maguire                                                  *
- *      Copied from rss_ringoccs.                                             *
- *      Made compatible with the rest of libtmpl.                             *
- *      Soft freeze for alpha release of libtmpl.                             *
- *  2021/06/26: Ryan Maguire                                                  *
- *      Hard freeze for alpha release of libtmpl. Reviewed code and comments. *
- *      No more changes unless something breaks.                              *
- *  2023/02/06: Ryan Maguire                                                  *
- *      Moved float and long double to their own files. Inlined routines.     *
  ******************************************************************************/
 
-/*  TMPL_USE_INLINE macro found here.                                         */
+/*  Location of the TMPL_USE_INLINE macro.                                    */
 #include <libtmpl/include/tmpl_config.h>
 
-/*  This file is only used if inline support is not requested.                */
+/*  Only used if inline support is not requested.                             */
 #if TMPL_USE_INLINE != 1
 
-/*  Arctan2 function found here.                                              */
-#include <libtmpl/include/tmpl_math.h>
-
-/*  Where the prototypes are declared and where complex types are defined.    */
-#include <libtmpl/include/tmpl_complex.h>
-
-/*  Single precision complex argument function (cargf equivalent).            */
-float tmpl_CFloat_Argument(tmpl_ComplexFloat z)
-{
-    /*  Compute the argument using arctan and return.                         */
-    return tmpl_Float_Arctan2(z.dat[1], z.dat[0]);
-}
-/*  End of tmpl_CFloat_Argument.                                              */
+/*  Implemented in include/complex/tmpl_complex_argument_float.h.             */
+#include "../../include/complex/tmpl_complex_argument_float.h"
 
 #endif
 /*  End of #if TMPL_USE_INLINE != 1.                                          */
