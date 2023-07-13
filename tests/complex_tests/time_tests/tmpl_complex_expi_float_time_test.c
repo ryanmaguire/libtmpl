37c7fbd769fd7b130d7ae8621715f520a625e410
ryanmaguire <rmaguire@protonmail.com>
2023-07-13 11:19:05 -0400

    Adding more tests.

---
+++ tests/complex_tests/time_tests/tmpl_complex_expi_float_time_test.c
@@ -0,0 +1,24 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is part of libtmpl.                                             *
+ *                                                                            *
+ *  libtmpl is free software: you can redistribute it and/or modify           *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  libtmpl is distributed in the hope that it will be useful,                *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with libtmpl.  If not, see <https://www.gnu.org/licenses/>.         *
+ ******************************************************************************/
+#include "tmpl_complex_time_tests.h"
+static inline complex float func(float t)
+{
+    return cosf(t) + _Complex_I * sinf(t);
+}
+TEST9(float, tmpl_ComplexFloat, complex float, tmpl_CFloat_Expi, func)
