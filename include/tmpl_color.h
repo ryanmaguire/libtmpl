64dc1d479685bd2a59249a30ad759a7a5bb86a08
ryanmaguire <rmaguire@protonmail.com>
2023-12-19 09:45:54 -0500

    Adding license and basic info for color header.

--- include/tmpl_color.h
+++ include/tmpl_color.h
@@ -1,6 +1,35 @@
-/*
- *  TODO: Add license info to all files.
- */
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is part of libtmpl.                                             *
+ *                                                                            *
+ *  libtmpl is free software: you can redistribute it and/or modify           *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  libtmpl is distributed in the hope that it will be useful,                *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with libtmpl.  If not, see <https://www.gnu.org/licenses/>.         *
+ ******************************************************************************
+ *                                 tmpl_color                                 *
+ ******************************************************************************
+ *  Purpose:                                                                  *
+ *      Provides tools for working with RGB colors in various formats (24-bit,*
+ *      30-bit, 48-bit, and real), as well as RGB colors with alpha           *
+ *      compositing. These can be used to create PPM and SVG images.          *
+ ******************************************************************************
+ *                                DEPENDENCIES                                *
+ ******************************************************************************
+ *    None.                                                                   *
+ ******************************************************************************
+ *  Author:     Ryan Maguire                                                  *
+ *  Date:       December 18, 2023                                             *
+ ******************************************************************************/
 
 /*  Include guard to prevent including this file twice.                       */
 #ifndef TMPL_COLOR_H
