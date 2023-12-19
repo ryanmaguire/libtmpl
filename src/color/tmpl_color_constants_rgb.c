62670ab66e82e060e6e4000688a78e8345e3ea08
ryanmaguire <rmaguire@protonmail.com>
2023-12-19 09:26:02 -0500

    Adding license and comments to rgb files.

--- src/color/tmpl_color_constants_rgb.c
+++ src/color/tmpl_color_constants_rgb.c
@@ -1,9 +1,49 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is part of libtmpl.                                             *
+ *                                                                            *
+ *  libtmpl is free software: you can redistribute it and/or modify           *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  libtmpl is distributed in the hope that it will be useful,                *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with libtmpl.  If not, see <https://www.gnu.org/licenses/>.         *
+ ******************************************************************************
+ *                          tmpl_color_constants_rgb                          *
+ ******************************************************************************
+ *  Purpose:                                                                  *
+ *      Provides commonly used colors in real RGB format.                     *
+ ******************************************************************************
+ *                                DEPENDENCIES                                *
+ ******************************************************************************
+ *  1.) tmpl_color.h:                                                         *
+ *          Header file where the colors are declared.                        *
+ ******************************************************************************
+ *  Author:     Ryan Maguire                                                  *
+ *  Date:       December 18, 2023                                             *
+ ******************************************************************************/
+
+/*  Header file where color structs are typedef'd and colors declared.        */
 #include <libtmpl/include/tmpl_color.h>
+
+/*  Black and white, set to the minimum and maximum possible values in each   *
+ *  color channel. For real-valued RGB this is 0 and 1, respectively.         */
 const tmpl_RGB tmpl_RGB_Black = {{0.0, 0.0, 0.0}};
 const tmpl_RGB tmpl_RGB_White = {{1.0, 1.0, 1.0}};
+
+/*  The RGB colors (red-green-blue). Set max value in the appropriate channel.*/
 const tmpl_RGB tmpl_RGB_Red = {{1.0, 0.0, 0.0}};
 const tmpl_RGB tmpl_RGB_Green = {{0.0, 1.0, 0.0}};
 const tmpl_RGB tmpl_RGB_Blue = {{0.0, 0.0, 1.0}};
-const tmpl_RGB tmpl_RGB_Magenta = {{1.0, 0.0, 1.0}};
+
+/*  The CYM colors (cyan-yellow-magenta). Blends RGB colors together.         */
 const tmpl_RGB tmpl_RGB_Cyan = {{0.0, 1.0, 1.0}};
 const tmpl_RGB tmpl_RGB_Yellow = {{1.0, 1.0, 0.0}};
+const tmpl_RGB tmpl_RGB_Magenta = {{1.0, 0.0, 1.0}};
--- src/color/tmpl_color_constants_rgb48.c
+++ src/color/tmpl_color_constants_rgb48.c
@@ -1,9 +1,49 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is part of libtmpl.                                             *
+ *                                                                            *
+ *  libtmpl is free software: you can redistribute it and/or modify           *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  libtmpl is distributed in the hope that it will be useful,                *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with libtmpl.  If not, see <https://www.gnu.org/licenses/>.         *
+ ******************************************************************************
+ *                         tmpl_color_constants_rgb48                         *
+ ******************************************************************************
+ *  Purpose:                                                                  *
+ *      Provides commonly used colors in 48-bit RGB format.                   *
+ ******************************************************************************
+ *                                DEPENDENCIES                                *
+ ******************************************************************************
+ *  1.) tmpl_color.h:                                                         *
+ *          Header file where the colors are declared.                        *
+ ******************************************************************************
+ *  Author:     Ryan Maguire                                                  *
+ *  Date:       December 18, 2023                                             *
+ ******************************************************************************/
+
+/*  Header file where color structs are typedef'd and colors declared.        */
 #include <libtmpl/include/tmpl_color.h>
+
+/*  Black and white, set to the minimum and maximum possible values in each   *
+ *  color channel. For 48-bit RGB this is 0 and 65535 = 2^16-1, respectively. */
 const tmpl_RGB48 tmpl_RGB48_Black = {{0x0000U, 0x0000U, 0x0000U}};
 const tmpl_RGB48 tmpl_RGB48_White = {{0xFFFFU, 0xFFFFU, 0xFFFFU}};
+
+/*  The RGB colors (red-green-blue). Set max value in the appropriate channel.*/
 const tmpl_RGB48 tmpl_RGB48_Red = {{0xFFFFU, 0x0000U, 0x0000U}};
 const tmpl_RGB48 tmpl_RGB48_Green = {{0x0000U, 0xFFFFU, 0x0000U}};
 const tmpl_RGB48 tmpl_RGB48_Blue = {{0x0000U, 0x0000U, 0xFFFFU}};
-const tmpl_RGB48 tmpl_RGB48_Magenta = {{0xFFFFU, 0x0000U, 0xFFFFU}};
+
+/*  The CYM colors (cyan-yellow-magenta). Blends RGB colors together.         */
 const tmpl_RGB48 tmpl_RGB48_Cyan = {{0x0000U, 0xFFFFU, 0xFFFFU}};
 const tmpl_RGB48 tmpl_RGB48_Yellow = {{0xFFFFU, 0xFFFFU, 0x0000U}};
+const tmpl_RGB48 tmpl_RGB48_Magenta = {{0xFFFFU, 0x0000U, 0xFFFFU}};
