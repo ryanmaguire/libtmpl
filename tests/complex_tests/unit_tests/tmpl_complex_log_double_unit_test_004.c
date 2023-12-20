5b270fefa565c3617f1e9cb657c1852d8f7307e0
ryanmaguire <rmaguire@protonmail.com>
2023-12-20 11:56:58 -0500

    Adding tests for complex log.

---
+++ tests/complex_tests/unit_tests/tmpl_complex_log_double_unit_test_004.c
@@ -0,0 +1,25 @@
+#include <libtmpl/include/tmpl.h>
+#include <complex.h>
+#include <math.h>
+#include <stdio.h>
+
+int main(void)
+{
+    const complex double a = 1.0;
+    const complex double b = clog(a);
+
+    const tmpl_ComplexDouble z = tmpl_CDouble_Rect(1.0, 0.0);
+    const tmpl_ComplexDouble w = tmpl_CDouble_Log(z);
+
+    const double dx = creal(b) - tmpl_CDouble_Real_Part(w);
+    const double dy = cimag(b) - tmpl_CDouble_Imag_Part(w);
+    const double err = hypot (dx, dy) / cabs(b);
+
+    if (err > 1.0E-14)
+        puts("FAIL");
+
+    else
+        puts("PASS");
+
+    return 0;
+}
