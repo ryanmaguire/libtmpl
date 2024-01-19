9f5fcd0ca705d58feabecfebbecfab8ec9a5ad93
ryanmaguire <rmaguire@protonmail.com>
2024-01-19 16:06:17 -0500

    More numerical tests.

---
+++ tests/differentiation_real_tests/unit_tests/tmpl_five_point_second_derivative_double_unit_test_001.c
@@ -0,0 +1,39 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is part of libtmpl.                                             *
+ *                                                                            *
+ *  libtmpl is free software: you can redistribute it and/or modify           *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  libtmpl is distributed in the hope that it will be useful,                *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with libtmpl.  If not, see <https://www.gnu.org/licenses/>.         *
+ ******************************************************************************/
+#include <libtmpl/include/tmpl_math.h>
+#include <libtmpl/include/tmpl_differentiation_real.h>
+#include <stdio.h>
+
+int main(void)
+{
+    double (*f)(double) = tmpl_Double_Sin;
+    const double x0 = tmpl_One_Pi;
+    const double h = 1.0E-4;
+    const double tol = 1.0E-4;
+    const double y0 = tmpl_Double_Five_Point_Second_Derivative(f, x0, h);
+    const double ans = 0.0;
+    const double err = tmpl_Double_Abs(y0 - ans);
+
+    if (err > tol)
+        puts("FAIL");
+    else
+        puts("PASS");
+
+    return 0;
+}
