3b1d181d7bc3e1332a451f3b79a42ae2bb497a84
ryanmaguire <rmaguire@protonmail.com>
2024-03-08 09:01:06 -0500

    Adding unsigned minmax tests.

---
+++ tests/array_integer_tests/unit_tests/tmpl_array_minmax_uchar_unit_test_003.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is part of libtmpl.                                             *
+ *                                                                            *
+ *  libtmpl is free software: you can redistribute it and/or modify           *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  libtmpl is distributed in the hope that it will be useful,                *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with libtmpl.  If not, see <https://www.gnu.org/licenses/>.         *
+ ******************************************************************************/
+#include "tmpl_array_integer_unit_tests.h"
+#define inarr {0x05U, 0x04U, 0x03U, 0x02U, 0x01U, 0x00U}
+TEST2(unsigned char, tmpl_UChar_Array_MinMax, inarr, 0x00U, 0x05U)
