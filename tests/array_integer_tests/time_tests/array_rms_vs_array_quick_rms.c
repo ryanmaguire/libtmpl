61517beaf92b2d6cbfd2aea4ccaac9a7b590b3a0
ryanmaguire <rmaguire@protonmail.com>
2024-03-25 09:05:50 -0400

    Updating tests.

--- tests/array_integer_tests/time_tests/array_rms_vs_array_quick_rms.c
+++ tests/array_integer_tests/time_tests/array_rms_vs_array_quick_rms.c
@@ -1,38 +1,56 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is part of libtmpl.                                             *
+ *                                                                            *
+ *  libtmpl is free software: you can redistribute it and/or modify           *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  libtmpl is distributed in the hope that it will be useful,                *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with libtmpl.  If not, see <https://www.gnu.org/licenses/>.         *
+ ******************************************************************************/
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
 #include <libtmpl/include/tmpl_array_integer.h>
 
-static signed int randint(void)
+static signed int * randarr(size_t len)
 {
-    int n = rand();
-    double x = (double)n / (double)RAND_MAX;
-    return (int)(x * 2.0);
+    size_t n;
+    signed int * const arr = malloc(sizeof(*arr) * len);
+    const double factor = 20.0 / (double)RAND_MAX;
+
+    for (n = 0; n < len; ++n)
+    {
+        const int val = rand();
+        const double x_val = (double)val * factor;
+        arr[n] = (int)x_val;
+    }
+
+    return arr;
 }
 
 int main(void)
 {
-    size_t n;
-    const size_t N = (size_t)1E9;
-    signed int * const arr = malloc(sizeof(*arr) * N);
-    clock_t t0, t1;
-    double rms0, rms1;
+    const size_t N = (size_t)1E7;
+    signed int * const arr = randarr(N);
 
-    for (n = 0; n < N; ++n)
-        arr[n] = randint();
-
-    t0 = clock();
-    rms0 = tmpl_Int_Array_Double_RMS(arr, N);
-    t1 = clock();
+    const clock_t t0 = clock();
+    const double rms0 = tmpl_Int_Array_Double_RMS(arr, N);
+    const clock_t t1 = clock();
+    const double rms1 = tmpl_Int_Array_Double_Quick_RMS(arr, N);
+    const clock_t t2 = clock();
 
     printf("RMS:       %e\n", (double)(t1 - t0) / CLOCKS_PER_SEC);
-
-    t0 = clock();
-    rms1 = tmpl_Int_Array_Double_Quick_RMS(arr, N);
-    t1 = clock();
-
-    printf("Quick RMS: %e\n", (double)(t1 - t0) / CLOCKS_PER_SEC);
-
-    printf("%e\n%e\n", rms0, rms1);
+    printf("Quick RMS: %e\n", (double)(t2 - t1) / CLOCKS_PER_SEC);
+    printf("Error:     %e\n", (rms0 - rms1) / rms0);
+    free(arr);
     return 0;
 }
