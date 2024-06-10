36a5a32789986132bcab24a5411f1e32eb362138
ryanmaguire <rmaguire@protonmail.com>
2024-06-10 15:39:41 -0400

    Adding license to file.

--- include/tmpl_minmax.h
+++ include/tmpl_minmax.h
@@ -1,10 +1,41 @@
+/******************************************************************************
+ *                                 LICENSE                                    *
+ ******************************************************************************
+ *  This file is part of libtmpl.                                             *
+ *                                                                            *
+ *  libtmpl is free software: you can redistribute it and/or modify           *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  libtmpl is distributed in the hope that it will be useful,                *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with libtmpl.  If not, see <https://www.gnu.org/licenses/>.         *
+ ******************************************************************************
+ *                                tmpl_minmax                                 *
+ ******************************************************************************
+ *  Purpose:                                                                  *
+ *      Provides type-independent macros for computing min and max.           *
+ ******************************************************************************
+ *  Author:     Ryan Maguire                                                  *
+ *  Date:       May 20, 2023                                                  *
+ ******************************************************************************/
+
+/*  Include guard to prevent including this file twice.                       */
 #ifndef TMPL_MINMAX_H
 #define TMPL_MINMAX_H
 
+/*  Computes the max and min of two real numbers (ints or floats).            */
 #define TMPL_MAX(x, y) ((x) < (y) ? (y) : (x))
 #define TMPL_MIN(x, y) ((x) < (y) ? (x) : (y))
 
-#define TMPL_MAX3(x, y, z) ((x) < (y) ? TMPL_MAX((y), (z)) : TMPL_MAX((x), (z)))
-#define TMPL_MIN3(x, y, z) ((x) < (y) ? TMPL_MIN((x), (z)) : TMPL_MIN((y), (z)))
+/*  Computes the max and min of three real numbers (ints or floats).          */
+#define TMPL_MAX3(x, y, z) TMPL_MAX((x), TMPL_MAX((y), (z)))
+#define TMPL_MIN3(x, y, z) TMPL_MIN((x), TMPL_MIN((y), (z)))
 
 #endif
+/*  End of include guard.                                                     */
