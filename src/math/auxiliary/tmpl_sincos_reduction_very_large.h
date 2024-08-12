a488f2c17e4723ae2676ed9850c31980cf6d7d03
ryanmaguire <rmaguire@protonmail.com>
2024-08-12 09:03:08 -0400

    Fixing global variable conflict.

--- src/math/auxiliary/tmpl_sincos_reduction_very_large.h
+++ src/math/auxiliary/tmpl_sincos_reduction_very_large.h
@@ -23,17 +23,6 @@
 
 #include <libtmpl/include/tmpl_math.h>
 
-static const double split = 134217729.0; /* 2^27 + 1 */
-static const double t576 = 2.473304014731045340605025210196E173; /* 2^576 */
-static const double tm600 = 2.409919865102884117740750034713E-181; /* 2^-600 */
-static const double tm24 = 5.9604644775390625E-08; /* 2^-24 */
-static const double big = 6.755399441055744E15; /* 1.5 * 2^52 */
-static const double big1 = 2.7021597764222976E16; /* 1.5 * 2^54 */
-static const double hp0 = 1.570796326794896557998981734272E00;  /* pi / 2 hi */
-static const double hp1 = 6.123233995736766035868820147292E-17; /* pi / 2 lo */
-static const double mp1 = 1.5707963407039642333984375E00;        /* pi / 2 hi */
-static const double mp2 = -1.390906767539945576572790741920E-08; /* pi / 2 lo */
-
 static const double toverp[75] = { /*  2/ PI base 24*/
     10680707.0,  7228996.0,  1387004.0,  2578385.0, 16069853.0,
     12639074.0,  9804092.0,  4427841.0, 16666979.0, 11263675.0,
@@ -55,6 +44,16 @@ static const double toverp[75] = { /*  2/ PI base 24*/
 static unsigned int
 tmpl_Double_SinCos_Reduction_Very_Large(double x, double *a, double *aa)
 {
+    const double split = 134217729.0; /* 2^27 + 1 */
+    const double t576 = 2.473304014731045340605025210196E173; /* 2^576 */
+    const double tm600 = 2.409919865102884117740750034713E-181; /* 2^-600 */
+    const double tm24 = 5.9604644775390625E-08; /* 2^-24 */
+    const double big = 6.755399441055744E15; /* 1.5 * 2^52 */
+    const double big1 = 2.7021597764222976E16; /* 1.5 * 2^54 */
+    const double hp0 = 1.570796326794896557998981734272E00;  /* pi / 2 hi */
+    const double hp1 = 6.123233995736766035868820147292E-17; /* pi / 2 lo */
+    const double mp1 = 1.5707963407039642333984375E00;        /* pi / 2 hi */
+    const double mp2 = -1.390906767539945576572790741920E-08; /* pi / 2 lo */
     unsigned int k;
     tmpl_IEEE754_Double u, gor;
     double r[6], s, t, sum, b, bb, sum1, sum2, b1, bb1, b2, bb2, x1, x2, t1, t2;
