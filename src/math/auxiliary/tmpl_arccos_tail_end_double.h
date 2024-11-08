005d81f3742d255d6a8a9bc2e43fe6d71b352127
ryanmaguire <rmaguire@protonmail.com>
2024-11-08 10:24:52 -0500

    Replacing (10, 8) approximation with an (8, 8) one.

--- src/math/auxiliary/tmpl_arccos_tail_end_double.h
+++ src/math/auxiliary/tmpl_arccos_tail_end_double.h
@@ -56,6 +56,8 @@
  ******************************************************************************
  *  2023/04/18: Ryan Maguire                                                  *
  *      Changed src/math/tmpl_arccos_tail_end_double.c to include this file.  *
+ *  2024/10/08: Ryan Maguire                                                  *
+ *      Updating coefficients to use the (8, 8) approximant.                  *
  ******************************************************************************/
 
 /*  Include guard to prevent including this file twice.                       */
@@ -68,23 +70,22 @@
 /*  The compiler needs to know about the sqrt function.                       */
 extern double tmpl_Double_Sqrt(double x);
 
-/*  Coefficients for the numerator.                                           */
-#define A00 (+1.66666666666666657415E-01)
-#define A01 (-3.25565818622400915405E-01)
-#define A02 (+2.01212532134862925881E-01)
-#define A03 (-4.00555345006794114027E-02)
-#define A04 (+7.91534994289814532176E-04)
-#define A05 (+3.47933107596021167570E-05)
+/*  Coefficients for the numerator of the Remez rational approximation.       */
+#define A00 (+1.6666666666666675172610409335401762495970069423667E-01)
+#define A01 (-2.9647442738212244852684254810912673101657174481766E-01)
+#define A02 (+1.6001969221867813049084016184632874497094695883901E-01)
+#define A03 (-2.5510481570872249173776491491771394448907125511346E-02)
+#define A04 (+2.6066097969323856113412749790103952111930280796716E-04)
 
-/*  Coefficients for the denominator.                                         */
-#define B00 (+1.00000000000000000000E+00)
-#define B01 (-2.40339491173441421878E+00)
-#define B02 (+2.02094576023350569471E+00)
-#define B03 (-6.88283971605453293030E-01)
-#define B04 (+7.70381505559019352791e-02)
+/*  Coefficients for the denominator of the Remez rational approximation.     */
+#define B00 (+1.0000000000000000000000000000000000000000000000000E+00)
+#define B01 (-2.2288465642924490579275829949790393075345284377974E+00)
+#define B02 (+1.6952419643599424152439428142515867324057155650851E+00)
+#define B03 (-5.0120096652328631713045487959099718175996563925832E-01)
+#define B04 (+4.5088915315077310386265964807853660211534733521946E-02)
 
 /*  Helper macros for evaluating polynomials using Horner's method.           */
-#define TMPL_POLYA_EVAL(z) A00 + z*(A01 + z*(A02 + z*(A03 + z*(A04 + z*A05))))
+#define TMPL_POLYA_EVAL(z) A00 + z*(A01 + z*(A02 + z*(A03 + z*A04)))
 #define TMPL_POLYB_EVAL(z) B00 + z*(B01 + z*(B02 + z*(B03 + z*B04)))
 
 /*  Function for computing acos(x) for 0.5 <= x < 1.0.                        */
@@ -101,11 +102,11 @@ double tmpl_Double_Arccos_Tail_End(double x)
     /*  p(z) / q(z) is the rational minimax approximant for                   *
      *  (asin(sqrt(z)) - sqrt(z)) / z^{3/2}. We need to multiply by z^{3/2}.  */
     const double r = z*p/q;
-    const double s = tmpl_Double_Sqrt(z);
-    const double t = r*s;
+    const double sqrt_z = tmpl_Double_Sqrt(z);
+    const double t = r * sqrt_z;
 
-    /*  We now have asin(sqrt(z)) - sqrt(z). We need 2*asin(sqrt(z)).         */
-    return 2.0*(s + t);
+    /*  We now have t = asin(sqrt(z)) - sqrt(z). We need 2*asin(sqrt(z)).     */
+    return 2.0*(sqrt_z + t);
 }
 /*  End of tmpl_Double_Arccos_Tail_End.                                       */
 
