7ba7b74cbcd772bec73db3bf05bde77868095586
ryanmaguire <rmaguire@protonmail.com>
2024-11-11 16:39:52 -0500

    Adding helper macros for armv7 assembly.

---
+++ src/assembly/armv7l/tmpl_armv7l_asm.h
@@ -0,0 +1,44 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is part of libtmpl.                                             *
+ *                                                                            *
+ *  libtmpl is free software: you can redistribute it and/or modify it        *
+ *  under the terms of the GNU General Public License as published by         *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  libtmpl is distributed in the hope that it will be useful,                *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with libtmpl.  If not, see <https://www.gnu.org/licenses/>.         *
+ ******************************************************************************/
+
+/*  Include guard to prevent including this file twice.                       */
+#ifndef TMPL_ARMV7L_ASM_H
+#define TMPL_ARMV7L_ASM_H
+
+#define ASM_BEGIN(func)  \
+.text;                   \
+.align 1;                \
+.global func;            \
+.arch armv7-a;           \
+.thumb_func;             \
+.fpu vfpv3-d16;          \
+.type func, %function;   \
+func:
+
+#if defined(__ELF__)
+#define ASM_END(func)   \
+.size func, . - func;   \
+.section .note.GNU-stack,"",%progbits
+#else
+#define ASM_END(func)   \
+.size func, . - func
+#endif
+
+#endif
+/*  End of include guard.                                                     */
