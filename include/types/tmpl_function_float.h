5f27f7b5d808debfe8497f9f2129e8c0230f7bf0
ryanmaguire <rmaguire@protonmail.com>
2025-01-15 12:26:38 -0500

    Adding more licenses and comments.

--- include/types/tmpl_function_float.h
+++ include/types/tmpl_function_float.h
@@ -1,6 +1,41 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is part of libtmpl.                                             *
+ *                                                                            *
+ *  libtmpl is free software: you can redistribute it and/or modify           *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  libtmpl is distributed in the hope that it will be useful,                *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with libtmpl.  If not, see <https://www.gnu.org/licenses/>.         *
+ ******************************************************************************
+ *                            tmpl_function_float                             *
+ ******************************************************************************
+ *  Purpose:                                                                  *
+ *      Provides a single precision complex data type.                        *
+ ******************************************************************************
+ *  Author:     Ryan Maguire                                                  *
+ *  Date:       October 17, 2024                                              *
+ ******************************************************************************
+ *                              Revision History                              *
+ ******************************************************************************
+ *  2025/01/13: Ryan Maguire                                                  *
+ *      Moved this typedef to the types directory.                            *
+ ******************************************************************************/
+
+/*  Include guard to prevent including this file twice.                       */
 #ifndef TMPL_TYPES_FUNCTION_FLOAT_H
 #define TMPL_TYPES_FUNCTION_FLOAT_H
 
+/*  Function pointer for single precision real-valued functions.              */
 typedef float (*tmpl_FloatFunction)(float);
 
 #endif
+/*  End of include guard.                                                     */
--- include/types/tmpl_function_ldouble.h
+++ include/types/tmpl_function_ldouble.h
@@ -1,6 +1,41 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is part of libtmpl.                                             *
+ *                                                                            *
+ *  libtmpl is free software: you can redistribute it and/or modify           *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  libtmpl is distributed in the hope that it will be useful,                *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with libtmpl.  If not, see <https://www.gnu.org/licenses/>.         *
+ ******************************************************************************
+ *                            tmpl_function_ldouble                           *
+ ******************************************************************************
+ *  Purpose:                                                                  *
+ *      Provides a long double precision complex data type.                   *
+ ******************************************************************************
+ *  Author:     Ryan Maguire                                                  *
+ *  Date:       October 17, 2024                                              *
+ ******************************************************************************
+ *                              Revision History                              *
+ ******************************************************************************
+ *  2025/01/13: Ryan Maguire                                                  *
+ *      Moved this typedef to the types directory.                            *
+ ******************************************************************************/
+
+/*  Include guard to prevent including this file twice.                       */
 #ifndef TMPL_TYPES_FUNCTION_LDOUBLE_H
 #define TMPL_TYPES_FUNCTION_LDOUBLE_H
 
+/*  Function pointer for long double precision real-valued functions.         */
 typedef long double (*tmpl_LongDoubleFunction)(long double);
 
 #endif
+/*  End of include guard.                                                     */
