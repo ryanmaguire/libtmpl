70f0d4f4b8c24af655d870e3ac14a938da959b27
ryanmaguire <rmaguire@protonmail.com>
2025-02-13 14:57:10 -0500

    Adding references, replacing macros with extern constants.

--- src/math/auxiliary/tmpl_arccos_rat_remez_double.h
+++ src/math/auxiliary/tmpl_arccos_rat_remez_double.h
@@ -51,6 +51,21 @@
  *      Remez exchange algorithm.                                             *
  *  Notes:                                                                    *
  *      Accurate for |x| < 0.5.                                               *
+ *  References:                                                               *
+ *      1.) Maguire, Ryan (2024)                                              *
+ *          tmpld                                                             *
+ *          https://github.com/ryanmaguire/libtmpl_data                       *
+ *                                                                            *
+ *          Python library providing an implementation of the rational        *
+ *          Remez exchange algorithm. The coefficients for the                *
+ *          approximation used by this function were computed using this.     *
+ *                                                                            *
+ *      2.) Tasissa, Abiy (2019)                                              *
+ *          Function Approximation and the Remez Exchange Algorithm.          *
+ *          https://sites.tufts.edu/atasissa/files/2019/09/remez.pdf          *
+ *                                                                            *
+ *          Survey of the Remez polynomial and Remez exchange algorithm.      *
+ *          The implementation in tmpld is based on these notes.              *
  ******************************************************************************
  *                                DEPENDENCIES                                *
  ******************************************************************************
@@ -68,10 +83,9 @@
  *      Corrected name and comments. This was previously called a "Pade"      *
  *      approximant, but it is not. The coefficients for the Pade approximant *
  *      and the rational minimax function are slightly different. To achieve  *
- *      the same precision the Pade approximation needs two more terms, one   *
- *      for the numerator and one for the denominator. That is, to attain     *
- *      double precision on [-0.5, 0.5] the (12, 10) Pade approximant is      *
- *      needed.                                                               *
+ *      the same precision the Pade approximation needs more terms, two for   *
+ *      the numerator and one for the denominator. That is, to attain double  *
+ *      precision on [-0.5, 0.5] the (12, 10) Pade approximant is needed.     *
  *  2024/10/29: Ryan Maguire                                                  *
  *      In an effort to have reproducible results, the Remez coefficients are *
  *      now the exact same ones that are output from the Python script found  *
@@ -81,6 +95,8 @@
  *      approximation is sufficient for double precision. Peak theoretical    *
  *      is 8.5 * 10^-17. Unit tests show the actual error is within 1 ULP of  *
  *      other libm implementations. Slight performance boost using this.      *
+ *  2025/02/13: Ryan Maguire                                                  *
+ *      Added references.                                                     *
  ******************************************************************************/
 
 /*  Include guard to prevent including this file twice.                       */
@@ -90,14 +106,14 @@
 /*  Location of the TMPL_STATIC_INLINE macro.                                 */
 #include <libtmpl/include/tmpl_config.h>
 
-/*  Coefficients for the numerator of the Remez rational approximation.       */
+/*  Coefficients for the numerator of the rational Remez approximation.       */
 #define A00 (+1.6666666666666675172610409335401762495970069423667E-01)
 #define A01 (-2.9647442738212244852684254810912673101657174481766E-01)
 #define A02 (+1.6001969221867813049084016184632874497094695883901E-01)
 #define A03 (-2.5510481570872249173776491491771394448907125511346E-02)
 #define A04 (+2.6066097969323856113412749790103952111930280796716E-04)
 
-/*  Coefficients for the denominator of the Remez rational approximation.     */
+/*  Coefficients for the denominator of the rational Remez approximation.     */
 #define B00 (+1.0000000000000000000000000000000000000000000000000E+00)
 #define B01 (-2.2288465642924490579275829949790393075345284377974E+00)
 #define B02 (+1.6952419643599424152439428142515867324057155650851E+00)
@@ -109,7 +125,7 @@
 #define TMPL_POLYB_EVAL(z) B00 + z*(B01 + z*(B02 + z*(B03 + z*B04)))
 
 /*  The constant Pi / 2.                                                      */
-#define TMPL_PI_BY_TWO (+1.5707963267948966192313216916397514420985846996)
+extern const double tmpl_Double_Pi_By_Two;
 
 /*  Function for computing the (8, 8) minimax approximation for acos(x).      */
 TMPL_STATIC_INLINE
@@ -121,11 +137,11 @@ double tmpl_Double_Arccos_Rat_Remez(double x)
     /*  Use Horner's method to evaluate the two polynomials.                  */
     const double p = TMPL_POLYA_EVAL(x2);
     const double q = TMPL_POLYB_EVAL(x2);
-    const double r = x2*p/q;
+    const double r = x2 * p / q;
 
-    /*  p/q is the rational minimax approximant for (acos(x) - pi/2 + x)/x^3. *
+    /*  p/q is the rational Remez approximation for (acos(x) - pi/2 + x)/x^3. *
      *  Solving for acos(x), we get pi/2 - (x + x*x2*p/q).                    */
-    return TMPL_PI_BY_TWO - (x + x*r);
+    return tmpl_Double_Pi_By_Two - (x + x*r);
 }
 /*  End of tmpl_Double_Arccos_Rat_Remez.                                      */
 
