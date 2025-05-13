45f0bcd825c5daa0a93d7f46003e84a77eaff0b9
ryanmaguire <rmaguire@protonmail.com>
2025-05-13 09:05:46 -0400

    Adding in-place vector subtraction.

---
+++ include/inline/vec2/tmpl_vec2_subtractfrom_double.h
@@ -0,0 +1,93 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is part of libtmpl.                                             *
+ *                                                                            *
+ *  libtmpl is free software: you can redistribute it and/or modify           *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  libtmpl is distributed in the hope that it will be useful,                *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with libtmpl.  If not, see <https://www.gnu.org/licenses/>.         *
+ ******************************************************************************
+ *                       tmpl_vec2_subtractfrom_double                        *
+ ******************************************************************************
+ *  Purpose:                                                                  *
+ *      In-place vector subtraction for double precision 2D vectors.          *
+ ******************************************************************************
+ *                             DEFINED FUNCTIONS                              *
+ ******************************************************************************
+ *  Function Name:                                                            *
+ *      tmpl_2DDouble_SubtractFrom                                            *
+ *  Purpose:                                                                  *
+ *      Subtracts two points in the plane component-wise and in-place.        *
+ *  Arguments:                                                                *
+ *      P (tmpl_TwoVectorDouble * const):                                     *
+ *          A pointer to a point in the plane. The difference is stored here. *
+ *      Q (const tmpl_TwoVectorDouble * const):                               *
+ *          Another point in the Euclidean plane.                             *
+ *  Output:                                                                   *
+ *      None (void).                                                          *
+ *  Called Functions:                                                         *
+ *      None.                                                                 *
+ *  Method:                                                                   *
+ *      Vector subtraction is done component-wise. That is, if P = (a, b) and *
+ *      Q = (c, d), then the vector difference is given by:                   *
+ *                                                                            *
+ *          diff = P - Q                                                      *
+ *               = (a, b) - (c, d)                                            *
+ *               = (a - c, b - d)                                             *
+ *                                                                            *
+ *      In other words:                                                       *
+ *                                                                            *
+ *          x = a - c                                                         *
+ *          y = b - d                                                         *
+ *                                                                            *
+ *      We perform these differences, storing the result in P.                *
+ *  Notes:                                                                    *
+ *      1.) There are no checks for NULL pointers. It is assumed P and Q are  *
+ *          valid pointers to 2D vectors.                                     *
+ *      2.) This function acts as the "-=" operator for tmpl_TwoVectorDouble. *
+ *          That is, the function performs P -= Q.                            *
+ ******************************************************************************
+ *                                DEPENDENCIES                                *
+ ******************************************************************************
+ *  1.) tmpl_config.h:                                                        *
+ *          Location of the TMPL_INLINE_DECL macro.                           *
+ *  2.) tmpl_vec2_double.h:                                                   *
+ *          The tmpl_TwoVectorDouble typedef is provided here.                *
+ ******************************************************************************
+ *  Author:     Ryan Maguire                                                  *
+ *  Date:       May 13, 2025                                                  *
+ ******************************************************************************/
+
+/*  Include guard to prevent including this file twice.                       */
+#ifndef TMPL_VEC2_SUBTRACTFROM_DOUBLE_H
+#define TMPL_VEC2_SUBTRACTFROM_DOUBLE_H
+
+/*  The TMPL_INLINE_DECL macro is provided here.                              */
+#include <libtmpl/include/tmpl_config.h>
+
+/*  Two-vector typedef found here.                                            */
+#include <libtmpl/include/types/tmpl_vec2_double.h>
+
+/*  Function for subtracting 2 two-dimensional vectors in-place.              */
+TMPL_INLINE_DECL
+void
+tmpl_2DDouble_SubtractFrom(tmpl_TwoVectorDouble * const P,
+                           const tmpl_TwoVectorDouble * const Q)
+{
+    /*  The difference of two vectors simply subtracts their components.      */
+    P->dat[0] -= Q->dat[0];
+    P->dat[1] -= Q->dat[1];
+}
+/*  End of tmpl_2DDouble_SubtractFrom.                                        */
+
+#endif
+/*  End of include guard.                                                     */
---
+++ src/vec2/tmpl_two_vector_subtractfrom_no_inline_ldouble.c
@@ -0,0 +1,47 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is part of libtmpl.                                             *
+ *                                                                            *
+ *  libtmpl is free software: you can redistribute it and/or modify           *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  libtmpl is distributed in the hope that it will be useful,                *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with libtmpl.  If not, see <https://www.gnu.org/licenses/>.         *
+ ******************************************************************************
+ *                    tmpl_two_vector_subtractfrom_ldouble                    *
+ ******************************************************************************
+ *  Purpose:                                                                  *
+ *      2D vector subtraction in-place, -= operator.                          *
+ *      Source: libtmpl/include/inline/vec2/tmpl_vec2_subtractfrom_ldouble.h. *
+ ******************************************************************************
+ *  Author:     Ryan Maguire                                                  *
+ *  Date:       May 13, 2025                                                  *
+ ******************************************************************************/
+
+/*  Location of the TMPL_USE_INLINE macro.                                    */
+#include <libtmpl/include/tmpl_config.h>
+
+/*  Only used if inline support is not available.                             */
+#if TMPL_USE_INLINE != 1
+
+/*  2D Vector type found here.                                                */
+#include <libtmpl/include/types/tmpl_vec2_ldouble.h>
+
+/*  Function prototype / forward declaration.                                 */
+extern void
+tmpl_2DLDouble_SubtractFrom(tmpl_TwoVectorLongDouble * const P,
+                            const tmpl_TwoVectorLongDouble * const Q);
+
+/*  Implemented here.                                                         */
+#include "../../include/inline/vec2/tmpl_vec2_subtractfrom_ldouble.h"
+
+#endif
+/*  End of #if TMPL_USE_INLINE != 1.                                          */
