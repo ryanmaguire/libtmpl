37b11393f802952a09b70e99abd881303e4086a4
ryanmaguire <rmaguire@protonmail.com>
2025-05-13 09:35:16 -0400

    Adding in-place projection functions along the x and y axes.

---
+++ src/vec2/tmpl_two_vector_y_projectself_no_inline_float.c
@@ -0,0 +1,45 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is part of libtmpl.                                             *
+ *                                                                            *
+ *  libtmpl is free software: you can redistribute it and/or modify           *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  libtmpl is distributed in the hope that it will be useful,                *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with libtmpl.  If not, see <https://www.gnu.org/licenses/>.         *
+ ******************************************************************************
+ *                    tmpl_two_vector_y_projectself_float                     *
+ ******************************************************************************
+ *  Purpose:                                                                  *
+ *      Projection onto the y axis.                                           *
+ *      Source: libtmpl/include/inline/vec2/tmpl_vec2_y_projectself_float.h.  *
+ ******************************************************************************
+ *  Author:     Ryan Maguire                                                  *
+ *  Date:       May 13, 2025                                                  *
+ ******************************************************************************/
+
+/*  Location of the TMPL_USE_INLINE macro.                                    */
+#include <libtmpl/include/tmpl_config.h>
+
+/*  Only used if inline support is not available.                             */
+#if TMPL_USE_INLINE != 1
+
+/*  2D Vector type found here.                                                */
+#include <libtmpl/include/types/tmpl_vec2_float.h>
+
+/*  Function prototype / forward declaration.                                 */
+extern void tmpl_2DFloat_Y_ProjectSelf(tmpl_TwoVectorFloat * const P);
+
+/*  Implemented here.                                                         */
+#include "../../include/inline/vec2/tmpl_vec2_y_projectself_float.h"
+
+#endif
+/*  End of #if TMPL_USE_INLINE != 1.                                          */
