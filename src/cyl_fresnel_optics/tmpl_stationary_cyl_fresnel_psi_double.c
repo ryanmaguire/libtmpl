ba16be90f624e9b4b7d4bd2480fc60e6312c3994
ryanmaguire <rmaguire@protonmail.com>
2025-05-26 16:08:45 -0400

    Adding optimized version of the stationary Fresnel kernel.
    Most of the computations in dPsi_dPhi and d2Psi_dPhi2 are duplicate. By 'inlining' the two functions we can cut the number of computations we do roughly in half.

--- src/cyl_fresnel_optics/tmpl_stationary_cyl_fresnel_psi_double.c
+++ src/cyl_fresnel_optics/tmpl_stationary_cyl_fresnel_psi_double.c
@@ -29,6 +29,8 @@
 /*  2D vector functions found here.                                           */
 #include <libtmpl/include/tmpl_vec2.h>
 
+#if 0
+
 double
 tmpl_Double_Stationary_Cyl_Fresnel_Psi(
     double k,
@@ -61,3 +63,103 @@ tmpl_Double_Stationary_Cyl_Fresnel_Psi(
 
     return tmpl_Double_Cyl_Fresnel_Psi(k, &rho_s, rho0, R);
 }
+
+#else
+
+double
+tmpl_Double_Stationary_Cyl_Fresnel_Psi(
+    double k,
+    const tmpl_TwoVectorDouble * const rho,
+    const tmpl_TwoVectorDouble * const rho0,
+    const tmpl_ThreeVectorDouble * TMPL_RESTRICT const R,
+    double eps,
+    unsigned int max_iters
+)
+{
+    /*  The relative position vector, u, of the observer from the ring        *
+     *  intercept point. Since rho0 lies in the plane, there is no z part.    */
+    const double ux = R->dat[0] - rho0->dat[0];
+    const double uy = R->dat[1] - rho0->dat[1];
+    const double uz = R->dat[2];
+
+    /*  Compute the distance between rho0 and R. This is "D" in MTR86.        */
+    const double rho0_dist = tmpl_Double_Hypot3(ux, uy, uz);
+
+    unsigned int iters = 0U;
+    const double r_s = tmpl_2DDouble_L2_Norm(rho);
+    double phi_s = tmpl_2DDouble_Polar_Angle(rho);
+    tmpl_TwoVectorDouble rhos = *rho;
+    double err;
+    eps /= k;
+
+    do {
+        /*  The difference between the "dummy" point in the plane, rho, that  *
+         *  is integrated over, and the orthogonal projection of the position *
+         *  vector for the observer, R, onto the ring plane.                  */
+        const double dx = R->dat[0] - rhos.dat[0];
+        const double dy = R->dat[1] - rhos.dat[1];
+
+        /*  We need the distances from R to rho and R to rho0. These can be   *
+         *  computed with the Hypot3 function.                                */
+        const double rho_dist = tmpl_Double_Hypot3(dx, dy, uz);
+
+        /*  The second derivative contains both 1 / || R - rho || and the     *
+         *  square of this expression. Pre-compute these.                     */
+        const double rcpr_rho_dist = 1.0 / rho_dist;
+        const double rcpr_rho_dist_sq = rcpr_rho_dist * rcpr_rho_dist;
+
+        /*  The derivative is given by:                                       *
+         *                                                                    *
+         *               -                                                 -  *
+         *              | dx * rho_y - dy * rho_x   uy * rho_x -ux * rho_y  | *
+         *      psi' = k| ----------------------- + ----------------------  | *
+         *              |     || R - rho ||             || R - rho0 ||      | *
+         *               -                                                 -  *
+         *                                                                    *
+         *  Call the left expression "left" and the right one "right."        */
+        const double left1 = (dx * rhos.dat[1] - dy * rhos.dat[0]) / rho_dist;
+        const double right1 = (uy * rhos.dat[0] - ux * rhos.dat[1]) / rho0_dist;
+
+        /*  The second derivative is given by:                                *
+         *                                                                    *
+         *                 -                                                  *
+         *                | (R - rho) . rho'' - rho' . rho'                   *
+         *      psi'' = k | ------------------------------ -                  *
+         *                |          || R - rho ||                            *
+         *                 -                                                  *
+         *                                                   -                *
+         *                  ((R - rho) . rho')^2              |               *
+         *                  -------------------- + un . rho'' |               *
+         *                    || R - rho ||^3/2               |               *
+         *                                                   -                *
+         *                                                                    *
+         *  The first expression simplifies to -(x rho_x + y rho_y) and the   *
+         *  central piece is (rho_x y - rho_y x)^2. Compute both of these.    */
+        const double diff = rhos.dat[0] * R->dat[1] - rhos.dat[1] * R->dat[0];
+        const double sum = R->dat[0] * rhos.dat[0] + R->dat[1] * rhos.dat[1];
+
+        /*  Call the combination of the left and center parts "left". Compute.*/
+        const double left2 = (diff*diff*rcpr_rho_dist_sq - sum) * rcpr_rho_dist;
+
+        /*  The expression u . rho'' is -(ux * rho_x + uy * rho_y). The right *
+         *  expression is this factor divided by the distance from R to rho0. */
+        const double right2 = (ux * rhos.dat[0] + uy * rhos.dat[1]) / rho0_dist;
+
+        const double dpsi = left1 + right1;
+        const double d2psi = left2 + right2;
+
+        err = tmpl_Double_Abs(dpsi);
+        phi_s = phi_s + dpsi / d2psi;
+        rhos = tmpl_2DDouble_Polar(r_s, phi_s);
+        ++iters;
+
+        if (iters > max_iters)
+            break;
+
+    } while (err > eps);
+
+    /*  The output is the sum scaled by the wavenumber, k.                    */
+    return tmpl_Double_Cyl_Fresnel_Psi(k, &rhos, rho0, R);
+}
+
+#endif
