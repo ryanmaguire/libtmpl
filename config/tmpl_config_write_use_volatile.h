2f99dd6eaab072dcd38d6cd4c0339b15db328c96
ryanmaguire <rmaguire@protonmail.com>
2025-10-27 22:00:44 -0400

    Adding volatile checker, moving to its own file.

---
+++ config/tmpl_config_write_use_volatile.h
@@ -0,0 +1,88 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is part of libtmpl.                                             *
+ *                                                                            *
+ *  libtmpl is free software: you can redistribute it and/or modify           *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  libtmpl is distributed in the hope that it will be useful,                *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with libtmpl.  If not, see <https://www.gnu.org/licenses/>.         *
+ ******************************************************************************
+ *                       tmpl_config_write_use_volatile                       *
+ ******************************************************************************
+ *  Purpose:                                                                  *
+ *      Writes the TMPL_VOLATILE macro to a file.                             *
+ ******************************************************************************
+ *                             DEFINED FUNCTIONS                              *
+ ******************************************************************************
+ *  Function Name:                                                            *
+ *      tmpl_write_use_volatile                                               *
+ *  Purpose:                                                                  *
+ *      Writes the TMPL_VOLATILE macro to a file.                             *
+ *  Arguments:                                                                *
+ *      fp (FILE *):                                                          *
+ *          File pointer for the file being written to.                       *
+ *  Output:                                                                   *
+ *      None (void).                                                          *
+ *  Called Functions:                                                         *
+ *      stdio.h:                                                              *
+ *          fputs:                                                            *
+ *              Writes a string to a file.                                    *
+ *  Method:                                                                   *
+ *      Check the TMPL_USE_VOLATILE macro and C99 support and then call fputs.*
+ ******************************************************************************
+ *                                DEPENDENCIES                                *
+ ******************************************************************************
+ *  1.) stdio.h:                                                              *
+ *          Standard library header file providing FILE and fputs.            *
+ ******************************************************************************
+ *  Author:     Ryan Maguire                                                  *
+ *  Date:       October 27, 2025                                              *
+ ******************************************************************************/
+
+/*  Include guard to prevent including this file twice.                       */
+#ifndef TMPL_CONFIG_WRITE_USE_VOLATILE_H
+#define TMPL_CONFIG_WRITE_USE_VOLATILE_H
+
+/*  FILE type and fputs given here.                                           */
+#include <stdio.h>
+
+/*  Function for writing the TMPL_VOLATILE macro to a file.                   */
+static void tmpl_write_use_volatile(FILE *fp)
+{
+#if defined(TMPL_USE_VOLATILE_DOUBLE_SPLIT)
+    fputs("#define TMPL_DOUBLE_VOLATILE_SPLIT\n", fp);
+#elif defined(TMPL_USE_CAUTIOUS_DOUBLE_SPLIT)
+    fputs("#define TMPL_DOUBLE_CAUTIOUS_SPLIT\n", fp);
+#endif
+
+#if defined(TMPL_USE_VOLATILE_FLOAT_SPLIT)
+    fputs("#define TMPL_FLOAT_VOLATILE_SPLIT\n", fp);
+#elif defined(TMPL_USE_CAUTIOUS_FLOAT_SPLIT)
+    fputs("#define TMPL_FLOAT_CAUTIOUS_SPLIT\n", fp);
+#endif
+
+#if defined(TMPL_USE_VOLATILE_LDOUBLE_SPLIT)
+    fputs("#define TMPL_LDOUBLE_VOLATILE_SPLIT\n", fp);
+#elif defined(TMPL_USE_CAUTIOUS_LDOUBLE_SPLIT)
+    fputs("#define TMPL_LDOUBLE_CAUTIOUS_SPLIT\n", fp);
+#endif
+
+#if defined(TMPL_USE_VOLATILE)
+    fputs("#define TMPL_VOLATILE volatile\n\n", fp);
+#else
+    fputs("#define TMPL_VOLATILE\n\n", fp);
+#endif
+}
+/*  End of tmpl_write_use_volatile.                                           */
+
+#endif
+/*  End of include guard.                                                     */
