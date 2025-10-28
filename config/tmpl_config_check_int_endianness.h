c20fa6da4f023c49734fdb80a6ba3616f8c8387a
ryanmaguire <rmaguire@protonmail.com>
2025-10-27 20:39:12 -0400

    Moving endiannes checker to its own file.

---
+++ config/tmpl_config_check_int_endianness.h
@@ -0,0 +1,73 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is part of libtmpl.                                             *
+ *                                                                            *
+ *  libtmpl is free software: you can redistribute it and/or modify           *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  libtmpl is distributed in the hope that it will be useful,                *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with libtmpl.  If not, see <https://www.gnu.org/licenses/>.         *
+ ******************************************************************************
+ *                      tmpl_config_check_int_endianness                      *
+ ******************************************************************************
+ *  Purpose:                                                                  *
+ *      Sets the config globals for integer endianness.                       *
+ ******************************************************************************
+ *                             DEFINED FUNCTIONS                              *
+ ******************************************************************************
+ *  Function Name:                                                            *
+ *      tmpl_check_int_endianness                                             *
+ *  Purpose:                                                                  *
+ *      Checks the endianness of integer types and sets the config globals.   *
+ *  Arguments:                                                                *
+ *      None (void).                                                          *
+ *  Output:                                                                   *
+ *      None (void).                                                          *
+ *  Called Functions:                                                         *
+ *      config/                                                               *
+ *          tmpl_config_det_int_endianness.h:                                 *
+ *              tmpl_det_int_endianness:                                      *
+ *                  Determines the endiannes of unsigned integer types.       *
+ *  Method:                                                                   *
+ *      Set the tmpl_endian and tmpl_int_endianness_has_been_checked globals  *
+ *      after calling the tmpl_det_int_endianness function.                   *
+ ******************************************************************************
+ *                                DEPENDENCIES                                *
+ ******************************************************************************
+ *  1.) tmpl_config_globals.h:                                                *
+ *          Header file with all of the globals used by config.c.             *
+ *  2.) tmpl_config_det_int_endianness.h:                                     *
+ *          Provides the tmpl_det_int_endianness function.                    *
+ ******************************************************************************
+ *  Author:     Ryan Maguire                                                  *
+ *  Date:       October 27, 2025                                              *
+ ******************************************************************************/
+
+/*  Include guard to prevent including this file twice.                       */
+#ifndef TMPL_CONFIG_CHECK_INT_ENDIANNESS_H
+#define TMPL_CONFIG_CHECK_INT_ENDIANNESS_H
+
+/*  Globals for the config file are all found here.                           */
+#include "tmpl_config_globals.h"
+
+/*  The tmpl_det_int_endianness function is found here.                       */
+#include "tmpl_config_det_int_endianness.h"
+
+/*  Function for setting the config globals for integer endianness.           */
+static void tmpl_check_int_endianness(void)
+{
+    tmpl_endian = tmpl_det_int_endianness();
+    tmpl_int_endianness_has_been_checked = 1U;
+}
+/*  End of tmpl_check_int_endianness.                                         */
+
+#endif
+/*  End of include guard.                                                     */
