0897342d650ca80f28cab8d292f5db72492dd9c4
ryanmaguire <rmaguire@protonmail.com>
2025-10-29 15:11:19 -0400

    Creating width detector function.

--- config/tmpl_config_det_widths.h
+++ config/tmpl_config_det_widths.h
@@ -1,113 +1,113 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is part of libtmpl.                                             *
+ *                                                                            *
+ *  libtmpl is free software: you can redistribute it and/or modify           *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  libtmpl is distributed in the hope that it will be useful,                *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with libtmpl.  If not, see <https://www.gnu.org/licenses/>.         *
+ ******************************************************************************
+ *                           tmpl_config_det_widths                           *
+ ******************************************************************************
+ *  Purpose:                                                                  *
+ *      Detects the widths of all unsigned integer types.                     *
+ ******************************************************************************
+ *                             DEFINED FUNCTIONS                              *
+ ******************************************************************************
+ *  Function Name:                                                            *
+ *      tmpl_det_widths                                                       *
+ *  Purpose:                                                                  *
+ *      Detects the widths of all unsigned integer types.                     *
+ *  Arguments:                                                                *
+ *      None (void).                                                          *
+ *  Output:                                                                   *
+ *      None (void).                                                          *
+ *  Called Functions:                                                         *
+ *      config/                                                               *
+ *          tmpl_config_det_uchar_width.h:                                    *
+ *              tmpl_det_uchar_width:                                         *
+ *                  Computes the number of bits in unsigned char.             *
+ *          tmpl_config_det_ushort_width.h:                                   *
+ *              tmpl_det_ushort_width:                                        *
+ *                  Computes the number of bits in unsigned short.            *
+ *          tmpl_config_det_uint_width.h:                                     *
+ *              tmpl_det_uint_width:                                          *
+ *                  Computes the number of bits in unsigned int.              *
+ *          tmpl_config_det_ulong_width.h:                                    *
+ *              tmpl_det_ulong_width:                                         *
+ *                  Computes the number of bits in unsigned long.             *
+ *          tmpl_config_det_ullong_width.h:                                   *
+ *              tmpl_det_ullong_width:                                        *
+ *                  Computes the number of bits in unsigned long long.        *
+ *  Method:                                                                   *
+ *      Call all of the width functions for each data type.                   *
+ *  Notes:                                                                    *
+ *      1.) This function does not attempt to determine the width of signed   *
+ *          integer data types.                                               *
+ ******************************************************************************
+ *                                DEPENDENCIES                                *
+ ******************************************************************************
+ *  1.) tmpl_config_globals.h:                                                *
+ *          Header file with all of the globals used by config.c.             *
+ *  2.) tmpl_config_det_uchar_width.h:                                        *
+ *          Provides the tmpl_det_uchar_width function.                       *
+ *  3.) tmpl_config_det_ushort_width.h:                                       *
+ *          Provides the tmpl_det_ushort_width function.                      *
+ *  4.) tmpl_config_det_uint_width.h:                                         *
+ *          Provides the tmpl_det_uint_width function.                        *
+ *  5.) tmpl_config_det_ulong_width.h:                                        *
+ *          Provides the tmpl_det_ulong_width function.                       *
+ *  6.) tmpl_config_det_ullong_width.h:                                       *
+ *          Provides the tmpl_det_ullong_width function.                      *
+ ******************************************************************************
+ *  Author:     Ryan Maguire                                                  *
+ *  Date:       March 10, 2021                                                *
+ ******************************************************************************
+ *                              Revision History                              *
+ ******************************************************************************
+ *  2025/04/08: Ryan Maguire                                                  *
+ *      Moved function to its own file.                                       *
+ *  2025/10/27: Ryan Maguire                                                  *
+ *      Moved enum definition to the config globals file.                     *
+ ******************************************************************************/
+
 /*  Include guard to prevent including this file twice.                       */
 #ifndef TMPL_CONFIG_DET_WIDTHS_H
 #define TMPL_CONFIG_DET_WIDTHS_H
 
-/*  Number of bits in char. char is not allowed to have padding, so all bits  *
- *  are used. This fact can be used to determine this value.                  */
-static unsigned int TMPL_CHAR_BIT;
+/*  Globals for the config file are all found here.                           */
+#include "tmpl_config_globals.h"
 
-/*  The other integer data types are allowed to have padding. These numbers   *
- *  represent the number of unpadded bits.                                    */
-static unsigned int TMPL_SHORT_BIT;
-static unsigned int TMPL_INT_BIT;
-static unsigned int TMPL_LONG_BIT;
-#ifdef TMPL_LONG_LONG_IS_AVAILABLE
-static unsigned int TMPL_LLONG_BIT;
-#endif
-static int TMPL_BITS_HAVE_BEEN_SET = 0;
-static int TMPL_HAS_64_BIT_INT;
-static int TMPL_HAS_32_BIT_INT;
+/*  Functions for computing the widths of unsigned types found here.          */
+#include "tmpl_config_det_uchar_width.h"
+#include "tmpl_config_det_ushort_width.h"
+#include "tmpl_config_det_uint_width.h"
+#include "tmpl_config_det_ulong_width.h"
+#include "tmpl_config_det_ullong_width.h"
 
-/*  Function for determining the number of bits in char, and others.          */
+/*  Function for determining the number of bits in all unsigned types.        */
 static void tmpl_det_widths(void)
 {
-    unsigned char c = 0x01U;
-    unsigned short int s = 1U;
-    unsigned int i = 1U;
-    unsigned long int l = 1UL;
+    /*  Char, short, int, and long are standard in all version of C.          */
+    tmpl_det_uchar_width();
+    tmpl_det_ushort_width();
+    tmpl_det_uint_width();
+    tmpl_det_ulong_width();
+
+    /*  Long long was only introduced with the C99 standard.                  */
 #ifdef TMPL_LONG_LONG_IS_AVAILABLE
-    unsigned long long int ll = 1ULL;
-    TMPL_LLONG_BIT = 0U;
-#endif
-    TMPL_CHAR_BIT = 0U;
-    TMPL_SHORT_BIT = 0U;
-    TMPL_INT_BIT = 0U;
-    TMPL_LONG_BIT = 0U;
-
-    /*  Unsigned integer types can not overflow since the result is computed  *
-     *  mod 2^N where N is the number of bits. By started with 1 and          *
-     *  repeatedly multiplying by 2 we will eventually get 2^N, which will    *
-     *  be computed as 0 mod 2^N. By counting the number of times we need to  *
-     *  to multiply by 2 in order to get zero we can compute the number of    *
-     *  bits in a char.                                                       */
-    while (c != 0x00U)
-    {
-        c = c * 0x02U;
-        TMPL_CHAR_BIT++;
-    }
-
-    /*  Similar idea for short, int, and long. These integer data types can   *
-     *  have padding. The number computed is not necessarily the number of    *
-     *  bits in a short, int, or long, just the number of used, or unpadded,  *
-     *  bits. For most compilers this is the same as the size of the type and *
-     *  no padding is actually used.                                          */
-    while (s != 0U)
-    {
-        s = s * 2U;
-        TMPL_SHORT_BIT++;
-    }
-
-    while (i != 0U)
-    {
-        i = i * 2U;
-        TMPL_INT_BIT++;
-    }
-
-    while (l != 0UL)
-    {
-        l = l * 2UL;
-        TMPL_LONG_BIT++;
-    }
-
-#ifdef TMPL_LONG_LONG_IS_AVAILABLE
-    while (ll != 0ULL)
-    {
-        ll = ll * 2ULL;
-        TMPL_LLONG_BIT++;
-    }
+    tmpl_det_ullong_width();
 #endif
 
-    TMPL_BITS_HAVE_BEEN_SET = 1;
-
-    if (TMPL_CHAR_BIT == 32)
-        TMPL_HAS_32_BIT_INT = 1;
-    else if (TMPL_SHORT_BIT == 32 && (TMPL_CHAR_BIT * sizeof(unsigned short int)) == 32)
-        TMPL_HAS_32_BIT_INT = 1;
-    else if (TMPL_INT_BIT == 32 && (TMPL_CHAR_BIT * sizeof(unsigned int)) == 32)
-        TMPL_HAS_32_BIT_INT = 1;
-    else if (TMPL_LONG_BIT == 32 && (TMPL_CHAR_BIT * sizeof(unsigned long int)) == 32)
-        TMPL_HAS_32_BIT_INT = 1;
-#ifdef TMPL_LONG_LONG_IS_AVAILABLE
-    else if (TMPL_LLONG_BIT == 32 && (TMPL_CHAR_BIT * sizeof(unsigned long long int)) == 32)
-        TMPL_HAS_32_BIT_INT = 1;
-#endif
-    else
-        TMPL_HAS_32_BIT_INT = 0;
-
-    if (TMPL_CHAR_BIT == 64)
-        TMPL_HAS_64_BIT_INT = 1;
-    else if (TMPL_SHORT_BIT == 64 && (TMPL_CHAR_BIT * sizeof(unsigned short int)) == 64)
-        TMPL_HAS_64_BIT_INT = 1;
-    else if (TMPL_INT_BIT == 64 && (TMPL_CHAR_BIT * sizeof(unsigned int)) == 64)
-        TMPL_HAS_64_BIT_INT = 1;
-    else if (TMPL_LONG_BIT == 64 && (TMPL_CHAR_BIT * sizeof(unsigned long int)) == 64)
-        TMPL_HAS_64_BIT_INT = 1;
-#ifdef TMPL_LONG_LONG_IS_AVAILABLE
-    else if (TMPL_LLONG_BIT == 64 && (TMPL_CHAR_BIT * sizeof(unsigned long long int)) == 64)
-        TMPL_HAS_64_BIT_INT = 1;
-#endif
-    else
-        TMPL_HAS_64_BIT_INT = 0;
 }
 /*  End of tmpl_det_widths.                                                   */
 
