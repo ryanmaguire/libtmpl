fbc3f83ead48b68786c1918bc9ff9b94891f5ad7
ryanmaguire <rmaguire@protonmail.com>
2026-01-12 18:49:41 -0500

    Adding const qualifier.

--- src/array_integer/tmpl_array_double_sum_int.c
+++ src/array_integer/tmpl_array_double_sum_int.c
@@ -109,8 +109,8 @@ tmpl_Int_Array_Double_Sum(const signed int * const arr, size_t len);
 
 /*  Lacking inline support, tell the compiler about the function.             */
 extern void
-tmpl_Double_Two_Sum(double x,
-                    double y,
+tmpl_Double_Two_Sum(const double x,
+                    const double y,
                     double * TMPL_RESTRICT const out,
                     double * TMPL_RESTRICT const err);
 
