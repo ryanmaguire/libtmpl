7fd0fa27c3199d015f54b883cdc9b9069689fb8c
ryanmaguire <rmaguire@protonmail.com>
2026-01-22 10:46:12 -0500

    Adding rough draft of Filon02 method.

---
+++ src/integration/tmpl_filon12_integrand_cdouble.c
@@ -0,0 +1,219 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is part of libtmpl.                                             *
+ *                                                                            *
+ *  libtmpl is free software: you can redistribute it and/or modify           *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  libtmpl is distributed in the hope that it will be useful,                *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with libtmpl.  If not, see <https://www.gnu.org/licenses/>.         *
+ ******************************************************************************
+ *                       tmpl_filon12_integrand_cdouble                       *
+ ******************************************************************************
+ *  Purpose:                                                                  *
+ *      Computes the integral of f(t) exp(i g(t)) assuming f(a) = A t + B and *
+ *      g(t) = a t^2 + b t + c.                                               *
+ ******************************************************************************
+ *                             DEFINED FUNCTIONS                              *
+ ******************************************************************************
+ *  Function Name:                                                            *
+ *      tmpl_CDouble_Filon12_Integrand                                        *
+ *  Purpose:                                                                  *
+ *      Numerically integrates f(t) exp(i g(t)) assuming affine f and         *
+ *      quadratic g, where f is complex-valued and g is real-valued.          *
+ *  Arguments:                                                                *
+ *      left (const tmpl_ComplexDouble):                                      *
+ *          The value for f(t) at the left end of the interval.               *
+ *      middle (const tmpl_ComplexDouble):                                    *
+ *          The value for f(t) in the middle of the interval.                 *
+ *      right (const tmpl_ComplexDouble):                                     *
+ *          The value for f(t) at the right end of the interval.              *
+ *      g_left (const double):                                                *
+ *          The left real value for the phase.                                *
+ *      g_middle (const double):                                              *
+ *          The central real value for the phase.                             *
+ *      g_right (const double):                                               *
+ *          The right real value for the phase.                               *
+ *      width (const double):                                                 *
+ *          The width of a single bin.                                        *
+ *  Output:                                                                   *
+ *      integrand (tmpl_ComplexDouble):                                       *
+ *          The integral of f(t) exp(i g(t)) across the two bins.             *
+ ******************************************************************************
+ *                                DEPENDENCIES                                *
+ ******************************************************************************
+ *  1.) tmpl_integration.h:                                                   *
+ *          Header file containing the function prototype.                    *
+ *  2.) tmpl_math.h:                                                          *
+ *          Header providing the absolute value function.                     *
+ *  3.) tmpl_complex.h:                                                       *
+ *          Header providing complex numbers and complex arithmetic.          *
+ *  4.) tmpl_special_functions_real.h:                                        *
+ *          Header providing the Fresnel functions, C(x) and S(x).            *
+ *  5.) tmpl_float.h:                                                         *
+ *          Header providing the TMPL_SQRT_DBL_EPS macro.                     *
+ ******************************************************************************
+ *  Author:     Ryan Maguire                                                  *
+ *  Date:       January 15, 2026                                              *
+ ******************************************************************************/
+
+/*  Function prototype given here.                                            */
+#include <libtmpl/include/tmpl_integration.h>
+
+/*  Absolute value function provided here.                                    */
+#include <libtmpl/include/tmpl_math.h>
+
+/*  Complex numbers provided here.                                            */
+#include <libtmpl/include/tmpl_complex.h>
+
+/*  Fresnel functions provided here.                                          */
+#include <libtmpl/include/tmpl_special_functions_real.h>
+
+/*  TMPL_SQRT_DBL_EPS macro provided here.                                    */
+#include <libtmpl/include/tmpl_float.h>
+
+/*  Function for numerically integrating using a Filon-like quadrature.       */
+tmpl_ComplexDouble
+tmpl_CDouble_Filon12_Integrand(const tmpl_ComplexDouble left,
+                               const tmpl_ComplexDouble mid,
+                               const tmpl_ComplexDouble right,
+                               const double g_left,
+                               const double g_mid,
+                               const double g_right,
+                               const double width)
+{
+    /*  Variable for the output, the integral of f(t) exp(i g(t)) across the  *
+     *  current bin [L, R], with R - L = 2 * width.                           */
+    tmpl_ComplexDouble integrand;
+
+    /*  Threshold for swapping between quadratic and linear methods.          */
+    const double threshold = 4.0 * TMPL_QURT_DBL_EPS;
+
+    /*  The function g is written in the form A (t - B)^2 + C. We use the     *
+     *  magnitude of A to determine whether we use a quadratic or a linear    *
+     *  interpolation across the two bins for the integral.                   */
+    const double rcpr_width = 1.0 / width;
+    const double half_rcpr_width_squared = 0.5 * rcpr_width * rcpr_width;
+    const double scaled_curvature = g_right - 2.0 * g_mid + g_left;
+    const double curvature = scaled_curvature * half_rcpr_width_squared;
+    const double abs_curvature = tmpl_Double_Abs(curvature);
+    tmpl_ComplexDouble l_int, r_int;
+
+    /*  If the curvature is very small, then the parabola essentially becomes *
+     *  a line and we may use a linear interpolation for the integral.        */
+    if (abs_curvature < threshold)
+    {
+        l_int = tmpl_CDouble_Filon11_Integrand(
+            left, mid, g_left, g_mid, width
+        );
+
+        r_int = tmpl_CDouble_Filon11_Integrand(
+            mid, right, g_mid, g_right, width
+        );
+
+        integrand = tmpl_CDouble_Add(l_int, r_int);
+    }
+
+    /*  If the curvature is large enough, way may safely use the quadratic    *
+     *  interpolation to integrate across the two bins.                       */
+    else
+    {
+        tmpl_ComplexDouble scale_left, scale_right, scale;
+        tmpl_ComplexDouble exp_left, exp_mid, exp_right;
+        tmpl_ComplexDouble factor_left, factor_right;
+        tmpl_ComplexDouble diff_left, diff_right;
+
+        const double scale_factor = 0.5 / scaled_curvature;
+        const double g_diff = g_left - g_right;
+
+        const double p0 = g_mid - 0.25 * g_diff * g_diff * scale_factor;
+        const double p1 = g_diff * width * scale_factor;
+        const double p2 = tmpl_Double_Sqrt(abs_curvature);
+
+        const double shift_left = p1 + width;
+        const double shift_right = p1 - width;
+
+        const double arg_left = p2 * shift_left;
+        const double arg_mid = p2 * p1;
+        const double arg_right = p2 * shift_right;
+
+        const double s_left = tmpl_Double_Fresnel_Sin(arg_left);
+        const double s_mid = tmpl_Double_Fresnel_Sin(arg_mid);
+        const double s_right = tmpl_Double_Fresnel_Sin(arg_right);
+
+        const double c_left = tmpl_Double_Fresnel_Cos(arg_left);
+        const double c_mid = tmpl_Double_Fresnel_Cos(arg_mid);
+        const double c_right = tmpl_Double_Fresnel_Cos(arg_right);
+
+        const double s_left_scale = s_left - s_mid;
+        const double s_right_scale = s_mid - s_right;
+        const double c_left_scale = c_left - c_mid;
+        const double c_right_scale = c_mid - c_right;
+
+        const double left_exp_arg = curvature * shift_left * shift_left;
+        const double mid_exp_arg = curvature * p1 * p1;
+        const double right_exp_arg = curvature * shift_right * shift_right;
+
+        exp_left = tmpl_CDouble_Expi(left_exp_arg);
+        exp_mid = tmpl_CDouble_Expi(mid_exp_arg);
+        exp_right = tmpl_CDouble_Expi(right_exp_arg);
+
+        factor_left = tmpl_CDouble_Subtract(exp_left, exp_mid);
+        factor_right = tmpl_CDouble_Subtract(exp_right, exp_mid);
+
+        diff_left = tmpl_CDouble_Subtract(mid, left);
+        diff_right = tmpl_CDouble_Subtract(right, mid);
+
+        scale_left = tmpl_CDouble_Multiply_Real(rcpr_width, diff_left);
+        scale_right = tmpl_CDouble_Multiply_Real(rcpr_width, diff_right);
+
+        factor_left = tmpl_CDouble_Subtract(exp_left, exp_mid);
+        factor_right = tmpl_CDouble_Subtract(exp_right, exp_mid);
+
+        tmpl_CDouble_MultiplyBy(&factor_left, &scale_left);
+        tmpl_CDouble_MultiplyBy(&factor_right, &scale_right);
+
+        tmpl_CDouble_MultiplyBy_Imag(&factor_left, 1.0);
+        tmpl_CDouble_MultiplyBy_Imag(&factor_right, 1.0);
+
+        tmpl_CDouble_MultiplyBy_Real(&scale_left, p1);
+        tmpl_CDouble_MultiplyBy_Real(&scale_right, p1);
+
+        tmpl_CDouble_AddTo(&scale_left, &mid);
+        tmpl_CDouble_AddTo(&scale_right, &mid);
+
+        tmpl_CDouble_MultiplyBy_Real(&scale_left, 2.0 * p2);
+        tmpl_CDouble_MultiplyBy_Real(&scale_right, 2.0 * p2);
+
+        l_int = tmpl_CDouble_Rect(c_left_scale, s_left_scale);
+        r_int = tmpl_CDouble_Rect(c_right_scale, s_right_scale);
+
+        scale = tmpl_CDouble_Polar(0.5 / curvature, p0);
+
+        if (curvature < 0.0)
+        {
+            tmpl_CDouble_ConjugateSelf(&l_int);
+            tmpl_CDouble_ConjugateSelf(&r_int);
+        }
+
+        tmpl_CDouble_MultiplyBy(&l_int, &scale_left);
+        tmpl_CDouble_MultiplyBy(&r_int, &scale_right);
+
+        tmpl_CDouble_AddTo(&l_int, &factor_left);
+        tmpl_CDouble_AddTo(&r_int, &factor_right);
+
+        integrand = tmpl_CDouble_Add(l_int, r_int);
+        tmpl_CDouble_MultiplyBy(&integrand, &scale);
+    }
+
+    return integrand;
+}
+/*  End of tmpl_CDouble_Filon02_Integrand.                                    */
