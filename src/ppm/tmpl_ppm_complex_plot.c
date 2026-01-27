60ebd7dcdb8e3a71a6e5c63004982e28273d7d2b
ryanmaguire <rmaguire@protonmail.com>
2026-01-27 15:13:04 -0500

    Getting rid of long lines.

--- src/ppm/tmpl_ppm_complex_plot.c
+++ src/ppm/tmpl_ppm_complex_plot.c
@@ -117,7 +117,8 @@ void tmpl_PPM_Complex_Plot(const char *func_name,
             tmpl_PPM_Write_Color(fp[1], tmpl_PPM_Scaled_Gradient(w_y));
             tmpl_PPM_Write_Color(fp[2], tmpl_PPM_Scaled_Gradient(mag));
             tmpl_PPM_Write_Color(
-                fp[3], tmpl_PPM_Linear_Gradient(arg, -tmpl_double_pi, tmpl_double_pi)
+                fp[3],
+                tmpl_PPM_Linear_Gradient(arg, -tmpl_double_pi, tmpl_double_pi)
             );
         }
     }
