17dd164207ed7194b2517a676e0fd5c9cdaca4fc
ryanmaguire <rmaguire@protonmail.com>
2026-02-13 16:17:19 -0500

    Adding 'linear' to the linear interp1d routines.

--- include/tmpl_interpolate.h
+++ include/tmpl_interpolate.h
@@ -34,54 +34,58 @@
 
 /******************************************************************************
  *  Function:                                                                 *
- *      tmpl_Double_Sorted_Interp1d                                           *
+ *      tmpl_Double_Sorted_Linear_Interp1d                                    *
  *  Purpose:                                                                  *
  *      Interpolate values of (x, y) to (x_new, y_new).                       *
  *  Arguments:                                                                *
- *      x (const double *):                                                   *
+ *      x (const double * const):                                             *
  *          A sorted array of real numbers that are monotonically increasing. *
- *      y (const double *):                                                   *
+ *      y (const double * const):                                             *
  *          The data points corresponding to x.                               *
- *      len (size_t):                                                         *
+ *      len (const size_t):                                                   *
  *          The number of elements of x and y.                                *
- *      x_new (const double *):                                               *
- *          The new data points, sorted in increasing order.                  *
- *      y_new (double *):                                                     *
- *          The interpolated data corresponding to x_new.                     *
- *      len_new (size_t):                                                     *
+ *      x_new (const double * const):                                         *
+ *          The new data points. Assumed sorted in increasing order.          *
+ *      y_new (double * const):                                               *
+ *          The interpolated data corresponding to x_new, to be computed.     *
+ *      len_new (const size_t):                                               *
  *          The number of elements of x_new and y_new.                        *
  *  Output:                                                                   *
  *      None (void).                                                          *
  *  Notes:                                                                    *
  *      1.) Both x and x_new are assumed to be sorted in increasing order.    *
  *                                                                            *
- *      2.) For values x_new[m] < x[0], this function sets y_new[m] = y[0].   *
- *          Similarly for values x_new[m] > x[len-1] the set value is         *
- *          y_new[m] = y[len-1]. No extrapolating is done.                    *
+ *      2.) For values x_new[m] <= x[0], this function sets y_new[m] = y[0].  *
+ *          Similarly for values x_new[m] >= x[len - 1] the set value is      *
+ *          y_new[m] = y[len - 1]. No extrapolating is done.                  *
+ *                                                                            *
+ *      3.) If any of the inputs are NULL, or if len_new = 0, nothing is done.*
+ *                                                                            *
+ *      4.) If len = 1, then we set y_new[m] = y[0] for all m.                *
  ******************************************************************************/
 extern void
-tmpl_Float_Sorted_Interp1d(const float *x,
-                           const float *y,
-                           size_t len,
-                           const float *x_new,
-                           float *y_new,
-                           size_t len_new);
+tmpl_Float_Sorted_Linear_Interp1d(const float * const x,
+                                  const float * const y,
+                                  const size_t len,
+                                  const float * const x_new,
+                                  float * const y_new,
+                                  const size_t len_new);
 
 extern void
-tmpl_Double_Sorted_Interp1d(const double *x,
-                            const double *y,
-                            size_t len,
-                            const double *x_new,
-                            double *y_new,
-                            size_t len_new);
+tmpl_Double_Sorted_Linear_Interp1d(const double * const x,
+                                   const double * const y,
+                                   const size_t len,
+                                   const double * const x_new,
+                                   double * const y_new,
+                                   const size_t len_new);
 
 extern void
-tmpl_LDouble_Sorted_Interp1d(const long double *x,
-                             const long double *y,
-                             size_t len,
-                             const long double *x_new,
-                             long double *y_new,
-                             size_t len_new);
+tmpl_LDouble_Sorted_Linear_Interp1d(const long double * const x,
+                                    const long double * const y,
+                                    const size_t len,
+                                    const long double * const x_new,
+                                    long double * const y_new,
+                                    const size_t len_new);
 
 #endif
 /*  End of include guard.                                                     */
