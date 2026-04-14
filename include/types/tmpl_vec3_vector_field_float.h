ecea34492204d7c67f9072882464e7bc2d147791
ryanmaguire <rmaguire@protonmail.com>
2026-04-14 11:55:28 -0400

    Moving vector field typedefs to the types directory.

---
+++ include/types/tmpl_vec3_vector_field_float.h
@@ -0,0 +1,48 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is part of libtmpl.                                             *
+ *                                                                            *
+ *  libtmpl is free software: you can redistribute it and/or modify           *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  libtmpl is distributed in the hope that it will be useful,                *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with libtmpl.  If not, see <https://www.gnu.org/licenses/>.         *
+ ******************************************************************************
+ *                        tmpl_vec3_vector_field_float                        *
+ ******************************************************************************
+ *  Purpose:                                                                  *
+ *      Provides the function type float vec3 -> float vec3.                  *
+ ******************************************************************************
+ *  Author:     Ryan Maguire                                                  *
+ *  Date:       June 7, 2024                                                  *
+ ******************************************************************************
+ *                              Revision History                              *
+ ******************************************************************************
+ *  2026/04/14: Ryan Maguire                                                  *
+ *      Moved this typedef to the types directory.                            *
+ ******************************************************************************/
+
+/*  Include guard to prevent including this file twice.                       */
+#ifndef TMPL_TYPES_VEC3_VECTOR_FIELD_FLOAT_H
+#define TMPL_TYPES_VEC3_VECTOR_FIELD_FLOAT_H
+
+/*  Single precision 3D vectors provided here.                                */
+#include "tmpl_vec3_float.h"
+
+/*  Function pointer for single precision 3D vector fields.                   */
+typedef
+float
+(*tmpl_ThreeVectorFloatVectorField)(
+    const tmpl_ThreeVectorFloat * const
+);
+
+#endif
+/*  End of include guard.                                                     */
---
+++ include/types/tmpl_vec3_vector_field_ldouble.h
@@ -0,0 +1,48 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is part of libtmpl.                                             *
+ *                                                                            *
+ *  libtmpl is free software: you can redistribute it and/or modify           *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  libtmpl is distributed in the hope that it will be useful,                *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with libtmpl.  If not, see <https://www.gnu.org/licenses/>.         *
+ ******************************************************************************
+ *                       tmpl_vec3_vector_field_ldouble                       *
+ ******************************************************************************
+ *  Purpose:                                                                  *
+ *      Provides the function type long double vec3 -> long double vec3.      *
+ ******************************************************************************
+ *  Author:     Ryan Maguire                                                  *
+ *  Date:       June 7, 2024                                                  *
+ ******************************************************************************
+ *                              Revision History                              *
+ ******************************************************************************
+ *  2026/04/14: Ryan Maguire                                                  *
+ *      Moved this typedef to the types directory.                            *
+ ******************************************************************************/
+
+/*  Include guard to prevent including this file twice.                       */
+#ifndef TMPL_TYPES_VEC3_VECTOR_FIELD_LDOUBLE_H
+#define TMPL_TYPES_VEC3_VECTOR_FIELD_LDOUBLE_H
+
+/*  Long double precision 3D vectors provided here.                           */
+#include "tmpl_vec3_ldouble.h"
+
+/*  Function pointer for long double precision 3D vector fields.              */
+typedef
+long double
+(*tmpl_ThreeVectorLongDoubleVectorField)(
+    const tmpl_ThreeVectorLongDouble * const
+);
+
+#endif
+/*  End of include guard.                                                     */
