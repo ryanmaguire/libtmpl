25d5f739d50a0c7079e0412480eccbf7ec71f8ae
ryanmaguire <rmaguire@protonmail.com>
2026-04-16 16:16:22 -0400

    Cleaning up a bit.

--- src/assembly/aarch64/tmpl_aarch64_asm.h
+++ src/assembly/aarch64/tmpl_aarch64_asm.h
@@ -3,8 +3,8 @@
  ******************************************************************************
  *  This file is part of libtmpl.                                             *
  *                                                                            *
- *  libtmpl is free software: you can redistribute it and/or modify it        *
- *  under the terms of the GNU General Public License as published by         *
+ *  libtmpl is free software: you can redistribute it and/or modify           *
+ *  it under the terms of the GNU General Public License as published by      *
  *  the Free Software Foundation, either version 3 of the License, or         *
  *  (at your option) any later version.                                       *
  *                                                                            *
@@ -15,6 +15,14 @@
  *                                                                            *
  *  You should have received a copy of the GNU General Public License         *
  *  along with libtmpl.  If not, see <https://www.gnu.org/licenses/>.         *
+ ******************************************************************************
+ *                              tmpl_aarch64_asm                              *
+ ******************************************************************************
+ *  Purpose:                                                                  *
+ *      Provides helper macros for aarch64 assembly code.                     *
+  ******************************************************************************
+ *  Author:     Ryan Maguire                                                  *
+ *  Date:       November 4, 2024                                              *
  ******************************************************************************/
 
 /*  Include guard to prevent including this file twice.                       */
