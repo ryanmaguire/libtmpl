a84a6ada9b5a39a3590f6169a247ca51daef4d1f
ryanmaguire <rmaguire@protonmail.com>
2026-05-27 16:49:46 -0400

    Adding support for C++11 compilers, and adding more GNU attributes as optional macros.

--- include/tmpl_attributes.h
+++ include/tmpl_attributes.h
@@ -34,6 +34,21 @@
 #ifndef TMPL_ATTRIBUTES_H
 #define TMPL_ATTRIBUTES_H
 
+/*  C++ introduced attributes in C++11.                                       */
+#ifdef __cplusplus
+
+/*  Check if the compiler supports attributes.                                */
+#if __cplusplus >= 201103L
+
+/*  C++11 support found, indicate this.                                       */
+#define TMPL_ATTRIBUTES_SUPPORTED 1
+
+#endif
+/*  End of #if __cplusplus >= 201103L.                                        */
+
+#else
+/*  Else for #ifdef __cplusplus.                                              */
+
 /*  C89 does not define the __STDC_VERSION__ macro at all. Check for this.    */
 #ifdef __STDC_VERSION__
 
@@ -49,6 +64,9 @@
 #endif
 /*  End of #ifdef __STDC_VERSION__.                                           */
 
+#endif
+/*  End of #ifdef __cplusplus.                                                */
+
 /*  Provide some common attribute lists for libtmpl functions.                */
 #ifdef TMPL_ATTRIBUTES_SUPPORTED
 
@@ -59,6 +77,10 @@
  *  vendor attributes if they are not supported, making this portable.        */
 #define TMPL_CONST_FUNC [[nodiscard, gnu::const]]
 #define TMPL_UNSEQUENCED [[reproducible, unsequenced]]
+#define TMPL_NO_ASSOCIATIVE_MATH [[gnu::optimize("no-associative-math")]]
+#define TMPL_NO_CONTRACT_MATH [[gnu::optimize("fp-contract=off")]]
+#define TMPL_NO_FAST_MATH [[gnu::optimize("no-fast-math")]]
+#define TMPL_ALWAYS_INLINE [[gnu::always_inline]]
 
 #else
 
@@ -66,6 +88,10 @@
  *  macros to empty.                                                          */
 #define TMPL_CONST_FUNC
 #define TMPL_UNSEQUENCED
+#define TMPL_NO_ASSOCIATIVE_MATH
+#define TMPL_NO_CONTRACT_MATH
+#define TMPL_NO_FAST_MATH
+#define TMPL_ALWAYS_INLINE
 
 #endif
 /*  End of #ifdef TMPL_ATTRIBUTES_SUPPORTED.                                  */
