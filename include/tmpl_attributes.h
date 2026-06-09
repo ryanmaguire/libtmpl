14f21060a421a899d2f2c2bc2ed05adaca809676
ryanmaguire <rmaguire@protonmail.com>
2026-06-08 20:29:54 -0400

    Checking for attribute support for each attribute macro.

--- include/tmpl_attributes.h
+++ include/tmpl_attributes.h
@@ -42,6 +42,7 @@
 
 /*  C++11 support found, indicate this.                                       */
 #define TMPL_ATTRIBUTES_SUPPORTED 1
+#define TMPL_HAS_ATTRIBUTE(attr) __has_cpp_attribute(attr)
 
 #endif
 /*  End of #if __cplusplus >= 201103L.                                        */
@@ -57,6 +58,7 @@
 
 /*  C23 support found, indicate this for libtmpl.                             */
 #define TMPL_ATTRIBUTES_SUPPORTED 1
+#define TMPL_HAS_ATTRIBUTE(attr) __has_c_attribute(attr)
 
 #endif
 /*  End of #if __STDC_VERSION__ >= 202311L.                                   */
@@ -70,21 +72,82 @@
 /*  Provide some common attribute lists for libtmpl functions.                */
 #ifdef TMPL_ATTRIBUTES_SUPPORTED
 
-/*  Many math functions, like those of the type const double -> double, can   *
- *  be declared with the reproducible and unsequenced attributes. The GNU     *
- *  const attribute, which is stricter, may also be applied. GCC and Clang    *
- *  support gnu::const, and conforming C23 compilers are required to ignore   *
- *  vendor attributes if they are not supported, making this portable.        */
+/*  Math functions of the form double -> double often do not need to edit     *
+ *  global data in order to perform their computation. The gnu::const         *
+ *  attribute helps compilers (like GCC) vectorize such functions on CPUs     *
+ *  that support SIMD operations.                                             */
+#if TMPL_HAS_ATTRIBUTE(nodiscard) && TMPL_HAS_ATTRIBUTE(gnu::const)
 #define TMPL_CONST_FUNC [[nodiscard, gnu::const]]
-#define TMPL_LEAF_FUNC [[gnu::leaf]]
+#elif TMPL_HAS_ATTRIBUTE(gnu::const)
+#define TMPL_CONST_FUNC [[gnu::const]]
+#else
+#define TMPL_CONST_FUNC
+#endif
+
+/*  Reproducible is a C23 attribute that provides optimization hints.         */
+#if TMPL_HAS_ATTRIBUTE(reproducible)
 #define TMPL_REPRODUCIBLE [[reproducible]]
+#else
+#define TMPL_REPRODUCIBLE
+#endif
+
+/*  Unsequenced implies reproducible, but not the other way around. Check for *
+ *  unsequenced support first, but if the compiler does not implement         *
+ *  unsequenced but does support reproducible, then have the macro fall back  *
+ *  to reproducible.                                                          */
+#if TMPL_HAS_ATTRIBUTE(unsequenced)
 #define TMPL_UNSEQUENCED [[reproducible, unsequenced]]
+#else
+#define TMPL_UNSEQUENCED TMPL_REPRODUCIBLE
+#endif
+
+/*  Leaf functions are functions with external linkage that make no function  *
+ *  calls. This attribute helps improve optimizations.                        */
+#if TMPL_HAS_ATTRIBUTE(gnu::leaf)
+#define TMPL_LEAF_FUNC [[gnu::leaf]]
+#else
+#define TMPL_LEAF_FUNC
+#endif
+
+/*  Some functions, like 2Sum, Fast2Sum, and 2Prod, make use of the fact that *
+ *  floating-point arithmetic is not associative. The gnu:optimize attribute  *
+ *  can help prevent compilers from aggressively optimizing and breaking such *
+ *  functions.                                                                */
+#if TMPL_HAS_ATTRIBUTE(gnu::optimize)
 #define TMPL_NO_ASSOCIATIVE_MATH [[gnu::optimize("no-associative-math")]]
+#else
+#define TMPL_NO_ASSOCIATIVE_MATH
+#endif
+
+/*  Similarly, some functions make use of the fact that x * y + z is done in  *
+ *  two steps, producing two rounds: fl(fl(x * y) + z), where fl(a) denotes   *
+ *  the real number a rounded to the nearest floating-point value. Compilers  *
+ *  may replace x * y + z with a Fused-Multiply-Add call (FMA), which may     *
+ *  ruin certain functions. The following attribute prevents this.            */
+#if TMPL_HAS_ATTRIBUTE(gnu::optimize)
 #define TMPL_NO_CONTRACT_MATH [[gnu::optimize("fp-contract=off")]]
-#define TMPL_NO_FAST_MATH [[gnu::optimize("no-fast-math")]]
+#else
+#define TMPL_NO_CONTRACT_MATH
+#endif
+
+/*  Many helper / auxiliary functions serve no purpose other than to improve  *
+ *  the organization of some main function. These functions should always be  *
+ *  inlined. Various compilers provide methods of doing this.                 */
+#if TMPL_HAS_ATTRIBUTE(gnu::always_inline)
 #define TMPL_ALWAYS_INLINE [[gnu::always_inline]]
+#elif TMPL_HAS_ATTRIBUTE(clang::always_inline)
+#define TMPL_ALWAYS_INLINE [[clang::always_inline]]
+#elif TMPL_HAS_ATTRIBUTE(msvc::forceinline)
+#define TMPL_ALWAYS_INLINE [[msvc::forceinline]]
+#else
+#define TMPL_ALWAYS_INLINE
+#endif
+
+/*  Undefine the attribute macro to prevent possible collisions.              */
+#undef TMPL_HAS_ATTRIBUTE
 
 #else
+/*  Else for #ifdef TMPL_ATTRIBUTES_SUPPORTED.                                */
 
 /*  For compilers lacking C23 support, or when using older C++ compilers, set *
  *  these macros to be empty.                                                 */
@@ -94,7 +157,6 @@
 #define TMPL_UNSEQUENCED
 #define TMPL_NO_ASSOCIATIVE_MATH
 #define TMPL_NO_CONTRACT_MATH
-#define TMPL_NO_FAST_MATH
 #define TMPL_ALWAYS_INLINE
 
 #endif
