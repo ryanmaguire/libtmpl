11552f72163f3b17ffd2671b65e7ef451dcad72f
ryanmaguire <rmaguire@protonmail.com>
2026-06-10 15:29:09 -0400

    Adding headers for the unit clamp function.

--- include/tmpl_math.h
+++ include/tmpl_math.h
@@ -1869,20 +1869,9 @@ extern long double tmpl_LDouble_Truncate(long double x);
  *  Notes:                                                                    *
  *      Float and long double equivalents are provided as well.               *
  ******************************************************************************/
-
-#if TMPL_USE_INLINE == 1
-
-#include <libtmpl/include/inline/math/tmpl_unit_clamp_float.h>
-#include <libtmpl/include/inline/math/tmpl_unit_clamp_double.h>
-#include <libtmpl/include/inline/math/tmpl_unit_clamp_ldouble.h>
-
-#else
-
-extern double tmpl_Double_Unit_Clamp(double x);
-extern float tmpl_Float_Unit_Clamp(float x);
-extern long double tmpl_LDouble_Unit_Clamp(long double x);
-
-#endif
+#include <libtmpl/include/clamp/tmpl_unit_clamp_float.h>
+#include <libtmpl/include/clamp/tmpl_unit_clamp_double.h>
+#include <libtmpl/include/clamp/tmpl_unit_clamp_ldouble.h>
 
 /******************************************************************************
  *  Function:                                                                 *
