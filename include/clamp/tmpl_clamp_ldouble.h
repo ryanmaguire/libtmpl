2c1493d88ea170ad0699d8c15f6eb11bf9af8ab1
ryanmaguire <rmaguire@protonmail.com>
2026-06-10 14:38:52 -0400

    Adding 'umbrella' header files for using the clamp functions.

---
+++ include/clamp/tmpl_clamp_ldouble.h
@@ -0,0 +1,67 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is part of libtmpl.                                             *
+ *                                                                            *
+ *  libtmpl is free software: you can redistribute it and/or modify           *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  libtmpl is distributed in the hope that it will be useful,                *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with libtmpl.  If not, see <https://www.gnu.org/licenses/>.         *
+ ******************************************************************************
+ *                             tmpl_clamp_ldouble                             *
+ ******************************************************************************
+ *  Purpose:                                                                  *
+ *      Provides an inlined clamp function, or a forward declaration if       *
+ *      inline support is not available.                                      *
+ ******************************************************************************
+ *                                DEPENDENCIES                                *
+ ******************************************************************************
+ *  1.) tmpl_config.h:                                                        *
+ *          Provides the TMPL_USE_INLINE macro.                               *
+ *  2.) tmpl_attributes.h:                                                    *
+ *          Provides optional C23 attributes for optimization.                *
+ ******************************************************************************
+ *  Author:     Ryan Maguire                                                  *
+ *  Date:       June 10, 2026                                                 *
+ ******************************************************************************/
+
+/*  Include guard to prevent including this file twice.                       */
+#ifndef TMPL_CLAMP_CLAMP_LDOUBLE_H
+#define TMPL_CLAMP_CLAMP_LDOUBLE_H
+
+/*  The TMPL_USE_INLINE macro is found here.                                  */
+#include <libtmpl/include/tmpl_config.h>
+
+/*  Macros providing C23 attributes (for optimization) are found here.        */
+#include <libtmpl/include/tmpl_attributes.h>
+
+/*  clamp function is small enough that it can be inlined. Check for support. */
+#if TMPL_USE_INLINE == 1
+
+/*  Implemented in the inline directory.                                      */
+#include <libtmpl/include/inline/math/tmpl_clamp_impl_ldouble.h>
+
+/*  Lacking inline support, provide the forward declaration to the compiler.  */
+#else
+
+/*  Single precision clamp function.                                          */
+TMPL_CONST_FUNC
+extern long double
+tmpl_LDouble_Clamp(const long double x,
+                   const long double min,
+                   const long double max)
+TMPL_UNSEQUENCED;
+
+#endif
+/*  End of #if TMPL_USE_INLINE == 1.                                          */
+
+#endif
+/*  End of include guard.                                                     */
