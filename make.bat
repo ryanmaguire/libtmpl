881e59f2b91cb0a95892fc62dc4bbd513ffae068
ryanmaguire <rmaguire@protonmail.com>
2026-06-24 18:37:01 -0400

    Improving the make.bat file to handle more compiler options.

--- make.bat
+++ make.bat
@@ -1,5 +1,5 @@
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-::                                 LICENSE                                    ::
+::                                  LICENSE                                   ::
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 ::  This file is part of libtmpl.                                             ::
 ::                                                                            ::
@@ -23,143 +23,178 @@
 :: Disable printing out the lines being executed.
 @echo off
 
-:: Delete old files.
-IF EXIST *.exe (del *.exe)
-IF EXIST *.obj (del *.obj)
-IF EXIST *.o (del *.o)
-IF EXIST *.lib (del *.lib)
-IF EXIST *.so (del *.so)
+:: Prevent C flags from leaking into the caller's shell and make sure
+:: command extensions are available.
+SETLOCAL EnableExtensions
 
-:: See if the user specified which compiler to use.
-IF %1.==. GOTO MakeCL
-IF %1 == cl GOTO MakeCL
-IF %1 == clang-cl GOTO MakeClangCL
-IF %1 == clang GOTO MakeClang
-IF %1 == gcc GOTO MakeGCC
+:: Clean up files produces by a previous build.
+DEL /q *.exe *.obj *.o *.lib *.a *.dll 2>nul
 
-:MakeClang
+:: Select the compiler. No argument defaults to MSVC.
+SET "COMPILER=%~1"
+IF "%COMPILER%"=="" SET "COMPILER=cl"
 
-    :: Arguments for the compiler.
-    SET CMACR=-DTMPL_SET_USE_MATH_TRUE
-    SET CWARN=-Weverything
-    SET CNOWARN=-Wno-padded -Wno-float-equal -Wno-reserved-id-macro
-    SET CARGS=-O2 -I..\ -c
+IF /I "%COMPILER%"=="cl" GOTO SetCL
+IF /I "%COMPILER%"=="clang-cl" GOTO SetClangCL
+IF /I "%COMPILER%"=="clang" GOTO SetClang
+IF /I "%COMPILER%"=="gcc" GOTO SetGCC
 
-    :: Create include\tmpl_endianness.h
-    clang %CMACR% config.c -o config.exe
+:: Invalid compiler provided, jump to the usage statement.
+GOTO Usage
+
+::  Build settings:
+::      CC:
+::          The chosen compiler.
+::      CONF:
+::          The build settings used when compiling config.c.
+::      CWARN:
+::          Warning flags for the C compiler.
+::      CFLAGS:
+::          Flags (optimization, LTO, etc.) for the C compiler.
+::      EXEOUT:
+::          Flag that names the config executable.
+::      ARCHIVER:
+::          "lib" for MSVC, "ar" for GCC and Clang.
+::      LIBOUT:
+::           The name of the output static library.
+:SetCL
+    SET "CC=cl"
+    SET "CONF=/DTMPL_SET_USE_MATH_TRUE"
+    SET "CWARN=/W4"
+    SET "CFLAGS=/nologo /I../ /O2 /GL /c"
+    SET "EXEOUT=/Fe"
+    SET "ARCHIVER=lib"
+    SET "LIBOUT=libtmpl.lib"
+    GOTO Build
+
+:SetClangCL
+    SET "CC=clang-cl"
+    SET "CONF=/DTMPL_SET_USE_MATH_TRUE"
+    SET "CWARN=-Weverything -Wno-padded -Wno-float-equal"
+    SET "CFLAGS=/nologo /I../ /O2 /GL /c"
+    SET "EXEOUT=/Fe"
+    SET "ARCHIVER=lib"
+    SET "LIBOUT=libtmpl.lib"
+    GOTO Build
+
+:SetClang
+    SET "CC=clang"
+    SET "CONF=-DTMPL_SET_USE_MATH_TRUE"
+    SET "CWARN=-Weverything -Wno-padded -Wno-float-equal"
+    SET "CFLAGS=-O3 -flto -I../ -c"
+    SET "EXEOUT=-o "
+    SET "ARCHIVER=ar"
+    SET "LIBOUT=libtmpl.a"
+    GOTO Build
+
+:SetGCC
+    SET "CC=gcc"
+    SET "CONF=-DTMPL_SET_USE_MATH_TRUE"
+    SET "CWARN=-Wall -Wextra -Wpedantic -Wno-padded -Wno-float-equal"
+    SET "CFLAGS=-O3 -flto -I../ -c"
+    SET "EXEOUT=-o "
+    SET "ARCHIVER=ar"
+    SET "LIBOUT=libtmpl.a"
+    GOTO Build
+
+:Build
+    ECHO Building libtmpl with %CC%...
+
+    :: Compile and execute the config.c file to probe the architecture.
+    ECHO Configuring...
+    %CC% %CONF% %EXEOUT%config.exe config.c
+    IF errorlevel 1 GOTO ConfigError
     config.exe
-    del *.exe *.obj
 
-    :: Compile the library.
+    :: Check if an error occurred, then clean up the files.
+    IF errorlevel 1 GOTO ConfigError
+    DEL /q config.exe config.obj config.o 2>nul
+
+    :: Compile every .c in the src directory, one subdirectory at a time,
+    :: stopping on the first failure.
     FOR /D %%d in (src\*) DO (
-        IF "%%d" == "src\assembly" (
-            ECHO Skipping src\assembly\
-        ) ELSE (
-            ECHO Compiling %%d:
-            FOR %%i in (%%d\*.c) DO (
-                ECHO     Compiling %%i
-                clang %CWARN% %CNOWARN% %CARGS% %%i
-            )
-        )
+        CALL :CompileDir "%%d"
+        IF errorlevel 1 GOTO CompileError
     )
 
-    :: Go to the Linking stage.
-    GOTO LinkOLib
+    :: Archive the objects into the static library.
+    ECHO Creating %LIBOUT%...
+    CALL :Archive
+    IF errorlevel 1 GOTO LinkError
 
-:MakeGCC
+    :: Remove intermediates; keep the finished library.
+    DEL /q *.exe *.obj *.o 2>nul
+    GOTO Done
 
-    :: Arguments for the compiler.
-    SET CMACR=-DTMPL_SET_USE_MATH_TRUE
-    SET CWARN=-Wall -Wextra -Wpedantic
-    SET CNOWARN=-Wno-padded -Wno-float-equal -Wno-reserved-id-macro
-    SET CARGS=-O2 -I..\ -c
-
-    :: Create include\tmpl_endianness.h
-    gcc %CMACR% config.c -o config.exe
-    config.exe
-    del *.exe *.obj
-
-    :: Compile the library.
-    FOR /D %%d in (src\*) DO (
-        IF "%%d" == "src\assembly" (
-            ECHO Skipping src\assembly\
-        ) ELSE (
-            ECHO Compiling %%d:
-            FOR %%i in (%%d\*.c) DO (
-                ECHO     Compiling %%i
-                gcc %CWARN% %CNOWARN% %CARGS% %%i
-            )
-        )
+:: Compile every C file in a given subdirectory.
+:CompileDir
+    :: The batch script does not handle assembly code.
+    :: If you wish to use assembly on Windows, build libtmpl using CMake.
+    IF /I "%~nx1"=="assembly" (
+        ECHO Skipping %~1\
+        EXIT /b 0
     )
 
-    :: Go to the Linking stage.
-    GOTO LinkOLib
+    ECHO Compiling %~1:
 
-:MakeClangCL
+    :: Compile each .c file.
+    FOR %%i in (%~1\*.c) DO (
+        ECHO     %%i
+        %CC% %CWARN% %CFLAGS% %%i
 
-    :: Arguments for the compiler.
-    SET CMACR=-DTMPL_SET_USE_MATH_TRUE
-    SET CWARN=-Weverything -Wno-padded -Wno-float-equal -Wno-reserved-id-macro
-    SET CARGS=-O2 -I..\ -c
-
-    :: Create include\tmpl_endianness.h
-    clang-cl %CMACR% config.c -o config.exe
-    config.exe
-    del *.exe *.obj
-
-    :: Compile the library.
-    FOR /D %%d in (src\*) DO (
-        IF "%%d" == "src\assembly" (
-            ECHO Skipping src\assembly\
-        ) ELSE (
-            ECHO Compiling %%d:
-            FOR %%i in (%%d\*.c) DO (
-                ECHO     Compiling %%i
-                clang-cl %CWARN% %CARGS% %%i
-            )
-        )
+        :: Check for compiler errors, abort if there are any.
+        IF errorlevel 1 EXIT /b 1
     )
 
-    :: Go to the Linking stage.
-    GOTO LinkObjLib
+    EXIT /b 0
 
-:MakeCL
+:: Bundle the compiled objects into the static library.
+:Archive
 
-    :: Arguments for the compiler.
-    SET CMACR=/DTMPL_SET_USE_MATH_TRUE
-    SET CWARN=/W4
-    SET CARGS=/I../ /O2 /c
-
-    :: Create include\tmpl_endianness.h
-    cl %CMACR% config.c /link /out:config.exe
-    config.exe
-    del *.exe *.obj
-
-    :: Compile the library.
-    FOR /D %%d in (.\src\*) DO (
-        IF "%%d" == ".\src\assembly" (
-            ECHO Skipping src\assembly\
-        ) ELSE (
-            cl %CWARN% %CARGS% %%d\*.c
-        )
+    :: MSVC and ClangCL use Microsoft's lib.exe tool.
+    IF "%ARCHIVER%"=="lib" (
+        lib /nologo /out:%LIBOUT% *.obj
+        exit /b
     )
 
-    :: Go to the Linking stage.
-    GOTO LinkObjLib
+    :: GCC and regular Clang use ar. Create a list of object files.
+    IF EXIST obj_list.tmp DEL /q obj_list.tmp
+    FOR %%o in (*.o) DO ECHO %%o>>obj_list.tmp
 
-:LinkOLib
+    :: Create the static library from this list.
+    ar rcs %LIBOUT% @obj_list.tmp
 
-    :: Link everything into a .lib file.
-    lib /out:libtmpl.lib *.o
+    :: Check for errors.
+    IF errorlevel 1 (DEL /q obj_list.tmp 2>nul & EXIT /b 1)
 
     :: Clean up.
-    del *.exe *.obj *.o
-    GOTO :EOF
+    DEL /q obj_list.tmp 2>nul
+    EXIT /b 0
 
-:LinkObjLib
+::  Error message if the user provides an unsupported compiler.
+:Usage
+    ECHO Unknown compiler "%COMPILER%".
+    ECHO Usage: make.bat [cl ^| clang-cl ^| clang ^| gcc]
+    ECHO No argument defaults to cl.
+    GOTO Fail
 
-    :: Link everything into a .lib file.
-    lib /out:libtmpl.lib *.obj
+:ConfigError
+    ECHO ERROR: Failed to compile and execute config.c.
+    GOTO Fail
 
-    :: Clean up.
-    del *.exe *.obj *.o
+:CompileError
+    ECHO ERROR: a source file failed to compile. See the messages above.
+    GOTO Fail
+
+:LinkError
+    ECHO ERROR: Failed to create %LIBOUT%.
+    GOTO Fail
+
+:Fail
+    DEL /q *.exe *.obj *.o 2>nul
+    ENDLOCAL
+    EXIT /b 1
+
+:Done
+    ENDLOCAL
+    EXIT /b 0
