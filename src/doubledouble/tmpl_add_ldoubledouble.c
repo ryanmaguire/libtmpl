6c07cd571cfc7b058671a51425fce91b2dd6fde1
ryanmaguire <rmaguire@protonmail.com>
2026-07-31 11:31:56 -0400

    Updating references and adding C23 attributes.

--- src/doubledouble/tmpl_add_ldoubledouble.c
+++ src/doubledouble/tmpl_add_ldoubledouble.c
@@ -36,14 +36,14 @@
  *      sum (tmpl_LongDoubleDouble):                                          *
  *          The double-double sum of x and y.                                 *
  *  Called Functions:                                                         *
- *      tmpl_two_sum.h:                                                       *
+ *      src/two_sum/                                                          *
  *          tmpl_LDouble_Two_Sum:                                             *
  *              Performs the 2Sum of two numbers: (sum, error) = 2Sum(x, y).  *
  *          tmpl_LDouble_Fast_Two_Sum:                                        *
  *              Performs Fast2Sum: (sum, error) = Fast2Sum(x, y).             *
  *  Method:                                                                   *
  *      We use the corrected algorithm presented in Li et al. 2002, and       *
- *      follow the implementation in Joldas et al. 2017 near verbatim. That   *
+ *      follow the implementation in Joldes et al. 2017 near verbatim. That   *
  *      is, we do:                                                            *
  *                                                                            *
  *          (sum_hi, err_hi) = 2Sum(xhi, yhi)                                 *
@@ -68,62 +68,102 @@
  *      works if |x| is significantly greater than |y|. If you are sure of    *
  *      this, use tmpl_LDoubleDouble_Quick_Add.                               *
  *  Notes:                                                                    *
- *      1.) Depending on compiler and architecture we may need to declare     *
- *          certain variables as volatile. Failure to do so results in a      *
- *          poor Fast2Sum or a poor 2Sum. The source code for 2Sum and        *
- *          Fast2Sum use the TMPL_VOLATILE macro for this. On default builds  *
- *          this macro is empty, meaning the volatile keyword is not used.    *
- *          If unit tests for double-double arithmetic fail, one should first *
- *          try enabling the TMPL_VOLATILE macro. In the Makefile this is     *
- *          done via make USE_VOLATILE=1 [other options].                     *
+ *      1.) This function calls libtmpl's 2Sum and Fast2Sum routines. Those   *
+ *          routines use floating-point barriers to protect against           *
+ *          optimizations like -ffast-math. On GCC and Clang this results in  *
+ *          a no-op and the generated machine code is both correct and        *
+ *          optimal. On other compilers these barriers may use the volatile   *
+ *          keyword which significantly slows things down. Clang-like         *
+ *          compilers such as ICX, Open XL, and AOCC are able to use the      *
+ *          no-op barriers, as is MSVC. Compilers like TCC and PCC are not.   *
+ *                                                                            *
  *      2.) If both x and y have the same sign, it is faster (and about as    *
- *          accurate) to use tmpl_DoubleDouble_Quick_Add. If x and y have     *
+ *          accurate) to use tmpl_LDoubleDouble_Quick_Add. If x and y have    *
  *          differing signs, you should use this function.                    *
  *  References:                                                               *
- *      1.) Joldas, M., Muller, J., Popescu, V. (October 2017).               *
- *          "Tight and rigorous error bounds for basic                        *
- *              building blocks of double-word arithmetic."                   *
+ *      1.) Joldes, M., Muller, J., Popescu, V. (October 2017).               *
+ *          Tight and rigorous error bounds for basic                         *
+ *          building blocks of double-word arithmetic.                        *
  *          ACM Transactions on Mathematical Software, Vol. 44, No. 2.        *
+ *                                                                            *
+ *          Detailed description of double-double algorithms are provided     *
+ *          here. libtmpl uses many of the ideas from this paper in it's      *
+ *          implementation of double-double arithmetic.                       *
+ *                                                                            *
  *      2.) Li, X., Demmel, J., Bailey, D., Henry, G., Hida, Y.,              *
- *              Iskandar, J., Kahan, W., Kapur, A., Martin, M.,               *
- *              Tung, T., Yoo, D. (2002).                                     *
- *          "Design, implementation and testing of extended                   *
- *              and mixed precision BLAS."                                    *
+ *          Iskandar, J., Kahan, W., Kapur, A., Martin, M.,                   *
+ *          Tung, T., Yoo, D. (2002).                                         *
+ *          Design, implementation and testing of extended and mixed          *
+ *          precision BLAS.                                                   *
  *          ACM Trans. Math. Software Vol. 28, No. 2: Pages 152-205           *
+ *                                                                            *
+ *          Lengthy paper on the details of double-double arithmetic with     *
+ *          applications to linear algebra routines.                          *
+ *                                                                            *
  *      3.) Hida, Y., Li, X., Bailey, D. (May 2008).                          *
- *          "Library for Double-Double and Quad-Double Arithmetic."           *
+ *          Library for Double-Double and Quad-Double Arithmetic.             *
+ *                                                                            *
+ *          Paper detailing the implementation of double-double and           *
+ *          quad-double arithmetic. The 2Sum and Fast2Sum algorithms are      *
+ *          described here, as is the 2Prod algorithm.                        *
+ *                                                                            *
  *      4.) Dekker, T.J. (June 1971).                                         *
- *          "A floating-point technique for                                   *
- *              extending the available precision."                           *
- *          Numerische Mathematik. Vol. 18, No. 3: Pages 224-242.             *
+ *          A floating-point technique for extending the available precision. *
+ *          Numerische Mathematik. Volume 18, Number 3: Pages 224-242.        *
+ *                                                                            *
+ *          Classic paper on double-double arithmetic. Dekker's analysis of   *
+ *          the Fast2Sum algorithm is provided here.                          *
+ *                                                                            *
  *      5.) Shewchuk, J. (October 1997).                                      *
- *          "Adaptive Precision Floating-Point Arithmetic                     *
- *              and Fast Robust Geometric Predicates."                        *
- *          Discrete & Computational Geometry Vol. 18, No. 3: Pages 305-363   *
+ *          Adaptive Precision Floating-Point Arithmetic and                  *
+ *          Fast Robust Geometric Predicates.                                 *
+ *          Discrete & Computational Geometry Vol 18, Number 3: Pages 305-363 *
+ *                                                                            *
+ *          Detailed analysis of 2Sum, Fast2Sum, and 2Prod, allowing for      *
+ *          arbitrarily sized floating-point types. This paper is useful for  *
+ *          extending these algorithms to the various long double types such  *
+ *          as 80-bit extended and 128-bit quadruple. Proofs are included     *
+ *          along with the algorithms.                                        *
  ******************************************************************************
  *                                DEPENDENCIES                                *
  ******************************************************************************
  *  1.) tmpl_config.h:                                                        *
- *          Header file containing TMPL_INLINE_DECL macro.                    *
+ *          Header file providing the TMPL_ALWAYS_INLINE macro.               *
+ *  2.) tmpl_attributes.h:                                                    *
+ *          Provides (optional) C23 attributes for optimization.              *
+ *  3.) tmpl_two_sum.h:                                                       *
+ *          Provides 2Sum, Fast2Sum, and other 2Sum-like routines.            *
+ *  4.) tmpl_doubledouble.h:                                                  *
+ *          Function prototype and double-double typedefs provided here.      *
  ******************************************************************************
  *  Author:     Ryan Maguire                                                  *
  *  Date:       November 26, 2024                                             *
+ ******************************************************************************
+ *                              Revision History                              *
+ ******************************************************************************
+ *  2026/07/29: Ryan Maguire                                                  *
+ *      Added C23 attributes, updated references.                             *
  ******************************************************************************/
 
-/*  TMPL_INLINE_DECL macro found here, as is TMPL_USE_INLINE.                 */
+/*  TMPL_ALWAYS_INLINE macro found here.                                      */
 #include <libtmpl/include/tmpl_config.h>
 
-/*  tmpl_LongDoubleDouble typedef provided here.                              */
-#include <libtmpl/include/tmpl_doubledouble.h>
+/*  Optional C23 attributes for optimization provided here.                   */
+#include <libtmpl/include/tmpl_attributes.h>
 
-/*  2Sum and Fast2Sum are needed for double-double addition.                  */
+/*  2Sum and Fast2Sum are needed for long-double-double addition.             */
 #include <libtmpl/include/tmpl_two_sum.h>
 
+/*  Function prototytpe / forward declaration found here.                     */
+#include <libtmpl/include/tmpl_doubledouble.h>
+
 /*  Performs addition for two long-double-doubles.                            */
+TMPL_PURE_FUNC
 TMPL_ALWAYS_INLINE
 tmpl_LongDoubleDouble
 tmpl_LDoubleDouble_Add(const tmpl_LongDoubleDouble * const x,
                        const tmpl_LongDoubleDouble * const y)
+TMPL_UNSEQUENCED
 {
     /*  Naive sums and their error terms from the two-sum algorithm.          */
     long double sum_hi, err_hi, sum_lo, err_lo;
@@ -151,7 +191,7 @@ tmpl_LDoubleDouble_Add(const tmpl_LongDoubleDouble * const x,
     /*  We can now use Fast2Sum, instead of 2Sum, with the correction terms.  *
      *  This is because sum_hi and corr_hi have exponents that differ enough  *
      *  that cancellation will not occur, and no additional error will be     *
-     *  introduced. See Joldas et. al (2017), theorem 3.1 for details. Use    *
+     *  introduced. See Joldes et. al (2017), theorem 3.1 for details. Use    *
      *  Fast2Sum and collect the compensation terms.                          */
     tmpl_LDouble_Fast_Two_Sum(sum_hi, corr_hi, &comp_hi, &comp_lo);
 
@@ -163,7 +203,7 @@ tmpl_LDoubleDouble_Add(const tmpl_LongDoubleDouble * const x,
     /*  We can conlude with a Fast2Sum with the high order compensation and   *
      *  the low order correction. One can show that Fast2Sum introduces no    *
      *  additional error with comp_hi and corr_lo, so we can save a call to   *
-     *  2Sum. Again, see theorem 3.1 in Joldas et. al (2017).                 */
+     *  2Sum. Again, see theorem 3.1 in Joldes et. al (2017).                 */
     tmpl_LDouble_Fast_Two_Sum(comp_hi, corr_lo, &(out.dat[0]), &(out.dat[1]));
 
     /*  out now has the long-double-double sum of x and y. Note that we may   *
