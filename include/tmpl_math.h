4a06936073fb283b17b6717782667479b0a94654
ryanmaguire <rmaguire@protonmail.com>
2026-08-26 20:30:20 -0400

    Merging inline vs. non-inline atan2 code.

--- include/tmpl_math.h
+++ include/tmpl_math.h
@@ -440,9 +440,6 @@ extern const float tmpl_Min_Float_Base_E;
 extern const double tmpl_Min_Double_Base_E;
 extern const long double tmpl_Min_LDouble_Base_E;
 
-/*  Commonly used constants (pi, e, etc.) found here.                         */
-#include <libtmpl/include/constants/tmpl_math_constants.h>
-
 /******************************************************************************
  *                              Tables and Data                               *
  ******************************************************************************/
@@ -2157,27 +2154,28 @@ extern tmpl_Bool tmpl_LDouble_Is_NaN_Or_Inf(long double x);
  *  Purpose:                                                                  *
  *      Computes the positive angle the point (y, x) makes with the x axis.   *
  *  Arguments:                                                                *
- *      y (double):                                                           *
+ *      y (const double):                                                     *
  *          A real number.                                                    *
- *      x (double):                                                           *
+ *      x (const double):                                                     *
  *          Another real number.                                              *
  *  Output:                                                                   *
  *      angle (double):                                                       *
  *          The positive angle the point (x, y) makes with the x axis.        *
  ******************************************************************************/
-#if TMPL_USE_INLINE == 1
+TMPL_CONST_FUNC
+extern double
+tmpl_Double_Positive_Arctan2(const double y, const double x)
+TMPL_UNSEQUENCED;
 
-#include <libtmpl/include/inline/math/tmpl_positive_arctan2_float.h>
-#include <libtmpl/include/inline/math/tmpl_positive_arctan2_double.h>
-#include <libtmpl/include/inline/math/tmpl_positive_arctan2_ldouble.h>
+TMPL_CONST_FUNC
+extern float
+tmpl_Float_Positive_Arctan2(const float y, const float x)
+TMPL_UNSEQUENCED;
 
-#else
-
-extern float tmpl_Float_Positive_Arctan2(float y, float x);
-extern double tmpl_Double_Positive_Arctan2(double y, double x);
-extern long double tmpl_LDouble_Positive_Arctan2(long double y, long double x);
-
-#endif
+TMPL_CONST_FUNC
+extern long double
+tmpl_LDouble_Positive_Arctan2(const long double y, const long double x)
+TMPL_UNSEQUENCED;
 
 /******************************************************************************
  *  Function:                                                                 *
--- src/math/tmpl_positive_arctan2_math_double.c
+++ src/math/tmpl_positive_arctan2_math_double.c
@@ -16,7 +16,7 @@
  *  You should have received a copy of the GNU General Public License         *
  *  along with libtmpl.  If not, see <https://www.gnu.org/licenses/>.         *
  ******************************************************************************
- *                        tmpl_positive_arctan2_float                         *
+ *                        tmpl_positive_arctan2_double                        *
  ******************************************************************************
  *  Purpose:                                                                  *
  *      Compute the positive angle the point (x, y) makes with the x axis.    *
@@ -24,71 +24,77 @@
  *                             DEFINED FUNCTIONS                              *
  ******************************************************************************
  *  Function Name:                                                            *
- *      tmpl_Float_Positive_Arctan2                                           *
+ *      tmpl_Double_Positive_Arctan2                                          *
  *  Purpose:                                                                  *
  *      Computes the positive angle the point (x, y) makes with the x axis.   *
  *  Arguments:                                                                *
- *      y (float):                                                            *
+ *      y (const double):                                                     *
  *          A real number, the vertical component of the point.               *
- *      x (float):                                                            *
+ *      x (const double):                                                     *
  *          A real number, the horizontal component of the point.             *
  *  Output:                                                                   *
- *      theta (float):                                                        *
+ *      theta (double):                                                       *
  *          The angle, between 0 and 2pi, the point (x, y) makes with the     *
  *          x axis.                                                           *
  *  Called Functions:                                                         *
  *      src/math/                                                             *
- *          tmpl_Float_Arctan2:                                               *
+ *          tmpl_Double_Arctan2:                                              *
  *              Computes the standard atan2 function, returning an angle      *
  *              between -pi and pi.                                           *
  *  Method:                                                                   *
- *      Call the tmpl_Float_Arctan2 function and shift the result by          *
- *      2*pi if the output is negative.                                       *
+ *      Call the tmpl_Double_Arctan2 function and shift the result by         *
+ *      2 * pi if the output is negative.                                     *
  ******************************************************************************
  *                                DEPENDENCIES                                *
  ******************************************************************************
  *  1.) tmpl_config.h:                                                        *
- *          Header file containing TMPL_INLINE_DECL macro.                    *
- *  2.) tmpl_math_constants.h:                                                *
- *          Header file providing math constants like 2 pi.                   *
+ *          Header file containing the TMPL_ALWAYS_INLINE macro.              *
+ *  2.) tmpl_attributes.h:                                                    *
+ *          Provides optional C23 attributes for optimization.                *
+ *  3.) tmpl_math.h:                                                          *
+ *          Header file providing the forward declaration and arctan2.        *
  ******************************************************************************
  *  Author:     Ryan Maguire                                                  *
  *  Date:       May 6, 2025                                                   *
+ ******************************************************************************
+ *                              Revision History                              *
+ ******************************************************************************
+ *  2026/08/12: Ryan Maguire                                                  *
+ *      Merged inline and non-inline versions, added TMPL_ALWAYS_INLINE use.  *
  ******************************************************************************/
 
-/*  Include guard to prevent including this file twice.                       */
-#ifndef TMPL_POSITIVE_ARCTAN2_FLOAT_H
-#define TMPL_POSITIVE_ARCTAN2_FLOAT_H
-
-/*  Location of the TMPL_INLINE_DECL macro.                                   */
+/*  Location of the TMPL_ALWAYS_INLINE macro.                                 */
 #include <libtmpl/include/tmpl_config.h>
 
-/*  tmpl_float_two_pi provided here.                                          */
+/*  Macros providing C23 attributes (for optimization) are found here.        */
+#include <libtmpl/include/tmpl_attributes.h>
+
+/*  2 pi, and other math constants, found here.                               */
 #include <libtmpl/include/constants/tmpl_math_constants.h>
 
-/*  Tell the compiler about the main Arctan2 function.                        */
-extern float tmpl_Float_Arctan2(float y, float x);
+/*  Function prototype / forward declaration and arctan2 found here.          */
+#include <libtmpl/include/tmpl_math.h>
 
-/*  Single precision positive angle function.                                 */
-TMPL_INLINE_DECL
-float tmpl_Float_Positive_Arctan2(float y, float x)
+/*  Double-precision positive angle function.                                 */
+TMPL_ALWAYS_INLINE
+TMPL_CONST_FUNC
+double
+tmpl_Double_Positive_Arctan2(const double y, const double x)
+TMPL_UNSEQUENCED
 {
     /*  Most of the work is handled by the Arctan2 routine.                   */
-    const float angle = tmpl_Float_Arctan2(y, x);
+    const double angle = tmpl_Double_Arctan2(y, x);
 
     /*  Shift by two pi if the angle is negative. Since angles are equivalent *
      *  mod two pi, this does not change the true angle. The Arctan2 function *
      *  always returns a value between -pi and pi, so shifting by two pi      *
      *  guarantees the output is non-negative. Note, if "angle" is NaN, the   *
-     *  output will also be NaN since comparing a NaN using "==" always       *
+     *  output will also be NaN since comparing a NaN using "<" always        *
      *  returns False. Hence there are no checks for NaNs here.               */
-    if (angle < 0.0F)
-        return angle + tmpl_float_two_pi;
+    if (angle < 0.0)
+        return angle + tmpl_double_two_pi;
 
     /*  Otherwise the angle lies between 0 and pi already. Simply return it.  */
     return angle;
 }
-/*  End of tmpl_Float_Positive_Arctan2.                                       */
-
-#endif
-/*  End of include guard.                                                     */
+/*  End of tmpl_Double_Positive_Arctan2.                                      */
